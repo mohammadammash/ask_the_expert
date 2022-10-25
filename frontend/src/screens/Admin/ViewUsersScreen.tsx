a55ffac7fa132a4d18d1db6497f8d0cf3caaaea2
mohammadammash <mohammadammash31@gmail.com>
2022-10-25 21:25:59 +0300

    frontend/src/screens/Admin/ViewUsersScreen.tsx: Adding Admin View Users Screen

---
+++ frontend/src/screens/Admin/ViewUsersScreen.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ViewUsersScreen = () => {
+  return (
+    <View>
+      <Text>ViewUsersScreen Admin</Text>
+    </View>
+  )
+}
+
+export default ViewUsersScreen
