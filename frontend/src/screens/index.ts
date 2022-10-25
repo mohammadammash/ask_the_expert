80ec0123d1bd771a06d009fd710c8448582726c3
mohammadammash <mohammadammash31@gmail.com>
2022-10-25 15:00:00 +0300

    frontend/src/screens/index.ts: Exporting all screens

---
+++ frontend/src/screens/index.ts
@@ -0,0 +1,17 @@
+//AUTH
+export {default as RegisterScreen} from "./Auth/RegisterScreen";
+export {default as LoginScreen} from "./Auth/LoginScreen";
+
+//COMMON
+export {default as AppointmentsScreen} from "./Common/AppointmentsScreen";
+export {default as ChatsScreen} from "./Common/ChatsScreen";
+export {default as LeaderboardScreen} from "./Common/LeaderboardScreen";
+export {default as SingleChatScreen} from "./Common/SingleChatScreen";
+export {default as EditProfileScreen} from "./Common/EditProfileScreen";
+
+//EXPERT
+export {default as ExpertProfileScreen} from "./Expert/ProfileScreen";
+export {default as GoOnlineScreen} from "./Expert/GoOnlineScreen";
+
+//NOVICE
+export {default as NoviceProfileScreen} from "./Novice/ProfileScreen";
