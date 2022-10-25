a0f9437b44bd27c2690cfb0097e80b0208e9e3a2
mohammadammash <mohammadammash31@gmail.com>
2022-10-25 17:34:39 +0300

    frontend/src/navigations/NoviceHomeStackNavigator.tsx:Creating NoviceHomeStack

---
+++ frontend/src/navigations/NoviceHomeStackNavigator.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { ExpertProfileScreen, BookAppointmentScreen, HomeScreen } from "../screens";
+import { ROUTES } from "../constants";
+const Stack = createStackNavigator();
+
+const NoviceHomeStackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name={ROUTES.NOVICE_HOME} component={HomeScreen} />
+      <Stack.Screen name={ROUTES.EXPERT_PROFILE} component={ExpertProfileScreen} />
+      <Stack.Screen name={ROUTES.NOVICE_BOOK_APPOINTMENT} component={BookAppointmentScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default NoviceHomeStackNavigator;
