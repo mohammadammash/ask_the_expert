0c67b7c49f7775ba51aae5d9fe731a543c77c68a
mohammadammash <mohammadammash31@gmail.com>
2022-10-25 14:47:10 +0300

    frontend/styles.ts: Adding reusable utility style classes

---
+++ frontend/styles.ts
@@ -0,0 +1,34 @@
+import { StyleSheet } from "react-native";
+import {COLORS} from "./src/constants/index";
+
+export default StyleSheet.create({
+  //COLORS
+  orange_text : {
+    color: COLORS.orange,
+  },
+  //INPUTS
+  text_input: {
+    borderWidth: 2,
+    height: 50,
+    borderRadius: 10,
+  },
+  //BUTTONS
+    blue_button_xl: {
+      borderWidth: 2,
+      height: 60,
+      borderRadius: 10,
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor: COLORS.blue,
+      color: COLORS.white,
+    },
+    blue_button_lg: {
+      borderWidth: 2,
+      height: 60,
+      width:200,
+      borderRadius: 10,
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor: COLORS.blue,
+    }
+});
