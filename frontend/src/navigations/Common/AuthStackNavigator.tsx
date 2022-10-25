b6e7999b9f74fa993def6b6a93e07aa16a21536d
mohammadammash <mohammadammash31@gmail.com>
2022-10-25 21:23:56 +0300

    frontend/navigations: Altering routes names to match usertype

--- frontend/src/navigations/Common/AuthStackNavigator.tsx
+++ frontend/src/navigations/Common/AuthStackNavigator.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import { LoginScreen, RegisterScreen } from "../../screens";
+import { AuthLoginScreen, AuthRegisterScreen } from "../../screens";
 import { ROUTES } from "../../constants";
 const Stack = createStackNavigator();
 
 const AuthStackNavigator = () => {
   return (
     <Stack.Navigator initialRouteName={ROUTES.LOGIN}>
-      <Stack.Screen name={ROUTES.LOGIN} component={LoginScreen} />
-      <Stack.Screen name={ROUTES.REGISTER} component={RegisterScreen} />
+      <Stack.Screen name={ROUTES.LOGIN} component={AuthLoginScreen} />
+      <Stack.Screen name={ROUTES.REGISTER} component={AuthRegisterScreen} />
     </Stack.Navigator>
   );
 }
--- frontend/src/navigations/Novice/NoviceHomeStackNavigator.tsx
+++ frontend/src/navigations/Novice/NoviceHomeStackNavigator.tsx
@@ -1,15 +1,15 @@
 import * as React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import { ExpertProfileScreen, BookAppointmentScreen, HomeScreen } from "../../screens";
+import { ExpertProfileScreen, NoviceBookAppointmentScreen, NoviceHomeScreen } from "../../screens";
 import { ROUTES } from "../../constants";
 const Stack = createStackNavigator();
 
 const NoviceHomeStackNavigator = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name={ROUTES.NOVICE_HOME} component={HomeScreen} />
+      <Stack.Screen name={ROUTES.NOVICE_HOME} component={NoviceHomeScreen} />
       <Stack.Screen name={ROUTES.EXPERT_PROFILE} component={ExpertProfileScreen} />
-      <Stack.Screen name={ROUTES.NOVICE_BOOK_APPOINTMENT} component={BookAppointmentScreen} />
+      <Stack.Screen name={ROUTES.NOVICE_BOOK_APPOINTMENT} component={NoviceBookAppointmentScreen} />
     </Stack.Navigator>
   );
 };
