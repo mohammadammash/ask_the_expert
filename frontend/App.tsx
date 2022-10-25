74232aaf2b3b9163b84d5aaafe51e20146585b3a
mohammadammash <mohammadammash31@gmail.com>
2022-10-25 17:34:14 +0300

    frontend/App.tsx: Intializing userContext with value and nest all pages within

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,11 +1,14 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { AuthStackNavigator, ExpertDrawerNavigator } from "./src/navigations";
+import { AuthStackNavigator, DrawerNavigator } from "./src/navigations";
+import { useState, createContext } from "react";
+import { UserContext } from "./src/hooks/UserContext";
 
 export default function App() {
+  const [user, setUser] = useState({ name: "hadi", user_type: "novice", imageURL: "/frontend/src/assets/images/dummy_profile.png" });
+
   return (
-    <NavigationContainer>
-      {/* <AuthStackNavigator/> */}
-      <ExpertDrawerNavigator />
-    </NavigationContainer>
+    <UserContext.Provider value={{ user, setUser }}>
+      <NavigationContainer>{user.user_type ? <DrawerNavigator /> : <AuthStackNavigator />}</NavigationContainer>
+    </UserContext.Provider>
   );
 }
