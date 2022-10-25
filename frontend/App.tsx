858eee513557cf55d391bb47ac2b96cffaffa469
mohammadammash <mohammadammash31@gmail.com>
2022-10-25 20:08:33 +0300

    frontend/App.tsx: Adding conditional rendering if user is admin

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,14 +1,16 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { AuthStackNavigator, DrawerNavigator } from "./src/navigations";
+import { AuthStackNavigator, DrawerNavigator, AdminTabNavigator } from "./src/navigations";
 import { useState, createContext } from "react";
 import { UserContext } from "./src/hooks/UserContext";
 
 export default function App() {
-  const [user, setUser] = useState({id:22, name: "hadi", user_type: "novice", imageURL: "/frontend/src/assets/images/dummy_profile.png" });
+  const [user, setUser] = useState({ id: 22, name: "hadi", user_type: "admin", imageURL: "/frontend/src/assets/images/dummy_profile.png" });
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
-      <NavigationContainer>{user.user_type ? <DrawerNavigator /> : <AuthStackNavigator />}</NavigationContainer>
+      <NavigationContainer>{user.user_type === "novice" || user.user_type === "expert" ? <DrawerNavigator /> : user.user_type === "admin" ? <AdminTabNavigator /> : <AuthStackNavigator />}</NavigationContainer>
     </UserContext.Provider>
   );
+}
+{
 }
