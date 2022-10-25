de3129fa1e075bfe2fbb890a594969db0703630f
mohammadammash <mohammadammash31@gmail.com>
2022-10-25 14:52:27 +0300

    frontend/src/navigations/AppointmentsStackNavigator.tsx: Creating Appointments Stack

---
+++ frontend/src/navigations/AppointmentsStackNavigator.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import { View, Text } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { AppointmentsScreen, NoviceProfileScreen, SingleChatScreen } from "../screens";
+import { ROUTES } from "../constants";
+const Stack = createStackNavigator();
+
+const AppointmentsStackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name={ROUTES.USER_APPOINTMENTS} component={AppointmentsScreen} />
+      <Stack.Screen name={ROUTES.NOVICE_PROFILE} component={NoviceProfileScreen} />
+      <Stack.Screen name={ROUTES.USER_SINGLE_CHAT} component={SingleChatScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default AppointmentsStackNavigator;
