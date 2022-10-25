5fe39ed7f81b3b517837436869ff12503a6427f9
mohammadammash <mohammadammash31@gmail.com>
2022-10-25 14:54:27 +0300

    frontend/src/screens/auth/LoginScreen.tsx: Creating LoginScreen with basic content

---
+++ frontend/src/screens/auth/LoginScreen.tsx
@@ -0,0 +1,41 @@
+import { useNavigation } from '@react-navigation/native';
+import { View, Text, Pressable, TextInput, Image } from 'react-native'
+import { ROUTES, IMAGES } from '../../constants';
+import styles from "../../../styles";
+
+const LoginScreen = () => {
+  const navigation = useNavigation();
+  return (
+    <View className="flex-1 items-center bg-white">
+
+      <View className='w-full mb-2 h-1/2'>
+        <Image className='max-w-full max-h-full' source={IMAGES.logo_500px} />
+      </View>
+      
+      <View className="w-4/5 h-60 justify-around">
+        <View>
+          <Text>Email</Text>
+          <TextInput style={styles.text_input} className="placeholder:pl-3" value="" placeholder="Email" />
+        </View>
+        <View>
+          <Text>Password</Text>
+          <TextInput style={styles.text_input} className="placeholder:pl-3" value="" placeholder="Password" />
+        </View>
+        <Pressable style={styles.blue_button_xl} onPress={() => navigation.navigate(ROUTES.REGISTER)}>
+          <Text className="text-xl text-white">LOGIN</Text>
+        </Pressable>
+      </View>
+
+      <View className="flex-row mt-10">
+        <Text className="inline mr-1">Don't have an account?</Text>
+        <Pressable onPress={() => navigation.navigate(ROUTES.REGISTER)}>
+          <Text className="font-bold" style={utilities_styles.orange_text}>
+            Sign up
+          </Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+};
+
+export default LoginScreen;
