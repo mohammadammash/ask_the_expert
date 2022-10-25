07ac79ae72f048db5a2aec2f38e6d7bd04232dd2
mohammadammash <mohammadammash31@gmail.com>
2022-10-25 14:59:39 +0300

    frontend/src/screens/Novice/ProfileScreen.tsx: Creating novice profile basic content

---
+++ frontend/src/screens/Novice/ProfileScreen.tsx
@@ -0,0 +1,26 @@
+import { useNavigation } from "@react-navigation/native";
+import { View, Text, Button, Image } from "react-native";
+import { ROUTES, IMAGES, COLORS } from "../../constants";
+import styles from "../../../styles";
+
+const ProfileScreen = () => {
+  const navigation = useNavigation();
+
+  return (
+    <View className="flex-1 items-center bg-white">
+      <View className="w-full mb-2 h-1/2">
+        <Image className="max-w-full max-h-full" source={IMAGES.logo_500px} />
+      </View>
+
+      <View style={styles.blue_button_lg}>
+        <Button color={COLORS.white} title="Expert Msg Novice" onPress={() => navigation.navigate(ROUTES.USER_SINGLE_CHAT)} />
+      </View>
+
+      <View className="flex-1 items-center justify-center">
+        <Text className="text-slate-800">Novice Profile! 🎉</Text>
+      </View>
+    </View>
+  );
+};
+
+export default ProfileScreen;
