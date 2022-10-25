8ad2faa8ee711e8bc6ceed577d2ae373383385e7
mohammadammash <mohammadammash31@gmail.com>
2022-10-25 20:11:10 +0300

    frontend/src/navigations/index.ts: Exporting admin navigatior

--- frontend/src/navigations/index.ts
+++ frontend/src/navigations/index.ts
@@ -1,6 +1,12 @@
+//COMMON
 export {default as AuthStackNavigator} from "./Common/AuthStackNavigator";
 export {default as DrawerNavigator} from "./Common/DrawerNavigator";
 export {default as ChatsStackNavigator} from "./Common/ChatsStackNavigator";
 export {default as AppointmentsStackNavigator} from "./Common/AppointmentsStackNavigator";
 export {default as ProfileStackNavigator} from "./Common/ProfileStackNavigator";
+
+//NOVICE
 export {default as NoviceHomeStackNavigator} from "./Novice/NoviceHomeStackNavigator";
+
+//ADMIN
+export {default as AdminTabNavigator} from "./Admin/AdminTabNavigator";
