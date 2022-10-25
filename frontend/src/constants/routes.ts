7c1810126fbb8c0acb9bf416f6fb2506c08ca2da
mohammadammash <mohammadammash31@gmail.com>
2022-10-25 14:50:56 +0300

    frontend/src/constants/routes.ts: Requiring routes names files and exporting it

---
+++ frontend/src/constants/routes.ts
@@ -0,0 +1,24 @@
+export default {
+  //AUTH ROUTES
+  LOGIN: 'Login',
+  REGISTER: 'Register',
+
+  //NAVIGATION ROUTES
+  CHATS_STACK: 'Chats_Stack',
+  APPOINTMENTS_STACK: 'Appointments_Stack',
+  PROFILE_STACK: 'Profile_Stack',
+
+  //COMMON ROUTES
+  USER_APPOINTMENTS: 'Appointments',
+  USER_CHATS: 'Chats',
+  USER_SINGLE_CHAT: 'Single_Chat',
+  USER_EDIT_PROFILE: 'Edit_Profile',
+  USER_LEADERBOARD: 'leaderboard',
+
+  //EXPERT ROUTES
+  EXPERT_PROFILE: 'Expert_Profile',
+  EXPERT_GO_ONLINE: 'Expert_Go_Online',
+
+  //NOVICE ROUTES
+  NOVICE_PROFILE: 'Novice_Profile',
+};
