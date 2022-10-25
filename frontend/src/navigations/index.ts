87e35aeee3900a12aab911814294bbf6dd123ae4
mohammadammash <mohammadammash31@gmail.com>
2022-10-25 14:53:35 +0300

    frontend/src/navigations/index.ts: Exporting navigations

---
+++ frontend/src/navigations/index.ts
@@ -0,0 +1,5 @@
+export {default as AuthStackNavigator} from "./AuthStackNavigator";
+export {default as ExpertDrawerNavigator} from "./ExpertDrawerNavigator";
+export {default as ChatsStackNavigator} from "./ChatsStackNavigator";
+export {default as AppointmentsStackNavigator} from "./AppointmentsStackNavigator";
+export {default as ProfileStackNavigator} from "./ProfileStackNavigator";
