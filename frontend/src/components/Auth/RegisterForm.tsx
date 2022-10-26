b09d862cd6c8a4b5e46c699177c25fa84ee79f5c
mohammadammash <mohammadammash31@gmail.com>
2022-10-26 14:11:15 +0300

    frontend/src/components/Auth/RegisterForm.tsx: Adding basic form yup inputs

--- frontend/src/components/Auth/RegisterForm.tsx
+++ frontend/src/components/Auth/RegisterForm.tsx
@@ -1,60 +1,127 @@
-import { View, Text, Pressable, TextInput } from "react-native";
+import { View, Text, Pressable, TextInput, Image } from "react-native";
 import styles from "../../../styles";
-import { ROUTES } from "../../constants";
+import { ROUTES, IMAGES } from "../../constants";
 import { useNavigation } from "@react-navigation/native";
+import { Formik } from "formik";
+import * as Yup from "yup";
+
+interface formValues {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+  profile_base64: string;
+  field: { selectedOption: string };
+  speciality: { selectedOption: string };
+  languages: string;
+  start_date: Date;
+}
 
 const RegisterForm = () => {
   const navigation = useNavigation();
+  const initialValues: formValues = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    profile_base64: "",
+    field: { selectedOption: "Tech" },
+    speciality: { selectedOption: "Software Engineer" },
+    languages: "",
+    start_date: new Date(),
+  };
+
+  const validationSchema = Yup.object().shape({
+    firstName: Yup.string().min(2, "Too Short!").max(50, "Too Long!").required("Required"),
+    lastName: Yup.string().min(2, "Too Short!").max(50, "Too Long!").required("Required"),
+    email: Yup.string().email("Please enter valid email").required("Email is required"),
+    password: Yup.string()
+      .matches(/\w*[a-z]\w*/, "Password must have a small letter")
+      .matches(/\w*[A-Z]\w*/, "Password must have a capital letter")
+      .matches(/\d/, "Password must have a number")
+      .matches(/[!@#$%^&*()\-_"=+{}; :,<.>]/, "Password must have a special character")
+      .required("Password is required"),
+    confirmPassword: Yup.string()
+      .required("Required")
+      .test("password-match", "Password must match", function (value) {
+        return this.parent.password === value;
+      }),
+  });
 
   return (
-    <View className="w-4/5 justify-around gap-2">
-      <View>
-        <Text>Email</Text>
-        <TextInput style={styles.text_input} className="placeholder:pl-3" value="" placeholder="Email" />
-      </View>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={(values, actions) => {
+        console.log({ values, actions });
+        alert(JSON.stringify(values, null, 2));
+      }}
+      validationSchema={validationSchema}
+    >
+      {({ handleChange, handleBlur, handleSubmit, errors, touched, values }) => (
+        <View className="w-4/5 justify-around gap-2">
 
-      <View>
-        <Text>Email</Text>
-        <TextInput style={styles.text_input} className="placeholder:pl-3" value="" placeholder="Email" />
-      </View>
+          <View className="items-center">
+            <View className="border-2 rounded-full h-36 w-36 ">
+              <Image className="rounded-full h-full w-full" source={IMAGES.dummyProfile} />
+            </View>
+          </View>
 
-      <View>
-        <Text>Email</Text>
-        <TextInput style={styles.text_input} className="placeholder:pl-3" value="" placeholder="Email" />
-      </View>
+          <View>
+            <Text className="font-bold">Firstname</Text>
+            <TextInput
+              onChangeText={handleChange("firstName")}
+              onBlur={handleBlur("firstName")}
+              value={values.firstName}
+              style={styles.text_input}
+              className="placeholder:pl-3"
+              placeholder="Firstname"
+            />
+            {errors.firstName && touched.firstName && <Text className="text-red-600">{errors.firstName}</Text>}
+          </View>
 
-      <View>
-        <Text>Email</Text>
-        <TextInput style={styles.text_input} className="placeholder:pl-3" value="" placeholder="Email" />
-      </View>
+          <View className="mt-2">
+            <Text className="font-bold">LastName</Text>
+            <TextInput onChangeText={handleChange("lastName")} onBlur={handleBlur("lastName")} value={values.lastName} style={styles.text_input} className="placeholder:pl-3" placeholder="lastname" />
+            {errors.lastName && touched.lastName && <Text className="text-red-600">{errors.lastName}</Text>}
+          </View>
 
-      <View>
-        <Text>Email</Text>
-        <TextInput style={styles.text_input} className="placeholder:pl-3" value="" placeholder="Email" />
-      </View>
+          <View>
+            <Text className="font-bold">Email</Text>
+            <TextInput onChangeText={handleChange("email")} onBlur={handleBlur("email")} value={values.email} style={styles.text_input} className="placeholder:pl-3" placeholder="Email" />
+            {errors.email && touched.email && <Text className="text-red-600">{errors.email}</Text>}
+          </View>
 
-      <View>
-        <Text>Email</Text>
-        <TextInput style={styles.text_input} className="placeholder:pl-3" value="" placeholder="Email" />
-      </View>
+          <View className="mt-2">
+            <Text className="font-bold">Password</Text>
+            <TextInput onChangeText={handleChange("password")} onBlur={handleBlur("password")} value={values.password} style={styles.text_input} className="placeholder:pl-3" placeholder="Password" />
+            {errors.password && touched.password && <Text className="text-red-600  ">{errors.password}</Text>}
+          </View>
 
-      <View className="mb-3">
-        <Text>Password</Text>
-        <TextInput style={styles.text_input} className="placeholder:pl-3" value="" placeholder="Password" />
-      </View>
+          <View className="mt-2">
+            <Text className="font-bold">Confirm Password</Text>
+            <TextInput
+              onChangeText={handleChange("confirmPassword")}
+              onBlur={handleBlur("ConfirmPassword")}
+              value={values.confirmPassword}
+              style={styles.text_input}
+              className="placeholder:pl-3"
+              placeholder="Renter Password"
+            />
+            {errors.confirmPassword && touched.confirmPassword && <Text className="text-red-600  ">{errors.confirmPassword}</Text>}
+          </View>
 
-      <Pressable className="mb-5" style={styles.blue_button_xl} onPress={() => navigation.navigate(ROUTES.REGISTER)}>
-        <Text className="text-xl text-white">REGISTER</Text>
-      </Pressable>
-    </View>
+          <Pressable className="mt-2" style={styles.blue_button_xl} onPress={handleSubmit}>
+            <Text className="text-xl text-white font-bold">LOGIN</Text>
+          </Pressable>
+        </View>
+      )}
+    </Formik>
   );
 };
 
 export default RegisterForm;
 {
-  /* confirmPassword: Yup.string()
-         .required("Required")
-       .test("password-match", "Password must match", function (value) {
-          return this.parent.password === value;
-         }), */
+  /*  */
 }
