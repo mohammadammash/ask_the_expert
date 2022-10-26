857fa3784bb75f7da1dc57ca58f88482c70eba86
mohammadammash <mohammadammash31@gmail.com>
2022-10-26 17:05:53 +0300

    frontend/src/components/index.ts: Extracting helper function

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,1 +1,3 @@
 export {default as LoginForm} from "./Auth/LoginForm";
+export {default as RegisterForm} from "./Auth/RegisterForm";
+export {validateRegisterFormSchema, registerFormValues, registerInitialValues } from "./helpers/registerHelper";
