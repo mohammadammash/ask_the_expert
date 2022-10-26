b7f4ccb20319c7133453508c9063e13787ecb210
mohammadammash <mohammadammash31@gmail.com>
2022-10-26 11:16:13 +0300

    frontend/src/components/Auth/LoginForm.tsx: Adding yup to validate login inputs

--- frontend/src/components/Auth/LoginForm.tsx
+++ frontend/src/components/Auth/LoginForm.tsx
@@ -4,7 +4,8 @@ import { UserContext } from "../../hooks/UserContext";
 import { useNavigation } from "@react-navigation/native";
 import { ROUTES } from "../../constants";
 import styles from "../../../styles";
-import { Formik, Form, FormikProps } from "formik";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
 interface formValues {
   email: string;
@@ -17,34 +18,45 @@ const LoginForm = () => {
     password: "",
   };
 
-  return (
-    <View className="w-4/5 h-60 justify-around">
-      <Formik
-        initialValues={initialValues}
-        onSubmit={(values, actions) => {
-          console.log({ values, actions });
-          alert(JSON.stringify(values, null, 2));
-        }}
-      >
+  const validationSchema = Yup.object().shape({
+    email: Yup.string().email("Please enter valid email").required("Email is required"),
+    password: Yup.string()
+      .matches(/\w*[a-z]\w*/, "Password must have a small letter")
+      .matches(/\w*[A-Z]\w*/, "Password must have a capital letter")
+      .matches(/\d/, "Password must have a number")
+      .matches(/[!@#$%^&*()\-_"=+{}; :,<.>]/, "Password must have a special character")
+      .required("Password is required"),
+  });
 
-        {({ handleChange, handleBlur, handleSubmit, values }) => (
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={(values, actions) => {
+        console.log({ values, actions });
+        alert(JSON.stringify(values, null, 2));
+      }}
+      validationSchema={validationSchema}
+    >
+      {({ handleChange, handleBlur, handleSubmit, errors, touched, values }) => (
+        <View className="w-4/5 h-64 justify-evenly">
           <View>
-            <View>
-              <Text className="font-bold">Email</Text>
-              <TextInput onChangeText={handleChange("email")} onBlur={handleBlur("email")} value={values.email} style={styles.text_input} className="placeholder:pl-3" placeholder="Email" />
-            </View>
-            <View className="mb-5">
-              <Text className="font-bold">Password</Text>
-              <TextInput onChangeText={handleChange("password")} onBlur={handleBlur("password")} value={values.password} style={styles.text_input} className="placeholder:pl-3" placeholder="Password" />
-            </View>
-            <Pressable style={styles.blue_button_xl} onPress={handleSubmit}>
-              <Text className="text-xl text-white font-bold">LOGIN</Text>
-            </Pressable>
+            <Text className="font-bold">Email</Text>
+            <TextInput onChangeText={handleChange("email")} onBlur={handleBlur("email")} value={values.email} style={styles.text_input} className="placeholder:pl-3" placeholder="Email" />
+            {errors.email && touched.email && <Text className="text-red-600">{errors.email}</Text>}
           </View>
-        )}
-        
-      </Formik>
-    </View>
+
+          <View className="mt-2">
+            <Text className="font-bold">Password</Text>
+            <TextInput onChangeText={handleChange("password")} onBlur={handleBlur("password")} value={values.password} style={styles.text_input} className="placeholder:pl-3" placeholder="Password" />
+            {errors.password && touched.password && <Text className="text-red-600  ">{errors.password}</Text>}
+          </View>
+
+          <Pressable className="mt-2" style={styles.blue_button_xl} onPress={handleSubmit}>
+            <Text className="text-xl text-white font-bold">LOGIN</Text>
+          </Pressable>
+        </View>
+      )}
+    </Formik>
   );
 };
 
