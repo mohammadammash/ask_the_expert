c17578da3ef26d551896f945ab7b51840aeca6fb
mohammadammash <mohammadammash31@gmail.com>
2022-10-26 08:49:01 +0300

    frontend/App.tsx: Replacing user types strings by constants USERTYPES

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -2,13 +2,18 @@ import { NavigationContainer } from "@react-navigation/native";
 import { AuthStackNavigator, DrawerNavigator, AdminTabNavigator } from "./src/navigations";
 import { useState, createContext } from "react";
 import { UserContext } from "./src/hooks/UserContext";
+import { USERTYPES } from "./src/constants";
 
 export default function App() {
   const [user, setUser] = useState({ id: 22, name: "hadi", user_type: "admin", imageURL: "/frontend/src/assets/images/dummy_profile.png" });
 
   return (
     <UserContext.Provider value={{ user, setUser }}>
-      <NavigationContainer>{user.user_type === "novice" || user.user_type === "expert" ? <DrawerNavigator /> : user.user_type === "admin" ? <AdminTabNavigator /> : <AuthStackNavigator />}</NavigationContainer>
+      <NavigationContainer>
+        {user.user_type === USERTYPES.NOVICE || user.user_type === USERTYPES.EXPERT ? <DrawerNavigator /> 
+        : user.user_type === USERTYPES.ADMIN ? <AdminTabNavigator /> 
+        : <AuthStackNavigator />}
+        </NavigationContainer>
     </UserContext.Provider>
   );
 }
