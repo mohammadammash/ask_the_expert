de84550c46898c669306e58fd220d1802f54476e
mohammadammash <mohammadammash31@gmail.com>
2022-10-26 08:48:34 +0300

    frontend/src/navigations/Common/index.ts: Adding Exporting file for common

---
+++ frontend/src/navigations/Common/index.ts
@@ -0,0 +1,3 @@
+export {default as ChatsStackNavigator} from "./ChatsStackNavigator";
+export {default as AppointmentsStackNavigator} from "./AppointmentsStackNavigator";
+export {default as ProfileStackNavigator} from "./ProfileStackNavigator";
