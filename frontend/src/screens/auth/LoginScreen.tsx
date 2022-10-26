44a6dca0d31c59d7ffd9db1377c126a1fae22bb5
mohammadammash <mohammadammash31@gmail.com>
2022-10-26 09:07:59 +0300

    frontend/src/screens/auth/LoginScreen.tsx: Importing loginForm component

--- frontend/src/screens/auth/LoginScreen.tsx
+++ frontend/src/screens/auth/LoginScreen.tsx
@@ -1,13 +1,11 @@
-import { useNavigation } from "@react-navigation/native";
 import { View, Text, Pressable, TextInput, Image } from "react-native";
-import { useContext } from "react";
+import { useNavigation } from "@react-navigation/native";
 import { ROUTES, IMAGES } from "../../constants";
-import { UserContext } from "../../hooks/UserContext";
+import { LoginForm } from "../../components";
 import styles from "../../../styles";
 
 const LoginScreen = () => {
   const navigation = useNavigation();
-  const { user, setUser } = useContext(UserContext);
 
   return (
     <View className="flex-1 items-center bg-white">
@@ -15,22 +13,10 @@ const LoginScreen = () => {
         <Image className="max-w-full max-h-full" source={IMAGES.logo_500px} />
       </View>
 
-      <View className="w-4/5 h-60 justify-around">
-        <View>
-          <Text>Email</Text>
-          <TextInput style={styles.text_input} className="placeholder:pl-3" value="" placeholder="Email" />
-        </View>
-        <View>
-          <Text>Password</Text>
-          <TextInput style={styles.text_input} className="placeholder:pl-3" value="" placeholder="Password" />
-        </View>
-        <Pressable style={styles.blue_button_xl} onPress={() => navigation.navigate(ROUTES.REGISTER)}>
-          <Text className="text-xl text-white">LOGIN</Text>
-        </Pressable>
-      </View>
+      <LoginForm />
 
       <View className="flex-row mt-10">
-        <Text className="inline mr-1">Don't have an account?</Text>
+        <Text className="inline mr-1 font-bold">Don't have an account?</Text>
         <Pressable onPress={() => navigation.navigate(ROUTES.REGISTER)}>
           <Text className="font-bold" style={styles.orange_text}>
             Sign up
