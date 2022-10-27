dd75b2e68515362e0f52be64f166b263b7d32f9c
mohammadammash <mohammadammash31@gmail.com>
2022-10-27 23:16:29 +0300

    frontend/src/components/Common/ProfilePersonalInfo.tsx: Adding calculateYearsOfExp helper to return date diff as a string

--- frontend/src/components/Common/ProfilePersonalInfo.tsx
+++ frontend/src/components/Common/ProfilePersonalInfo.tsx
@@ -1,12 +1,28 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import { UserContext } from "../../hooks/UserContext";
+import { useContext } from "react";
+//internal imports
+import CalculateYearsOfExperienceHelper from "./helpers/calculateYearsOfExperienceHelper";
 
 const ProfilePersonalInfo = () => {
-  return (
-    <View>
-      <Text>ProfilePersonalInfo</Text>
-    </View>
-  )
-}
+  const { user, setUser } = useContext(UserContext);
+  const { firstName, lastName, field, start_date, about, speciality } = user;
+  const yearsOfExp = CalculateYearsOfExperienceHelper(start_date);
 
-export default ProfilePersonalInfo
+  return (
+    <View className="border-b w-full items-center pt-5 justify-start gap-3 h-1/4">
+      <Text>
+        {firstName} {lastName}
+      </Text>
+      <Text>{field}</Text>
+      <Text>{speciality}</Text>
+      <Text>WITH</Text>
+      <Text>
+        {yearsOfExp} of Experience
+      </Text>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default ProfilePersonalInfo;
