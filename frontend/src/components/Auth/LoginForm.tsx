77c29223156a57a0bf066efdcd9c802c1f48ab2c
mohammadammash <mohammadammash31@gmail.com>
2022-10-27 13:27:30 +0300

    frontend/src/components/Auth/LoginForm.tsx: Adding firebase to signInUserWithEmail

--- frontend/src/components/Auth/LoginForm.tsx
+++ frontend/src/components/Auth/LoginForm.tsx
@@ -1,22 +1,47 @@
 import { View, Text, TextInput, Pressable } from "react-native";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../../hooks/UserContext";
 import styles from "../../../styles";
 import { Formik } from "formik";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../../firebaseConfig";
 
 import { validateLoginFormSchema, loginIntialValues } from "./helpers/loginFormHelper";
 
 const LoginForm = () => {
+  const [inValidCredentials, setInValidCredentials] = useState(false);
+
+  const firbaseConfirmLogin = (email: string, password: string) => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log("___SUCESSS________");
+        console.log("___SUCESSS________");
+        console.log(user);
+        console.log("___SUCESSS________");
+        console.log("___SUCESSS________");
+        alert(user);
+        // ...
+      })
+      .catch((error) => {
+        alert(error.code);
+        alert("Invalid Credentials");
+        setInValidCredentials(true);
+      });
+  };
+
   return (
     <Formik
       initialValues={loginIntialValues}
       onSubmit={(values, actions) => {
-        console.log({ values, actions });
-        alert(JSON.stringify(values, null, 2));
+        setInValidCredentials(false);
+        firbaseConfirmLogin(values.email, values.password);
+        // console.log({ values, actions });
+        // alert(JSON.stringify(values, null, 2));
       }}
       validationSchema={validateLoginFormSchema}
     >
-
       {({ handleChange, handleBlur, handleSubmit, errors, touched, values }) => (
         <View className="w-4/5 h-64 justify-evenly">
           <View>
@@ -34,6 +59,7 @@ const LoginForm = () => {
           <Pressable className="mt-2" style={styles.blue_button_xl} onPress={handleSubmit}>
             <Text className="text-xl text-white font-bold">LOGIN</Text>
           </Pressable>
+          {inValidCredentials && <Text className="text-red-600 text-center">InValid Credentials</Text>}
         </View>
       )}
     </Formik>
