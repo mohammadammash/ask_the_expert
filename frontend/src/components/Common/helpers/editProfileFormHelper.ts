d0188c0d99caa51697000b63292809ebede31af8
mohammadammash <mohammadammash31@gmail.com>
2022-10-28 19:29:38 +0300

    frontend/src/components/Common/helpers/editProfileFormHelper.ts: Adding edit profile form helper to handle types and validation for form

---
+++ frontend/src/components/Common/helpers/editProfileFormHelper.ts
@@ -0,0 +1,30 @@
+import * as Yup from "yup";
+
+export interface editProfileFormValues {
+  firstName: string;
+  lastName: string;
+  about: string;
+  speciality: string;
+  profile_url: string,
+  languages: string[];
+  language: string,
+  theme: string,
+}
+
+export const registerInitialValues: editProfileFormValues = {
+    firstName: "",
+    lastName: "",
+    about: "",
+    profile_url: "",
+    languages: [],
+    speciality: "Software developer",
+    language: '',
+    theme: '',
+  };
+
+export const validateEditProfileFormSchema = Yup.object().shape({
+    firstName: Yup.string().min(2, "Too Short!").max(50, "Too Long!").required("Required"),
+    lastName: Yup.string().min(2, "Too Short!").max(50, "Too Long!").required("Required"),
+    about: Yup.string().min(20, 'Min 20 Characters').max(250, 'Too Long').required('Required'),
+    languages: Yup.array().min(1, 'At least one language').required('Required')
+  });
