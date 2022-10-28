5a897cafcb9f99d32f677d6cb3e85e49bd86b208
mohammadammash <mohammadammash31@gmail.com>
2022-10-28 21:48:02 +0300

    frontend/src/components/Common/ProfileScreen: Creating new Folder to hold all Profile Screen components

--- frontend/src/components/Common/ProfileScreen/ProfileImageCard.tsx
+++ frontend/src/components/Common/ProfileScreen/ProfileImageCard.tsx
@@ -1,6 +1,6 @@
 import { View, Image, Switch } from "react-native";
-import { IMAGES, USERTYPES, ROUTES, COLORS } from "../../constants";
-import { UserContext } from "../../hooks/UserContext";
+import { IMAGES, USERTYPES, ROUTES, COLORS } from "../../../constants";
+import { UserContext } from "../../../hooks/UserContext";
 import { useContext, useState } from "react";
 
 import { useNavigation } from "@react-navigation/native";
@@ -8,10 +8,9 @@ import { useNavigation } from "@react-navigation/native";
 const ProfileImageCard = () => {
   const { user, setUser } = useContext(UserContext);
 
-
   return (
     <View className="h-1/4 w-full items-center justify-center">
-      <View className="avatar aspect-square rounded-full items-center w-2/3 my-2 h-3/4 border-2">
+      <View style={{ borderColor: COLORS.blue }} className="avatar aspect-square rounded-full items-center w-2/3 my-2 h-3/4 border-8">
         <Image className="max-w-full max-h-full h-full w-full rounded-full" source={IMAGES.dummyProfile} />
       </View>
     </View>
