6f0db9b1169c29e8138cb6b115d6b2cf7ce67e29
mohammadammash <mohammadammash31@gmail.com>
2022-10-28 20:08:39 +0300

    frontend/src/components/Common/helpers/editProfileFormHelper.ts: Adding language with default english and as a string

--- frontend/src/components/Common/helpers/editProfileFormHelper.ts
+++ frontend/src/components/Common/helpers/editProfileFormHelper.ts
@@ -11,12 +11,12 @@ export interface editProfileFormValues {
   theme: string,
 }
 
-export const registerInitialValues: editProfileFormValues = {
+export const editProfileInitialValues: editProfileFormValues = {
     firstName: "",
     lastName: "",
     about: "",
     profile_url: "",
-    languages: [],
+    languages: [], //spoken
     speciality: "Software developer",
     language: '',
     theme: '',
@@ -25,6 +25,6 @@ export const registerInitialValues: editProfileFormValues = {
 export const validateEditProfileFormSchema = Yup.object().shape({
     firstName: Yup.string().min(2, "Too Short!").max(50, "Too Long!").required("Required"),
     lastName: Yup.string().min(2, "Too Short!").max(50, "Too Long!").required("Required"),
-    about: Yup.string().min(20, 'Min 20 Characters').max(250, 'Too Long').required('Required'),
+    about: Yup.string().min(20, "Min 20 Characters").max(250, "Too Long").required('Required'),
     languages: Yup.array().min(1, 'At least one language').required('Required')
   });
