e927d8a06c72f6f75f35b6b09b6b5ae684664bbe
mohammadammash <mohammadammash31@gmail.com>
2022-10-28 21:45:11 +0300

    frontend/src/screens/Novice/ProfileScreen.tsx: Adding needed componet links

--- frontend/src/screens/Novice/ProfileScreen.tsx
+++ frontend/src/screens/Novice/ProfileScreen.tsx
@@ -1,40 +1,31 @@
 import { useNavigation } from "@react-navigation/native";
 import { View, Text, Button, Image } from "react-native";
-import { ROUTES, IMAGES, COLORS, USERTYPES } from "../../constants";
-import styles from "../../../styles";
 import { UserContext } from "../../hooks/UserContext";
 import { useContext } from "react";
+//internal imports:
+import { ROUTES, IMAGES, COLORS, USERTYPES } from "../../constants";
+import styles from "../../../styles";
+import { ProfilePersonalInfoComponent, ProfileImageCardComponent } from "../../components";
 
 const ProfileScreen = () => {
-  const navigation = useNavigation();
+  const navigation = useNavigation<any>();
   const { user, setUser } = useContext(UserContext);
   const shownuser_id = 22; //user_id
   const { id, user_type } = user;
 
   return (
     <View className="flex-1 items-center bg-white">
-      <View className="w-full mb-2 h-1/2">
-        <Image className="max-w-full max-h-full" source={IMAGES.logo_500px} />
-      </View>
+      <ProfileImageCardComponent />
 
-      {/* EXPERT VISITING NOVICE PROFILE */}
-      {user_type === USERTYPES.EXPERT && (
-        <>
-          <View style={styles.blue_button_lg}>
-            <Button color={COLORS.white} title="Expert Send Msg to Novice" onPress={() => navigation.navigate(ROUTES.USER_SINGLE_CHAT)} />
-          </View>
-          <View className="mt-5" style={styles.blue_button_lg}>
-            <Button color={COLORS.white} title="Block" onPress={() => navigation.navigate(ROUTES.USER_SINGLE_CHAT)} />
-          </View>
-        </>
-      )}
+      <ProfilePersonalInfoComponent />
+      
 
       {/* NOVICE VISITING HIS OWN PROFILE */}
-      {user_type === USERTYPES.NOVICE && id === shownuser_id && (
+      {/* {user_type === USERTYPES.NOVICE && id === shownuser_id && (
         <View className="mt-5" style={styles.blue_button_lg}>
           <Button color={COLORS.white} title="Edit Profile" onPress={() => navigation.navigate(ROUTES.USER_EDIT_PROFILE)} />
         </View>
-      )}
+      )} */}
 
       <View className="flex-1 items-center justify-center">
         <Text className="text-slate-800">Novice Profile! 🎉</Text>
