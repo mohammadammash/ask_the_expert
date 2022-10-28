958886235ca3d8ce869e6b305bd5de7cf0d856b9
mohammadammash <mohammadammash31@gmail.com>
2022-10-28 16:37:14 +0300

    frontend/src/screens/Expert/GoOnlineScreen.tsx: Adding tsx content and designing the page

--- frontend/src/screens/Expert/GoOnlineScreen.tsx
+++ frontend/src/screens/Expert/GoOnlineScreen.tsx
@@ -1,19 +1,20 @@
 import { useNavigation } from "@react-navigation/native";
 import { View, Text} from "react-native";
+import styles from "../../../styles";
 import { ConfirmAvailabilityFormCard } from "../../components";
+import { COLORS } from "../../constants";
 
 const ProfileScreen = () => {
 
   return (
-    <View className="items-center bg-white border">
+    <View className="flex-1 items-center justify-around bg-white border">
       <Text className="text-slate-800 text-xl text-center w-5/6">The World is waiting for your Touch Go Online?</Text>
-      <View></View>
+      <Text className="text-center w-3/4 text-xs color-[#828282]">Choose the time you will be available by, and the time of each meeting</Text>
 
       {/* CONFIRM AVAILBILITY FORM */}
-      <View>
-        <ConfirmAvailabilityFormCard/>
+      <View className="w-full items-center justify-around">
+        <ConfirmAvailabilityFormCard />
       </View>
-
     </View>
   );
 };
