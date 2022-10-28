a0a65f7ef4ff1207111a4c04504a007f52b34634
mohammadammash <mohammadammash31@gmail.com>
2022-10-28 15:33:24 +0300

    frontend/src/screens/Expert/GoOnlineScreen.tsx: Adding main components of GoOnline Screen

--- frontend/src/screens/Expert/GoOnlineScreen.tsx
+++ frontend/src/screens/Expert/GoOnlineScreen.tsx
@@ -4,13 +4,17 @@ import { ROUTES, IMAGES } from "../../constants";
 import styles from "../../../styles";
 
 const ProfileScreen = () => {
-  const navigation = useNavigation();
+  const navigation = useNavigation<any>();
 
   return (
-    <View className="flex-1 items-center bg-white">
+    <View className="items-center bg-white border">
+      <Text className="text-slate-800 text-xl text-center w-5/6">The World is waiting for your Touch Go Online?</Text>
+      <View></View>
 
-      <View className="flex-1 items-center justify-center">
-        <Text className="text-slate-800">GO online expert! 🎉</Text>
+      <View>
+        <Pressable className="my-2" style={styles.blue_button_lg} onPress={() => navigation.navigate(ROUTES.EXPERT_PROFILE)}>
+          <Text className="text-white font-bold text-base">CONFIRM</Text>
+        </Pressable>
       </View>
     </View>
   );
