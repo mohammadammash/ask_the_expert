7c558a997d06aa6bc22e553435d1b235422f9048
mohammadammash <mohammadammash31@gmail.com>
2022-10-28 12:28:56 +0300

    frontend: Installing react-native-progress

--- frontend/app.d.ts
+++ frontend/app.d.ts
@@ -1,2 +1,3 @@
 /// <reference types="nativewind/types" />
-declare module 'react-native-image-base64'
+declare module 'react-native-image-base64';
+declare module 'react-native-progress/Bar'
--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -2,6 +2,7 @@
 export {default as LoginFormComponent} from "./Auth/LoginForm";
 export {default as RegisterFormComponent} from "./Auth/RegisterForm";
 
-//COMMON
-export {default as ProfileImageCardComponent} from "./Common/ProfileImageCard";
-export {default as ProfilePersonalInfoComponent} from "./Common/ProfilePersonalInfo";
+//EXPERT
+export {default as ProfileImageCardComponent} from "./Expert/ProfileImageCard";
+export {default as ProfilePersonalInfoComponent} from "./Expert/ProfilePersonalInfo";
+export {default as AllReviewsStatsComponent} from "./Expert/AllReviewsStats";
