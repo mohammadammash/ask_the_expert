4dbc1c6e234d3ee5daaac81b5d9b30287c9e8a04
mohammadammash <mohammadammash31@gmail.com>
2022-10-29 13:47:23 +0300

    frontend/src/screens/Admin/ProfileScreen.tsx: Adding personal info with buttons component

--- frontend/src/screens/Admin/ProfileScreen.tsx
+++ frontend/src/screens/Admin/ProfileScreen.tsx
@@ -1,33 +1,38 @@
-import { useNavigation } from "@react-navigation/native";
 import { View, Text } from "react-native";
 import { UserContext } from "../../hooks/UserContext";
 import { useContext } from "react";
-import { Entypo } from "@expo/vector-icons"; 
+import { Entypo } from "@expo/vector-icons";
 //internal imports:
-import { ProfileImageCardComponent } from "../../components";
-import { ROUTES, COLORS} from "../../constants";
+import { ProfileImageCardComponent, ButtonsComponent } from "../../components";
+import { COLORS } from "../../constants";
 
 const ProfileScreen = () => {
-  const navigation = useNavigation<any>();
   const { user, setUser } = useContext(UserContext);
-  const {firstName, lastName, user_type, email} = user;
-  const shownuser_id = 22; //user_id
+  const { firstName, lastName, email } = user;
 
   return (
     <View className="flex-1 items-center bg-white">
       <ProfileImageCardComponent />
-        <View className="w-full items-center justify-start gap-2">
-          {/* PERSONAL INFO */}
-          <View className="mb-2">
-            <Text style={{ color: COLORS.blue }} className="font-bold text-2xl">
-              {firstName} {lastName}
-            </Text>
-            <Text className="opacity-60 text-sm text-center">{email}</Text>
-          </View>
+      <View className="w-full items-center justify-start gap-6">
+        {/* PERSONAL INFO */}
+        <View className="mb-2">
+          <Text style={{ color: COLORS.blue }} className="font-bold text-2xl">
+            {firstName} {lastName}
+          </Text>
+          <Text className="opacity-60 text-sm text-center">{email}</Text>
+        </View>
+
+        <View className="items-center gap-2">
           <Entypo name="shield" size={50} color="black" />
           <Text style={{ color: COLORS.dark }} className="text-base font-bold">
             ADMINSTRATOR
           </Text>
+        </View>
+
+        <View>
+          <ButtonsComponent />
+        </View>
+
       </View>
     </View>
   );
