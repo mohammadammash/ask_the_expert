de9c5ccd4380fb94e312e8285240e8ad5b395cc1
mohammadammash <mohammadammash31@gmail.com>
2022-10-29 21:11:09 +0300

    frontend/src/screens/Admin/HomeScreen.tsx: Adding chart user to the admin home and installing gifted charts

--- frontend/src/screens/Admin/HomeScreen.tsx
+++ frontend/src/screens/Admin/HomeScreen.tsx
@@ -1,22 +1,15 @@
 import { View, Text, ScrollView } from "react-native";
-import React from "react";
+import AllUsersCountChart from "../../components/Admin/Charts/AllUsersStatistics";
 
 const HomeScreen = () => {
   return (
-    <View className="bg-white items-center justify-evenly flex-1">
-      <View className="h-2/5">
+    <View className="bg-white items-center justify-evenly flex-1 gap-1">
+      <View className="h-5/6">
         <ScrollView horizontal={true} className="gap-2">
-          <View className="rounded-lg border-2 items-center w-80 bg-slate-100 bg-opacity-70">
-          </View>
+            <AllUsersCountChart/>
         </ScrollView>
       </View>
 
-      <View className="h-2/5">
-        <ScrollView horizontal={true} className="gap-2">
-          <View className="rounded-lg border-2 items-center w-80 bg-slate-100 bg-opacity-70">
-          </View>
-        </ScrollView>
-      </View>
     </View>
   );
 };
