54f4278287dd4324e3ac51e99c5c9646fb3e10c5
mohammadammash <mohammadammash31@gmail.com>
2022-10-29 17:26:49 +0300

    frontend/src/screens/Admin/ViewUsersScreen.tsx: Adding User Cards section with sample data

--- frontend/src/screens/Admin/ViewUsersScreen.tsx
+++ frontend/src/screens/Admin/ViewUsersScreen.tsx
@@ -10,6 +10,12 @@ const ViewUsersScreen = () => {
   return (
     <View className="flex-1 items-center justify-between bg-white">
       <FilterPageTitleComponent userType={shownUsersType} options={USERS_TYPES_OPTIONS} set_userType={setShownUsersType} />
+
+      <ScrollView className="h-5/6 pt-5" horizontal={true}>
+        <UserCardComponent />
+        <UserCardComponent />
+        <UserCardComponent />
+      </ScrollView>
     </View>
   );
 };
