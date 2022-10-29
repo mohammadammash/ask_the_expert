7dbdc5bc2cde3b5a725950ec76b94abe881c7229
mohammadammash <mohammadammash31@gmail.com>
2022-10-29 13:13:08 +0300

    frontend/src/constants/assets.ts: Adding new icon and exporting it to use in components

--- frontend/src/constants/assets.ts
+++ frontend/src/constants/assets.ts
@@ -1,4 +1,5 @@
 export default {
   dummyProfile: require('../assets/images/dummy_profile.png'),
   logo_500px: require('../assets/icons/logo_500.png'),
+  topbadge: require('../assets/icons/leaderboard_topbadge.png'),
 };
