9986c6ec40c843fd3263ad3cd7d63ec1fd3f4877
mohammadammash <mohammadammash31@gmail.com>
2022-10-29 09:49:26 +0300

    frontend/src/components/Common/ChatAndAppointmentCard.tsx: Adding tsx content and layout of card

--- frontend/src/components/Common/ChatAndAppointmentCard.tsx
+++ frontend/src/components/Common/ChatAndAppointmentCard.tsx
@@ -1,12 +1,33 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+//internal imports
+import styles from "../../../styles";
+import { ROUTES, IMAGES } from "../../constants";
 
 const ChatAndAppointmentCard = () => {
-  return (
-    <View>
-      <Text>ChatAndAppointmentCard</Text>
-    </View>
-  )
-}
+  const navigation = useNavigation<any>();
 
-export default ChatAndAppointmentCard
+  return (
+    <TouchableOpacity
+      style={styles.bg_grey_opacity30}
+      onPress={() => navigation.navigate(ROUTES.NOVICE_PROFILE)}
+    >
+      <View>
+        <Image source={IMAGES.dummyProfile} />
+      </View>
+
+      <View>
+        <View>
+          <Text>Mohammad Ammash</Text>
+          <Text>Senior Web Developer</Text>
+        </View>
+        <View>
+          <Text>Last msg 10chars..</Text>
+          <Text>1d</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default ChatAndAppointmentCard;
