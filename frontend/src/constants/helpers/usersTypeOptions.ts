f717c7eae53cd0fafb63fb80fff717086f01e499
mohammadammash <mohammadammash31@gmail.com>
2022-10-29 14:37:32 +0300

    frontend/src/constants/index.ts: Adding and extracting constant user types for dropdown

---
+++ frontend/src/constants/helpers/usersTypeOptions.ts
@@ -0,0 +1,7 @@
+const users_shown_options = [
+    { label: "All", value: "users" },
+    { label: "Novices", value: "novices" },
+    { label: "Experts", value: "experts" },
+];
+
+export default users_shown_options;
--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -10,5 +10,7 @@ const USERTYPES = {
 import ALLANGUAGES from './helpers/allLanguages';
 import ALLJOBSFIELDS from './helpers/allJobsFields';
 import ALLJOBSSPECIALTIES from './helpers/allJobsSpecialties';
+import {session_time_options as AVAILABILITY_SESSION_OPTIONS, availability_options as AVAILABILITY_OPTIONS} from "./helpers/availablityTimeOptions";
+import USERS_TYPES_OPTIONS from "./helpers/usersTypeOptions";
 
-export {ROUTES, COLORS, IMAGES, USERTYPES, ALLANGUAGES, ALLJOBSFIELDS,ALLJOBSSPECIALTIES};
+export {ROUTES, COLORS, IMAGES, USERTYPES, ALLANGUAGES, ALLJOBSFIELDS,ALLJOBSSPECIALTIES, AVAILABILITY_OPTIONS, AVAILABILITY_SESSION_OPTIONS, USERS_TYPES_OPTIONS};
