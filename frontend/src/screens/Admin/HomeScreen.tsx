5872575c0c9e9a5c10497e8ada76ef71131fc1b5
mohammadammash <mohammadammash31@gmail.com>
2022-10-29 17:50:16 +0300

    frontend/src/screens/Admin/HomeScreen.tsx: Designing the layout of the admin home screen

--- frontend/src/screens/Admin/HomeScreen.tsx
+++ frontend/src/screens/Admin/HomeScreen.tsx
@@ -1,12 +1,24 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView } from "react-native";
+import React from "react";
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen Admin</Text>
-    </View>
-  )
-}
+    <View className="bg-white items-center justify-evenly flex-1">
+      <View className="h-2/5">
+        <ScrollView horizontal={true} className="gap-2">
+          <View className="rounded-lg border-2 items-center w-80 bg-slate-100 bg-opacity-70">
+          </View>
+        </ScrollView>
+      </View>
 
-export default HomeScreen
+      <View className="h-2/5">
+        <ScrollView horizontal={true} className="gap-2">
+          <View className="rounded-lg border-2 items-center w-80 bg-slate-100 bg-opacity-70">
+          </View>
+        </ScrollView>
+      </View>
+    </View>
+  );
+};
+
+export default HomeScreen;
