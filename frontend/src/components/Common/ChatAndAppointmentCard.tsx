f55138617092a0e3566c8ae497d74396c6f5a8a0
mohammadammash <mohammadammash31@gmail.com>
2022-10-29 10:24:45 +0300

    frontend/src/screens/Common/AppointmentsScreen.tsx: Altering component to match styling of appointment card

--- frontend/src/components/Common/ChatAndAppointmentCard.tsx
+++ frontend/src/components/Common/ChatAndAppointmentCard.tsx
@@ -1,33 +1,55 @@
 import { View, Text, TouchableOpacity, Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { MaterialCommunityIcons, Entypo } from "@expo/vector-icons";
 //internal imports
 import styles from "../../../styles";
 import { ROUTES, IMAGES } from "../../constants";
 
 const ChatAndAppointmentCard = () => {
   const navigation = useNavigation<any>();
+  const NavigateToSingleChat = ()=>{
+    // CHATS PAGE NAVIGATION
+    navigation.navigate(ROUTES.USER_SINGLE_CHAT);
+  }
 
   return (
-      <TouchableOpacity
-        style={styles.bg_grey_opacity30}
-        className="flex-row w-5/6 rounded-lg border-0.5 items-center justify-around h-24 mb-5"
-        onPress={() => navigation.navigate(ROUTES.USER_SINGLE_CHAT)}
-      >
-        <View className="avatar aspect-square max-w-1/5 max-h-1/5 h-2/5 w-1/5 rounded-full items-center border-2 border-[#1FA6D1]">
-          <Image className="max-w-full max-h-full h-full w-full rounded-full" source={IMAGES.dummyProfile} />
+    // CHATS TOUCHABLE OPACITY
+    // <TouchableOpacity
+    //   style={styles.bg_grey_opacity30}
+    //   className="flex-row w-5/6 rounded-lg border-0.5 items-center justify-around h-24 mb-5"
+    //   onPress={NavigateToSingleChat}
+    // >
+
+    <View className="flex-row w-5/6 rounded-lg border-0.5 items-center justify-around h-24 mb-5">
+      <View className="avatar aspect-square max-w-1/5 max-h-1/5 h-2/5 w-1/5 rounded-full items-center border-2 border-[#1FA6D1]">
+        <Image className="max-w-full max-h-full h-full w-full rounded-full" source={IMAGES.dummyProfile} />
+      </View>
+
+      <View className="h-full justify-around">
+        <View>
+          <Text className="text-sm font-bold">Mohammad Ammash</Text>
+          <Text className="text-xs opacity-50">Senior Web Developer</Text>
         </View>
 
-        <View className="h-full justify-around">
-          <View>
-            <Text className="text-sm font-bold">Mohammad Ammash</Text>
-            <Text className="text-xs opacity-50">Senior Web Developer</Text>
-          </View>
-          <View className="flex-row items-center justify-between w-52 ">
+        {/* CHATS CARD: */}
+        {/* <View className="flex-row items-center justify-between w-52 ">
             <Text className="text-xs opacity-80">Last msg 10chars..</Text>
             <Text className="text-[9px] opacity-80">1d</Text>
+          </View> */}
+
+        {/* APPOINTMENTS CARD: */}
+        <View className="flex-row items-center justify-between w-52 ">
+          <View className="opacity-80 flex-row gap-2">
+            <MaterialCommunityIcons onPress={() => alert("Removed")} name="book-cancel" size={24} color="black" />
+            <Entypo name="new-message" size={24} color="black" onPress={NavigateToSingleChat}/>
+          </View>
+          <View className="gap-1">
+            <Text className="text-[9px] opacity-80">12:15pm</Text>
+            <Text className="text-[9px] opacity-80">12:30pm</Text>
           </View>
         </View>
-      </TouchableOpacity>
+      </View>
+    </View>
   );
 };
 
