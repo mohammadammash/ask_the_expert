d42e7ebef7b24de6d09cdba0faa5711b22ecaaae
mohammadammash <mohammadammash31@gmail.com>
2022-10-29 11:34:28 +0300

    frontend/src/components/Common/SendMessageForm.tsx: Adding botom input and handling input value

--- frontend/src/components/Common/SendMessageForm.tsx
+++ frontend/src/components/Common/SendMessageForm.tsx
@@ -1,11 +1,25 @@
-import { View, Text } from 'react-native'
+import { View, TextInput } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+import { useState } from "react";
+//internal imports
+import { COLORS } from "../../constants";
+import styles from "../../../styles";
 
 const SendMessageForm = () => {
-  return (
-    <View>
-      <Text>SendMessageForm</Text>
-    </View>
-  )
-}
+  const [message, onChangeMessage] = useState("");
+  const submitMessage = () => {
+    if(!message) alert('Cannot be empty!');
+    else alert(message);
+  };
 
-export default SendMessageForm
+  return (
+    <View style={{ backgroundColor: COLORS.grey }} className="flex-row h-20 items-center justify-around">
+      <TextInput value={message} onChangeText={onChangeMessage} style={[styles.text_input, styles.send_message_input]} className="ml-1 w-5/6 placeholder:pl-3" placeholder="Write your message" />
+      <View className="mr-3">
+        <FontAwesome name="send" size={24} color={COLORS.blue} onPress={submitMessage} />
+      </View>
+    </View>
+  );
+};
+
+export default SendMessageForm;
