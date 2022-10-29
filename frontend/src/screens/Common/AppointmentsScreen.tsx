1612f39341977ef2a4dc59c330cba62ce0deb1fa
mohammadammash <mohammadammash31@gmail.com>
2022-10-29 10:24:01 +0300

    frontend/src/screens/Common/AppointmentsScreen.tsx: Adding appointment card component

--- frontend/src/screens/Common/AppointmentsScreen.tsx
+++ frontend/src/screens/Common/AppointmentsScreen.tsx
@@ -1,19 +1,16 @@
-import { useNavigation } from "@react-navigation/native";
-import { View, Text, Button, Image } from "react-native";
-import { ROUTES, IMAGES, COLORS } from "../../constants";
-import styles from "../../../styles";
+import { View } from "react-native";
+//internal imports
+import { ChatAndAppointmentCardComponent } from "../../components";
+import { ScrollView } from "react-native-gesture-handler";
 
 const AppointmentsScreen = () => {
-  const navigation = useNavigation();
 
   return (
     <View className="flex-1 items-center bg-white">
-      <View className="items-center justify-center h-1/2">
-        <Text className="text-slate-800">My Appointments! 🎉</Text>
-      </View>
-      <View style={styles.blue_button_lg}>
-        <Button color={COLORS.white} title="Novice Profile" onPress={() => navigation.navigate(ROUTES.NOVICE_PROFILE)} />
-      </View>
+      <ScrollView className="w-full  mt-10" contentContainerStyle={{ alignItems: "center" }}>
+
+        <ChatAndAppointmentCardComponent />
+      </ScrollView>
     </View>
   );
 };
