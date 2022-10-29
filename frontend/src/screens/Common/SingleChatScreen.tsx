ea917743b0a0f037b888c209d1c5274a1f69f5d8
mohammadammash <mohammadammash31@gmail.com>
2022-10-29 10:56:44 +0300

    frontend/src/screens/Common/SingleChatScreen.tsx: Adding left chat bubble with styling

--- frontend/src/screens/Common/SingleChatScreen.tsx
+++ frontend/src/screens/Common/SingleChatScreen.tsx
@@ -1,6 +1,6 @@
 import { useNavigation } from "@react-navigation/native";
 import { View, Text, Pressable, TextInput, Image } from "react-native";
-import { ROUTES, IMAGES } from "../../constants";
+import { ROUTES, IMAGES, COLORS } from "../../constants";
 import styles from "../../../styles";
 
 const SingleChatScreen = () => {
@@ -8,12 +8,15 @@ const SingleChatScreen = () => {
 
   return (
     <View className="flex-1 items-center bg-white">
-      <View className="w-full mb-2 h-1/2">
-        <Image className="max-w-full max-h-full" source={IMAGES.logo_500px} />
-      </View>
+      <View className="w-full pt-3">
+        {/* LEFT BUBBLE */}
+        <View style={{ backgroundColor: COLORS.blue }} className="ml-2 min-w-1/2 w-3/5 max-w-3/4 min-h-8 p-2 rounded-2xl rounded-tl-none mb-2 flex-row">
+          <Text className="w-5/6 text-white">
+            Lorem psum dolor sit amet consectetur adipisicing elit. Earum iusto perspiciatis nisi placeat dignissimos temporibus saepe vero. Rerum, recusandae harum.
+          </Text>
+          <Text className="w-1/6 text-center text-[10px] opacity-75 text-white">11:59</Text>
+        </View>
 
-      <View className="flex-1 items-center justify-center">
-        <Text className="text-slate-800">One Single Chat! 🎉</Text>
       </View>
     </View>
   );
