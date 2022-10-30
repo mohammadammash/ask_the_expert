76f778902b80ddf1bcbb2bf2d5d193cd40e805f3
mohammadammash <mohammadammash31@gmail.com>
2022-10-30 17:43:05 +0200

    frontend/src/navigations/Novice/NoviceHomeStackNavigator.tsx: Adding Single Chat screen navigation to the Stack

--- frontend/src/navigations/Novice/NoviceHomeStackNavigator.tsx
+++ frontend/src/navigations/Novice/NoviceHomeStackNavigator.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import { ExpertProfileScreen, NoviceBookAppointmentScreen, NoviceHomeScreen } from "../../screens";
+import { ExpertProfileScreen, NoviceBookAppointmentScreen, NoviceHomeScreen, SingleChatScreen } from "../../screens";
 import { ROUTES } from "../../constants";
 const Stack = createStackNavigator();
 
@@ -10,6 +10,7 @@ const NoviceHomeStackNavigator = () => {
       <Stack.Screen name={ROUTES.NOVICE_HOME} component={NoviceHomeScreen} />
       <Stack.Screen name={ROUTES.EXPERT_PROFILE} component={ExpertProfileScreen} />
       <Stack.Screen name={ROUTES.NOVICE_BOOK_APPOINTMENT} component={NoviceBookAppointmentScreen} />
+      <Stack.Screen name={ROUTES.USER_SINGLE_CHAT} component={SingleChatScreen} />
     </Stack.Navigator>
   );
 };
