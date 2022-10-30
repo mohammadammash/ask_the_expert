4da8d14ebd28be12046b7838c081b1fcdef5ce91
mohammadammash <mohammadammash31@gmail.com>
2022-10-30 17:32:55 +0200

    frontend/src/components/Novice/AddReviewModalForm.tsx: Adding rating input and capturing the input rating using formik

--- frontend/src/components/Novice/AddReviewModalForm.tsx
+++ frontend/src/components/Novice/AddReviewModalForm.tsx
@@ -1,49 +1,64 @@
 import { View, Text, TextInput, Pressable } from "react-native";
 import { Formik } from "formik";
 import * as Yup from "yup";
+import { AirbnbRating } from "react-native-ratings";
 //internal imports
 import styles from "../../../styles";
+
+interface AddReviewModalFormProps {
+  modalRef: React.MutableRefObject<undefined>;
+}
 interface formValues {
   content: string;
+  rating: number;
 }
 export const reviewIntialValues: formValues = {
   content: "",
+  rating: 3,
 };
-
 export const validateReviewIntialValues = Yup.object().shape({
   content: Yup.string(),
+  rating: Yup.number().required("Required"),
 });
 
-const AddReviewModalForm = () => {
+// MAIN COMPONENT
+const AddReviewModalForm: React.FC<AddReviewModalFormProps> = ({ modalRef }) => {
   return (
-      <Formik
-        initialValues={{ about: "" }}
-        onSubmit={(values, actions) => {
-          console.log({ values, actions });
-          alert(JSON.stringify(values, null, 2));
-        }}
-        validationSchema={validateReviewIntialValues}
-      >
-        {({ handleChange, handleBlur, handleSubmit, errors, touched, values }) => (
-          <View className="w-full h-3/4 items-center justify-around">
-            <Text className="font-bold">About</Text>
-            <TextInput
-              onChangeText={handleChange("about")}
-              onBlur={handleBlur("about")}
-              value={values.about}
-              className=" placeholder:pl-3 h-20 border-2 w-5/6 rounded-lg"
-              placeholder="Talk About Your Career/Experience"
-              multiline={true}
-              numberOfLines={10}
-            />
-            {errors.about && touched.about && <Text className="text-red-600">{errors.about}</Text>}
+    <Formik
+      initialValues={reviewIntialValues}
+      onSubmit={(values, actions) => {
+        console.log({ values, actions });
+        alert(JSON.stringify(values, null, 2));
+        modalRef.current?.close();
+      }}
+      validationSchema={validateReviewIntialValues}
+    >
+      {({ handleChange, handleBlur, handleSubmit, errors, touched, values }) => (
+        <View className="w-full items-center">
+          <AirbnbRating
+            onFinishRating={(v) => (values.rating = v)}
+            starContainerStyle={{ display: "flex", alignItems: "center", height: 30 }}
+            size={20}
+            reviews={["Terrible", "Bad", "Okay", "Good", "Great"]}
+          />
 
-            <Pressable className="mt-2" style={styles.blue_button_sm} onPress={handleSubmit}>
-              <Text className="text-sm text-white font-bold">SUBMIT</Text>
-            </Pressable>
-          </View>
-        )}
-      </Formik>
+          <TextInput
+            onChangeText={handleChange("content")}
+            onBlur={handleBlur("content")}
+            value={values.content}
+            className=" placeholder:pl-3 h-20 my-5 border-2 w-5/6 rounded-lg"
+            placeholder="Clearify what "
+            multiline={true}
+            numberOfLines={10}
+          />
+          {errors.content && touched.content && <Text className="text-red-600">{errors.content}</Text>}
+
+          <Pressable className="mt-2" style={styles.blue_button_sm} onPress={handleSubmit}>
+            <Text className="text-sm text-white font-bold">SUBMIT</Text>
+          </Pressable>
+        </View>
+      )}
+    </Formik>
   );
 };
 
