2c70842ceb1f1f9308e625f630117cefa6ae1031
mohammadammash <mohammadammash31@gmail.com>
2022-10-30 21:48:38 +0200

    frontend/src/components/Novice/BookAppointmentFormCard.tsx: Altering Appointemnt form card to handle inputs with formik and validation

--- frontend/src/components/Novice/BookAppointmentFormCard.tsx
+++ frontend/src/components/Novice/BookAppointmentFormCard.tsx
@@ -1,54 +1,121 @@
-import { View, Text } from "react-native";
+import { TextInput, Text, Pressable, View } from "react-native";
 import RadioButton from "radio-buttons-react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
+import { FontAwesome } from "@expo/vector-icons";
 import { useState } from "react";
+import { Formik } from "formik";
+
 //internal
 import { COLORS } from "../../constants";
+import styles from "../../../styles";
 
 // HARD CODED DATA TO REMOVE LATER
 const data = [
   {
     label: "12:15pm 12:30pm",
-    value: "1",
+    start_timestamp: "1",
+    end_timestamp: "2",
   },
   {
     label: "12:15pm 12:30pm",
-    value: "2",
+    start_timestamp: "1",
+    end_timestamp: "2",
   },
   {
     label: "12:15pm 12:30pm",
-    value: "3",
+    start_timestamp: "1",
+    end_timestamp: "2",
   },
   {
     label: "12:15pm 12:30pm",
-    value: "3",
+    start_timestamp: "1",
+    end_timestamp: "2",
   },
   {
     label: "12:15pm 12:30pm",
-    value: "4",
+    start_timestamp: "1",
+    end_timestamp: "2",
   },
   {
     label: "12:15pm 12:30pm",
-    value: "4",
+    start_timestamp: "1",
+    end_timestamp: "2",
   },
   {
     label: "12:15pm 12:30pm",
-    value: "8",
+    start_timestamp: "1",
+    end_timestamp: "2",
   },
 ];
 
+//TYPES DEFINITIONS
+interface formValues {
+  notes: string;
+  start_timestamp: string;
+  end_timestamp: string;
+}
+type selectedTimeStateType = {
+  start_timestamp: string;
+  end_timestamp: string;
+};
+const selectedTimeStateInitialValues: selectedTimeStateType = {
+  start_timestamp: "",
+  end_timestamp: "",
+};
+//FORMIK
+const appointmentIntialValues: formValues = {
+  notes: "",
+  start_timestamp: "",
+  end_timestamp: "",
+};
+
+//MAIN COMPONENT
 const BookAppointmentFormCard = () => {
-  const [selectedTime, setSelectedTime] = useState(0);
+  const [selectedTimeStamps, setSelectedTimeStamp] = useState<selectedTimeStateType>(selectedTimeStateInitialValues);
+  const [submitButtonTouched, setSubmitButtonTouched] = useState<boolean>(false);
 
   return (
-    <RadioButton
-      data={data}
-      style={{ flex: 1, flexDirection: "row", flexWrap: "wrap", alignItems: "flex-start", justifyContent: 'space-evenly', marginTop: 10}}
-      boxStyle={{width: '40%', justifyContent: 'space-evenly'}}
-      textStyle={{marginLeft: 10, fontSize: 12}}
-      selectedBtn={(e) => alert(e.value)}
-      icon={<Icon name="check-circle" size={25} color={COLORS.blue} />}
-    />
+    <Formik
+      initialValues={appointmentIntialValues}
+      onSubmit={(values, actions) => {
+        setSubmitButtonTouched(true);
+        if (!selectedTimeStamps.start_timestamp) return;
+        [values.start_timestamp, values.end_timestamp] = [selectedTimeStamps.start_timestamp, selectedTimeStamps.end_timestamp];
+        alert(JSON.stringify(values, null, 2));
+      }}
+    >
+      {({ handleChange, handleBlur, handleSubmit, errors, touched, values }) => (
+        <View className="items-center">
+          <RadioButton
+            data={data}
+            style={{ flex: 1, flexDirection: "row", flexWrap: "wrap", alignItems: "flex-start", justifyContent: "space-evenly", marginTop: 10 }}
+            boxStyle={{ width: "40%", justifyContent: "space-evenly" }}
+            textStyle={{ marginLeft: 10, fontSize: 12 }}
+            selectedBtn={(e) => setSelectedTimeStamp({ start_timestamp: e.start_timestamp, end_timestamp: e.end_timestamp })}
+            icon={<Icon name="check-circle" size={25} color={COLORS.blue} />}
+          />
+          {submitButtonTouched && !selectedTimeStamps.start_timestamp && <Text className="text-red-600 mt-2">Time Should be Specified</Text>}
+
+          <TextInput
+            onChangeText={handleChange("notes")}
+            onBlur={handleBlur("notes")}
+            value={values.notes}
+            className=" placeholder:pl-3 h-20 border-2 mt-5 w-5/6 rounded-lg"
+            placeholder="Clearify what "
+            multiline={true}
+            numberOfLines={10}
+          />
+
+          <Text className="my-5 font-bold">10/11/2022 12:15 pm</Text>
+          <Pressable className="mb-5 flex-row" style={styles.blue_button_lg} onPress={handleSubmit}>
+            <FontAwesome name="calendar-check-o" size={20} color="white" />
+            <Text className="font-bold text-base ml-2" style={{ color: COLORS.white }}>
+              BOOK
+            </Text>
+          </Pressable>
+        </View>
+      )}
+    </Formik>
   );
 };
 
--- frontend/src/screens/Novice/BookAppointmentScreen.tsx
+++ frontend/src/screens/Novice/BookAppointmentScreen.tsx
@@ -1,16 +1,23 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
+//internal imports
+import { BookAppointmentFormCardComponent } from "../../components";
+import { COLORS } from "../../constants";
 
 const BookAppointmentScreen = () => {
   return (
     <View className="flex-1 w-full items-center justify-evenly bg-white border">
-      <View className="w-full items-center gap-5">
-        <Text className="text-slate-800 text-xl text-center">Your Career Advice Is One Click Away</Text>
+      <View className="w-full items-center h-1/6 gap-5">
+        <Text style={{ color: COLORS.blue }} className="text-slate-800 font-bold text-lg text-center">
+          Your Career Advice Is One Click Away
+        </Text>
         <Text className="text-center w-3/4 text-xs color-[#828282]">Choose the available appointment time, and make sure to be there on time. A short timespan for a huge boost.</Text>
       </View>
 
       {/* CONFIRM AVAILBILITY FORM */}
-      <View className="w-full items-center justify-around">
-        <BookAppointmentScreen />
+      <View className="w-full h-4/6 min-h-3/5 items-center">
+        <ScrollView className="w-5/6 border-2 rounded-xl">
+          <BookAppointmentFormCardComponent />
+        </ScrollView>
       </View>
     </View>
   );
