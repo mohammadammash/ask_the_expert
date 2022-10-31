188d7d9132e1ee6af1f8f6ce48246f79e71905f9
mohammadammash <mohammadammash31@gmail.com>
2022-10-31 19:47:37 +0200

    frontend/src/navigations/helpers/navigatorsHelpers.tsx: Moving drawerHelper to navigation main folder as we are using it in different navigation directories

--- frontend/src/navigations/helpers/navigatorsHelpers.tsx
+++ frontend/src/navigations/helpers/navigatorsHelpers.tsx
@@ -1,15 +1,19 @@
 import { MaterialCommunityIcons, MaterialIcons, Entypo } from "@expo/vector-icons";
-import { TouchableOpacity, Text } from "react-native";
+import { TouchableOpacity } from "react-native";
 //internal imports
-import { COLORS } from "../../../constants";
+import { COLORS } from "../../constants";
 
 export const homeIcon = <MaterialCommunityIcons name="home" size={24} color={COLORS.blue} />;
 export const appointmentsIcon = <MaterialCommunityIcons name="calendar-account-outline" size={24} color={COLORS.blue} />;
 export const leaderboardIcon = <MaterialIcons name="leaderboard" size={24} color={COLORS.blue} />;
 export const chatIcon = <Entypo name="chat" size={24} color={COLORS.blue} />;
 export const settingsIcon = <MaterialCommunityIcons name="account-cog" size={24} color={COLORS.blue} />;
+export const menuIcon = (
+  <MaterialCommunityIcons style={{ marginLeft: 20, marginBottom: 5 }} name="microsoft-xbox-controller-menu" size={32} color={COLORS.white} />
+);
 
-export const screenOptions = (navigation: any) => ({
+// DRAWER NAVIGATOR: CUSTOM HEADER STYLE
+export const drawerScreenOptionsStyle = (navigation: any) => ({
   drawerActiveBackgroundColor: COLORS.grey,
   drawerActiveTintColor: COLORS.blue,
   drawerInactiveTintColor: COLORS.white,
@@ -17,9 +21,17 @@ export const screenOptions = (navigation: any) => ({
   headerStyle: {
     backgroundColor: COLORS.blue,
   },
-  headerLeft: () => (
-    <TouchableOpacity onPress={() => navigation.openDrawer()}>
-      <MaterialCommunityIcons style={{ marginLeft: 20, marginBottom: 5 }} name="microsoft-xbox-controller-menu" size={32} color={COLORS.white} />
-    </TouchableOpacity>
-  ),
+  headerLeft: () => <TouchableOpacity onPress={() => navigation.openDrawer()}>{menuIcon}</TouchableOpacity>,
+});
+
+// STACK SCREEN: CUSTOM SCREEN OPTIONS
+export const stackScreenOptionsStyle = () => ({
+  headerBackTitleVisible: false,
+  headerStyle: {
+    backgroundColor: COLORS.blue,
+  },
+  headerTintColor: COLORS.white,
+  headerTitleStyle: {
+    fontWeight: "bold",
+  },
 });
