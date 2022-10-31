8214c81d5b10b32b3edf95ef0188db610ac87bdd
mohammadammash <mohammadammash31@gmail.com>
2022-10-31 19:19:41 +0200

    frontend/src/navigations/Common/ProfileStackNavigator.tsx: Removing parent navigator header and keeping stack header

--- frontend/src/navigations/Common/ProfileStackNavigator.tsx
+++ frontend/src/navigations/Common/ProfileStackNavigator.tsx
@@ -1,5 +1,10 @@
 import * as React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
+import { TouchableOpacity } from "react-native";
+import { UserContext } from "../../hooks/UserContext";
+import { useContext } from "react";
+import { useNavigation } from "@react-navigation/native";
+//internal imports
 import {
   ExpertProfileScreen,
   NoviceProfileScreen,
@@ -10,13 +15,15 @@ import {
   AdminViewBannedUsersScreen,
 } from "../../screens";
 import { ROUTES, USERTYPES, COLORS } from "../../constants";
-import { UserContext } from "../../hooks/UserContext";
-import { useContext } from "react";
+import { menuIcon } from "./helpers/drawerNavigatorHelper";
+
 const Stack = createStackNavigator();
 
 const ProfileStackNavigator = () => {
   const { user, setUser } = useContext(UserContext);
   const { user_type } = user;
+  const navigation = useNavigation<any>();
+
   const titleScreenOptions = {
     headerStyle: {
       backgroundColor: COLORS.blue,
@@ -29,36 +36,39 @@ const ProfileStackNavigator = () => {
   };
 
   return (
-    <Stack.Navigator>
+    <Stack.Navigator screenOptions={titleScreenOptions}>
+      {/* EXPERT PROFILE STACK */}
       {user_type === USERTYPES.EXPERT && (
         <>
-          <Stack.Screen name={ROUTES.EXPERT_PROFILE} component={ExpertProfileScreen} options={{ headerShown: false }} />
           <Stack.Screen
-            name={ROUTES.EXPERT_GO_ONLINE}
-            component={ExpertGoOnlineScreen}
-            options={{ ...titleScreenOptions, title: "Set Availability" }}
+            name={ROUTES.EXPERT_PROFILE}
+            component={ExpertProfileScreen}
+            options={{ headerLeft: () => <TouchableOpacity onPress={() => navigation.openDrawer()}>{menuIcon}</TouchableOpacity>, title: "Profile" }}
           />
-          <Stack.Screen name={ROUTES.NOVICE_PROFILE} component={NoviceProfileScreen} options={{ ...titleScreenOptions, title: "Profile" }} />
-          <Stack.Screen name={ROUTES.USER_SINGLE_CHAT} component={SingleChatScreen} options={{ ...titleScreenOptions, title: "Messages" }} />
+          <Stack.Screen name={ROUTES.EXPERT_GO_ONLINE} component={ExpertGoOnlineScreen} options={{ title: "Set Availability" }} />
+          <Stack.Screen name={ROUTES.NOVICE_PROFILE} component={NoviceProfileScreen} options={{ title: "Profile" }} />
+          <Stack.Screen name={ROUTES.USER_SINGLE_CHAT} component={SingleChatScreen} options={{ title: "Messages" }} />
         </>
       )}
 
+      {/* NOVICE PROFILE STACK */}
       {user_type === USERTYPES.NOVICE && (
-        <Stack.Screen name={ROUTES.NOVICE_PROFILE} component={NoviceProfileScreen} options={{ headerShown: false }} />
+        <Stack.Screen
+          name={ROUTES.NOVICE_PROFILE}
+          component={NoviceProfileScreen}
+          options={{ headerLeft: () => <TouchableOpacity onPress={() => navigation.openDrawer()}>{menuIcon}</TouchableOpacity>, title: "Profile" }}
+        />
       )}
 
+      {/* ADMIN PROFILE STACK */}
       {user_type === USERTYPES.ADMIN && (
         <>
-          <Stack.Screen name={ROUTES.ADMIN_PROFILE} component={AdminProfileScreen} options={{ headerShown: false }} />
-          <Stack.Screen
-            name={ROUTES.ADMIN_VIEW_BANNED_USERS}
-            component={AdminViewBannedUsersScreen}
-            options={{ ...titleScreenOptions, title: "Banned Users" }}
-          />
+          <Stack.Screen name={ROUTES.ADMIN_PROFILE} component={AdminProfileScreen} />
+          <Stack.Screen name={ROUTES.ADMIN_VIEW_BANNED_USERS} component={AdminViewBannedUsersScreen} options={{ title: "Banned Users" }} />
         </>
       )}
 
-      <Stack.Screen name={ROUTES.USER_EDIT_PROFILE} component={EditProfileScreen} options={{ ...titleScreenOptions, title: "Edit Profile" }} />
+      <Stack.Screen name={ROUTES.USER_EDIT_PROFILE} component={EditProfileScreen} options={{ title: "Edit Profile" }} />
     </Stack.Navigator>
   );
 };
