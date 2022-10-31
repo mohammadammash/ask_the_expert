f36b00a686786daabcd4f70d152718f15860d1f0
mohammadammash <mohammadammash31@gmail.com>
2022-10-31 19:20:09 +0200

    frontend/src/navigations/Common/DrawerNavigator.tsx: Removing parent header and keeping stack header

--- frontend/src/navigations/Common/DrawerNavigator.tsx
+++ frontend/src/navigations/Common/DrawerNavigator.tsx
@@ -18,21 +18,42 @@ const DrawerNavigator = () => {
     <Drawer.Navigator
       useLegacyImplementation={true}
       drawerContent={(props) => <CustomDrawerComponent {...props} />}
-      screenOptions={({ navigation}) => screenOptions(navigation)}
+      screenOptions={({ navigation }) => screenOptions(navigation)}
     >
       {/* NOVICE HOME_STACK */}
       {user.user_type === USERTYPES.NOVICE && (
-        <Drawer.Screen name={ROUTES.NOVICE_HOME_STACK} component={NoviceHomeStackNavigator} options={{ drawerIcon: () => homeIcon }} />
+        <Drawer.Screen
+          name={ROUTES.NOVICE_HOME_STACK}
+          component={NoviceHomeStackNavigator}
+          options={{ drawerIcon: () => homeIcon, headerShown: false }}
+        />
+      )}
+
+      {/* EXPERT PROFILE - ADDED BEFORE TO BE INTIAL ROUTE NAME */}
+      {user.user_type === USERTYPES.EXPERT && (
+        <Drawer.Screen
+          name={ROUTES.PROFILE_STACK}
+          component={ProfileStackNavigator}
+          options={{ drawerIcon: () => settingsIcon, headerShown: false }}
+        />
       )}
 
       {/* COMMON STACKS */}
       <Drawer.Screen name={ROUTES.USER_LEADERBOARD} component={LeaderboardScreen} options={{ drawerIcon: () => leaderboardIcon }} />
-      <Drawer.Screen name={ROUTES.CHATS_STACK} component={ChatsStackNavigator} options={{ drawerIcon: () => chatIcon}} />
-      <Drawer.Screen name={ROUTES.APPOINTMENTS_STACK} component={AppointmentsStackNavigator} options={{ drawerIcon: () => appointmentsIcon }} />
+      <Drawer.Screen name={ROUTES.CHATS_STACK} component={ChatsStackNavigator} options={{ drawerIcon: () => chatIcon, headerShown: false }} />
+      <Drawer.Screen
+        name={ROUTES.APPOINTMENTS_STACK}
+        component={AppointmentsStackNavigator}
+        options={{ drawerIcon: () => appointmentsIcon, headerShown: false }}
+      />
 
       {/* NOVICE&EXPERT PROFILE_STACK */}
-      {(user.user_type === USERTYPES.NOVICE || user.user_type === USERTYPES.EXPERT) && (
-        <Drawer.Screen name={ROUTES.PROFILE_STACK} component={ProfileStackNavigator} options={{ drawerIcon: () => settingsIcon }} />
+      {user.user_type === USERTYPES.NOVICE && (
+        <Drawer.Screen
+          name={ROUTES.PROFILE_STACK}
+          component={ProfileStackNavigator}
+          options={{ drawerIcon: () => settingsIcon, headerShown: false }}
+        />
       )}
     </Drawer.Navigator>
   );
