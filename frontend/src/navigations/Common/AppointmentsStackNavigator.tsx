a564b2a4df0dac6ef9836cdd3784112c2381f796
mohammadammash <mohammadammash31@gmail.com>
2022-10-31 16:08:24 +0200

    frontend/src/navigations/Common/AppointmentsStackNavigator.tsx: Styling headers and title navigator

--- frontend/src/navigations/Common/AppointmentsStackNavigator.tsx
+++ frontend/src/navigations/Common/AppointmentsStackNavigator.tsx
@@ -1,15 +1,27 @@
-import * as React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import { AppointmentsScreen, NoviceProfileScreen, SingleChatScreen } from "../../screens";
-import { ROUTES } from "../../constants";
+//internal imports
+import { AppointmentsScreen, NoviceProfileScreen, ExpertProfileScreen, SingleChatScreen } from "../../screens";
+import { ROUTES, COLORS } from "../../constants";
 const Stack = createStackNavigator();
+
+const titleScreenOptions = {
+  headerBackTitleVisible: false,
+  headerStyle: {
+    backgroundColor: COLORS.blue,
+  },
+  headerTintColor: COLORS.white,
+  headerTitleStyle: {
+    fontWeight: "bold",
+  },
+};
 
 const AppointmentsStackNavigator = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name={ROUTES.USER_APPOINTMENTS} component={AppointmentsScreen} options={{headerShown: false}} />
-      <Stack.Screen name={ROUTES.NOVICE_PROFILE} component={NoviceProfileScreen} />
-      <Stack.Screen name={ROUTES.USER_SINGLE_CHAT} component={SingleChatScreen} />
+      <Stack.Screen name={ROUTES.USER_APPOINTMENTS} component={AppointmentsScreen} options={{ headerShown: false }} />
+      <Stack.Screen name={ROUTES.NOVICE_PROFILE} component={NoviceProfileScreen} options={titleScreenOptions} />
+      <Stack.Screen name={ROUTES.EXPERT_PROFILE} component={ExpertProfileScreen} options={titleScreenOptions} />
+      <Stack.Screen name={ROUTES.USER_SINGLE_CHAT} component={SingleChatScreen} options={titleScreenOptions} />
     </Stack.Navigator>
   );
 };
