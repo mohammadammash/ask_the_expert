c221045db04aab97b7a566de0feccfa9490b72a6
mohammadammash <mohammadammash31@gmail.com>
2022-10-31 19:50:12 +0200

    frontend/src/navigations/Common/AppointmentsStackNavigator.tsx: Adding navigation helper options and removing local ones

--- frontend/src/navigations/Common/AppointmentsStackNavigator.tsx
+++ frontend/src/navigations/Common/AppointmentsStackNavigator.tsx
@@ -3,35 +3,24 @@ import { useNavigation } from "@react-navigation/native";
 import { TouchableOpacity } from "react-native";
 //internal imports
 import { AppointmentsScreen, NoviceProfileScreen, ExpertProfileScreen, SingleChatScreen } from "../../screens";
-import { ROUTES, COLORS } from "../../constants";
-import { menuIcon } from "./helpers/drawerNavigatorHelper";
+import { ROUTES } from "../../constants";
+import { menuIcon, stackScreenOptionsStyle } from "../helpers/navigatorsHelpers";
 
 const Stack = createStackNavigator();
 
-const titleScreenOptions = {
-  headerBackTitleVisible: false,
-  headerStyle: {
-    backgroundColor: COLORS.blue,
-  },
-  headerTintColor: COLORS.white,
-  headerTitleStyle: {
-    fontWeight: "bold",
-  },
-};
-
 const AppointmentsStackNavigator = () => {
   const navigation = useNavigation<any>();
-  
+
   return (
-    <Stack.Navigator screenOptions={titleScreenOptions}>
+    <Stack.Navigator screenOptions={stackScreenOptionsStyle}>
       <Stack.Screen
         name={ROUTES.USER_APPOINTMENTS}
         component={AppointmentsScreen}
         options={{ headerLeft: () => <TouchableOpacity onPress={() => navigation.openDrawer()}>{menuIcon}</TouchableOpacity> }}
       />
-      <Stack.Screen name={ROUTES.NOVICE_PROFILE} component={NoviceProfileScreen} />
-      <Stack.Screen name={ROUTES.EXPERT_PROFILE} component={ExpertProfileScreen}/>
-      <Stack.Screen name={ROUTES.USER_SINGLE_CHAT} component={SingleChatScreen} />
+      <Stack.Screen name={ROUTES.NOVICE_PROFILE} component={NoviceProfileScreen} options={{ title: "Profile" }} />
+      <Stack.Screen name={ROUTES.EXPERT_PROFILE} component={ExpertProfileScreen} options={{ title: "Profile" }} />
+      <Stack.Screen name={ROUTES.USER_SINGLE_CHAT} component={SingleChatScreen} options={{ title: "Messages" }} />
     </Stack.Navigator>
   );
 };
