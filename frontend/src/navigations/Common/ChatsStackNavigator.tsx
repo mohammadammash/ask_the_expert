c0f26dd6447b720f5ebdce2a7b182bac8dddeda4
mohammadammash <mohammadammash31@gmail.com>
2022-10-31 16:08:05 +0200

    frontend/src/navigations/Common/ChatsStackNavigator.tsx: Styling header and title navigator

--- frontend/src/navigations/Common/ChatsStackNavigator.tsx
+++ frontend/src/navigations/Common/ChatsStackNavigator.tsx
@@ -1,16 +1,23 @@
 import { createStackNavigator } from "@react-navigation/stack";
-import { TouchableOpacity } from "react-native";
-import { Ionicons } from "@expo/vector-icons"; 
 //internal imports
 import { ChatsScreen, SingleChatScreen } from "../../screens";
 import { COLORS, ROUTES } from "../../constants";
 const Stack = createStackNavigator();
-
+const titleScreenOptions = {
+  headerBackTitleVisible: false,
+  headerStyle: {
+    backgroundColor: COLORS.blue,
+  },
+  headerTintColor: COLORS.white,
+  headerTitleStyle: {
+    fontWeight: "bold",
+  },
+};
 const ChatsStackNavigator = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name={ROUTES.USER_CHATS} component={ChatsScreen} screenOptions={{ headerShown: false }} />
-      <Stack.Screen name={ROUTES.USER_SINGLE_CHAT} component={SingleChatScreen} />
+      <Stack.Screen name={ROUTES.USER_CHATS} component={ChatsScreen} options={{ headerShown: false }} />
+      <Stack.Screen name={ROUTES.USER_SINGLE_CHAT} component={SingleChatScreen} options={titleScreenOptions} />
     </Stack.Navigator>
   );
 };
