130c3dc2ccbb193b4a78cd2a6a1c762f99bd844b
mohammadammash <mohammadammash31@gmail.com>
2022-10-31 16:47:11 +0200

    frontend/src/navigations/Admin/AdminTabNavigator.tsx: Altering and designing headers and titles

--- frontend/src/navigations/Admin/AdminTabNavigator.tsx
+++ frontend/src/navigations/Admin/AdminTabNavigator.tsx
@@ -1,19 +1,22 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+//internal imports
 import { ROUTES } from "../../constants";
+import { homeIcon, leaderboardIcon, screenOptions, settingsIcon, usersIcon } from "./helpers/bottomTabNavigatorHelper";
 import { LeaderboardScreen, AdminHomeScreen, AdminViewUsersScreen } from "../../screens";
 import ProfileStackNavigator from "../Common/ProfileStackNavigator";
-
-
 const Tab = createBottomTabNavigator();
 
 const AdminTabNavigator = () => {
-
   return (
-    <Tab.Navigator>
-      <Tab.Screen name={ROUTES.ADMIN_HOME} component={AdminHomeScreen} />
-      <Tab.Screen name={ROUTES.USER_LEADERBOARD} component={LeaderboardScreen} />
-      <Tab.Screen name={ROUTES.ADMIN_VIEW_USERS} component={AdminViewUsersScreen} />
-      <Tab.Screen name={ROUTES.PROFILE_STACK} component={ProfileStackNavigator} />
+    <Tab.Navigator screenOptions={({ navigation }) => screenOptions(navigation)}>
+      <Tab.Screen name={ROUTES.ADMIN_HOME} component={AdminHomeScreen} options={{ tabBarIcon: () => homeIcon, title: "Home" }} />
+      <Tab.Screen
+        name={ROUTES.USER_LEADERBOARD}
+        component={LeaderboardScreen}
+        options={{ tabBarIcon: () => leaderboardIcon, title: "Leaderboard" }}
+      />
+      <Tab.Screen name={ROUTES.ADMIN_VIEW_USERS} component={AdminViewUsersScreen} options={{ tabBarIcon: () => usersIcon, title: "All Users" }} />
+      <Tab.Screen name={ROUTES.PROFILE_STACK} component={ProfileStackNavigator} options={{ tabBarIcon: () => settingsIcon, title: "Profile" }} />
     </Tab.Navigator>
   );
 };
