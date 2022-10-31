ef2034f3c7b4235b3ec0d255719b2418a46af895
mohammadammash <mohammadammash31@gmail.com>
2022-10-31 10:35:07 +0200

    frontend/src/navigations/Common/helpers/darwerNavigatorHelper.tsx: Adding icons and options

---
+++ frontend/src/navigations/Common/helpers/darwerNavigatorHelper.tsx
@@ -0,0 +1,22 @@
+import { MaterialCommunityIcons, MaterialIcons, Entypo } from "@expo/vector-icons";
+import { TouchableOpacity, Text } from "react-native";
+//internal imports
+import { COLORS } from "../../../constants";
+
+export const homeIcon = <MaterialCommunityIcons name="home" size={24} color={COLORS.blue} />;
+export const appointmentsIcon = <MaterialCommunityIcons name="calendar-account-outline" size={24} color={COLORS.blue} />;
+export const leaderboardIcon = <MaterialIcons name="leaderboard" size={24} color={COLORS.blue} />;
+export const chatIcon = <Entypo name="chat" size={24} color={COLORS.blue} />;
+export const settingsIcon = <MaterialCommunityIcons name="account-cog" size={24} color={COLORS.blue} />;
+
+export const screenOptions = (navigation: any) => ({
+  headerTitle: "",
+  headerStyle: {
+    backgroundColor: COLORS.blue,
+  },
+  headerLeft: () => (
+    <TouchableOpacity onPress={() => navigation.openDrawer()}>
+      <MaterialCommunityIcons style={{ marginLeft: 20, marginBottom: 5 }} name="microsoft-xbox-controller-menu" size={32} color={COLORS.white} />
+    </TouchableOpacity>
+  ),
+});
