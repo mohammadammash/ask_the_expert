c0ac597639cc66835ebedec5c8ed5095b432d8c0
mohammadammash <mohammadammash31@gmail.com>
2022-10-31 15:54:29 +0200

    frontend/src/navigations/Novice/NoviceHomeStackNavigator.tsx: Designing the stacks titles and names

--- frontend/src/components/Navigation/CustomDrawer.tsx
+++ frontend/src/components/Navigation/CustomDrawer.tsx
@@ -1,18 +1,26 @@
-import { ImageBackground, Image, View } from "react-native";
+import { ImageBackground, Image, View, StatusBar, TouchableOpacity, Text } from "react-native";
 import { DrawerContentScrollView, DrawerItemList, DrawerItem } from "@react-navigation/drawer";
+import { SimpleLineIcons } from "@expo/vector-icons"; 
 //internal imports
 import { IMAGES, COLORS } from "../../constants";
 import navigationStyles from "./navigation.styles";
 
 const CustomDrawer = (props: DrawerContentComponentProps): JSX.Element => {
+
   return (
     // drawer whole content
-    <DrawerContentScrollView {...props}>
+    <DrawerContentScrollView {...props} contentContainerStyle={{ paddingTop: 0, backgroundColor: COLORS.dark, height: "100%" }}>
       <ImageBackground className="h-36" source={IMAGES.fakeMapImage}>
         <Image source={IMAGES.dummyProfile} style={navigationStyles.userImg} />
       </ImageBackground>
-      <View style={navigationStyles.drawerListWrapper}>
-        <DrawerItemList {...props} />
+      <View className="h-5/6 justify-between pb-5">
+        <View style={navigationStyles.drawerListWrapper}>
+          <DrawerItemList {...props} />
+        </View>
+        <TouchableOpacity style={{borderColor: COLORS.grey}} className="flex-row border-t h-14 items-center pl-3 py-3" onPress={() => alert("LOGOUT")}>
+          <SimpleLineIcons name="logout" size={24} color={COLORS.blue} />
+          <Text className="ml-9 text-sm text-white font-semibold">Logout</Text>
+        </TouchableOpacity>
       </View>
     </DrawerContentScrollView>
   );
--- frontend/src/navigations/Novice/NoviceHomeStackNavigator.tsx
+++ frontend/src/navigations/Novice/NoviceHomeStackNavigator.tsx
@@ -1,16 +1,27 @@
 import * as React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { ExpertProfileScreen, NoviceBookAppointmentScreen, NoviceHomeScreen, SingleChatScreen } from "../../screens";
-import { ROUTES } from "../../constants";
+import { ROUTES, COLORS } from "../../constants";
 const Stack = createStackNavigator();
 
 const NoviceHomeStackNavigator = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name={ROUTES.NOVICE_HOME} component={NoviceHomeScreen} />
-      <Stack.Screen name={ROUTES.EXPERT_PROFILE} component={ExpertProfileScreen} />
-      <Stack.Screen name={ROUTES.NOVICE_BOOK_APPOINTMENT} component={NoviceBookAppointmentScreen} />
-      <Stack.Screen name={ROUTES.USER_SINGLE_CHAT} component={SingleChatScreen} />
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: COLORS.blue,
+        },
+        headerTintColor: COLORS.white,
+        headerBackTitleVisible: false,
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+      }}
+    >
+      <Stack.Screen name={ROUTES.NOVICE_HOME} component={NoviceHomeScreen} options={{ headerShown: false }} />
+      <Stack.Screen name={ROUTES.EXPERT_PROFILE} component={ExpertProfileScreen} options={{ title: "Profile" }} />
+      <Stack.Screen name={ROUTES.NOVICE_BOOK_APPOINTMENT} component={NoviceBookAppointmentScreen} options={{ title: "Book" }} />
+      <Stack.Screen name={ROUTES.USER_SINGLE_CHAT} component={SingleChatScreen} options={{ title: "Messages" }} />
     </Stack.Navigator>
   );
 };
