37673442bcd1615e048a14075f11824f284c4fa4
mohammadammash <mohammadammash31@gmail.com>
2022-10-31 16:57:54 +0200

    frontend/src/screens/Expert/GoOnlineScreen.tsx: Designing the parent view of appointemntform Component

--- frontend/src/screens/Expert/GoOnlineScreen.tsx
+++ frontend/src/screens/Expert/GoOnlineScreen.tsx
@@ -1,19 +1,24 @@
 import { useNavigation } from "@react-navigation/native";
-import { View, Text} from "react-native";
+import { View, Text } from "react-native";
 import styles from "../../../styles";
 import { ConfirmAvailabilityFormCard } from "../../components";
 import { COLORS } from "../../constants";
 
 const ProfileScreen = () => {
-
   return (
-    <View className="flex-1 items-center justify-around bg-white border">
-      <Text className="text-slate-800 text-xl text-center w-5/6">The World is waiting for your Touch Go Online?</Text>
-      <Text className="text-center w-3/4 text-xs color-[#828282]">Choose the time you will be available by, and the time of each meeting</Text>
+    <View className="flex-1 w-full h-full items-center justify-evenly bg-white border">
+      <View className="w-full px-3 items-center gap-5">
+        <Text style={{ color: COLORS.blue }} className="text-slate-800 font-bold text-lg text-center">
+          The World is waiting for your Touch Go Online?
+        </Text>
+        <Text className="text-center w-3/4 text-xs color-[#828282]">Choose the time you will be available by, and the time of each meeting</Text>
+      </View>
 
       {/* CONFIRM AVAILBILITY FORM */}
-      <View className="w-full items-center justify-around">
-        <ConfirmAvailabilityFormCard />
+      <View className="w-full h-3/5 items-center justify-center">
+        <View className="w-5/6 h-5/6 border-2 rounded-xl">
+          <ConfirmAvailabilityFormCard />
+        </View>
       </View>
     </View>
   );
