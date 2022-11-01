7494e589d70deaecb7a9d4b3921652fbabc0171f
mohammadammash <mohammadammash31@gmail.com>
2022-11-01 20:11:26 +0200

    frontend/src/components/Novice/AddReviewButtonSection.tsx: Extracting types and states to parent screen

--- frontend/src/components/Novice/AddReviewButtonSection.tsx
+++ frontend/src/components/Novice/AddReviewButtonSection.tsx
@@ -1,15 +1,12 @@
-import { View, Text, Pressable } from "react-native";
+import { Text, Pressable, View } from "react-native";
 import RBSheet from "react-native-raw-bottom-sheet";
-import { useRef, useState } from "react";
 //internal imports
 import { COLORS } from "../../constants";
 import styles from "../../../styles";
 import AddReviewModalForm from "./AddReviewModalForm";
+import { AddReviewButtonSectionProps } from "./types";
 
-const AddReviewButtonSection = () => {
-  const modalRef = useRef();
-  const [showModal, setShowModal] = useState(false); //to close the modal when submit
-
+const AddReviewButtonSection: React.FC<AddReviewButtonSectionProps> = ({ modalRef }) => {
   return (
     <View className="items-center">
       {/* SHOW REVIEW */}
@@ -20,28 +17,28 @@ const AddReviewButtonSection = () => {
       </Pressable>
 
       {/* REVIEW BOTTOM SHEET/MODAL CONTENT */}
-        <RBSheet
-          ref={modalRef}
-          closeOnDragDown={true}
-          closeOnPressMask={true}
-          customStyles={{
-            wrapper: {
-              backgroundColor: "rgba(255,255,255,0.9)",
-            },
-            draggableIcon: {
-              backgroundColor: COLORS.dark,
-            },
-            container: {
-              borderTopWidth: 2,
-              borderColor: COLORS.dark,
-              alignItems: "center",
-              height: '40%',
-            },
-          }}
-        >
-          <Text>How was Your Experience with Ahmad?</Text>
-          <AddReviewModalForm modalRef={modalRef} />
-        </RBSheet>
+      <RBSheet
+        ref={modalRef}
+        closeOnDragDown={true}
+        closeOnPressMask={true}
+        customStyles={{
+          wrapper: {
+            backgroundColor: "rgba(255,255,255,0.9)",
+          },
+          draggableIcon: {
+            backgroundColor: COLORS.dark,
+          },
+          container: {
+            borderTopWidth: 2,
+            borderColor: COLORS.dark,
+            alignItems: "center",
+            height: "40%",
+          },
+        }}
+      >
+        <Text>How was Your Experience with Ahmad?</Text>
+        <AddReviewModalForm modalRef={modalRef} />
+      </RBSheet>
     </View>
   );
 };
