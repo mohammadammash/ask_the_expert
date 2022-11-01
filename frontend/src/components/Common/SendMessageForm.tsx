d49c4a2678e658ae9646cd64867d808682a35a2c
mohammadammash <mohammadammash31@gmail.com>
2022-11-01 20:08:01 +0200

    frontend/src/components/Common/helpers/editProfileFormHelper.ts: Extracting types, and states to parent screen

--- frontend/src/components/Common/SendMessageForm.tsx
+++ frontend/src/components/Common/SendMessageForm.tsx
@@ -1,20 +1,20 @@
 import { View, TextInput } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
-import { useState } from "react";
 //internal imports
 import { COLORS } from "../../constants";
 import styles from "../../../styles";
+import { sendMessageFormProps } from "./types";
 
-const SendMessageForm = () => {
-  const [message, onChangeMessage] = useState("");
-  const submitMessage = () => {
-    if(!message) alert('Cannot be empty!');
-    else alert(message);
-  };
-
+const SendMessageForm: React.FC<sendMessageFormProps> = ({ message, handleMessageChange, submitMessage }) => {
   return (
     <View style={{ backgroundColor: COLORS.grey }} className="flex-row h-20 items-center justify-around">
-      <TextInput value={message} onChangeText={onChangeMessage} style={[styles.text_input, styles.send_message_input]} className="ml-1 w-5/6 placeholder:pl-3" placeholder="Write your message" />
+      <TextInput
+        value={message}
+        onChangeText={(v)=>handleMessageChange(v)}
+        style={[styles.text_input, styles.send_message_input]}
+        className="ml-1 w-5/6 placeholder:pl-3"
+        placeholder="Write your message"
+      />
       <View className="mr-3">
         <FontAwesome name="send" size={24} color={COLORS.blue} onPress={submitMessage} />
       </View>
