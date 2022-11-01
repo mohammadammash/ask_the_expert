0d965e0d08254ad4f940d70e5fb864b03992b0ed
mohammadammash <mohammadammash31@gmail.com>
2022-11-01 20:18:31 +0200

    frontend/src/screens/Novice/ProfileScreen.tsx: Adding states and handling components logic

--- frontend/src/screens/Novice/ProfileScreen.tsx
+++ frontend/src/screens/Novice/ProfileScreen.tsx
@@ -1,18 +1,46 @@
-import { View, } from "react-native";
+import { View } from "react-native";
+import { useContext } from "react";
+import { UserContext } from "../../hooks/UserContext";
+import { useNavigation } from "@react-navigation/native";
 //internal imports:
 import { ProfilePersonalInfoComponent, ProfileImageCardComponent, AboutSectionComponent, ButtonsComponent } from "../../components";
+import CalculateYearsOfExperience from "../helpers/calculateYearsOfExperienceHelper";
 
 const ProfileScreen = () => {
+  const navigation = useNavigation<any>();
+  const navigateToPage = (routeName: string) => navigation.navigate(routeName);
+
+  const { user, setUser } = useContext(UserContext);
+  const { firstName, lastName, field, speciality, spoken_languages, start_date, profile_url, about, user_type } = user;
+  const yearsOfExperience = CalculateYearsOfExperience(start_date);
+
+  //params
+  const personalInfoData = {
+    firstName,
+    lastName,
+    field,
+    speciality,
+    spoken_languages,
+    yearsOfExperience,
+  };
+  const aboutData = {
+    about,
+    user_type,
+  };
+  const buttonsData = {
+    user_type,
+    navigateToPage,
+  }
 
   return (
     <View className="flex-1 items-center bg-white">
-      <ProfileImageCardComponent />
+      <ProfileImageCardComponent profile_url={profile_url} />
 
-      <ProfilePersonalInfoComponent />
+      <ProfilePersonalInfoComponent {...personalInfoData} />
 
-      <AboutSectionComponent />
+      <AboutSectionComponent {...aboutData} />
 
-      <ButtonsComponent/>
+      <ButtonsComponent {...buttonsData}/>
     </View>
   );
 };
