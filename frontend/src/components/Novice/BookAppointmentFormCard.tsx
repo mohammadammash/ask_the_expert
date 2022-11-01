406d262f7adf9ef23141ed6946392c3156f4905e
mohammadammash <mohammadammash31@gmail.com>
2022-11-01 20:11:57 +0200

    frontend/src/components/Novice/BookAppointmentFormCard.tsx: Extracting types and states to parent screen

--- frontend/src/components/Novice/BookAppointmentFormCard.tsx
+++ frontend/src/components/Novice/BookAppointmentFormCard.tsx
@@ -2,96 +2,47 @@ import { TextInput, Text, Pressable, View } from "react-native";
 import RadioButton from "radio-buttons-react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 import { FontAwesome } from "@expo/vector-icons";
-import { useState } from "react";
 import { Formik } from "formik";
-
 //internal
 import { COLORS } from "../../constants";
 import styles from "../../../styles";
+import {BookFormValuesTypes, BookAppointmentFormCardProps} from "./types";
 
-// HARD CODED DATA TO REMOVE LATER
-const data = [
-  {
-    label: "12:15pm 12:30pm",
-    start_timestamp: "1",
-    end_timestamp: "2",
-  },
-  {
-    label: "12:15pm 12:30pm",
-    start_timestamp: "1",
-    end_timestamp: "2",
-  },
-  {
-    label: "12:15pm 12:30pm",
-    start_timestamp: "1",
-    end_timestamp: "2",
-  },
-  {
-    label: "12:15pm 12:30pm",
-    start_timestamp: "1",
-    end_timestamp: "2",
-  },
-  {
-    label: "12:15pm 12:30pm",
-    start_timestamp: "1",
-    end_timestamp: "2",
-  },
-  {
-    label: "12:15pm 12:30pm",
-    start_timestamp: "1",
-    end_timestamp: "2",
-  },
-  {
-    label: "12:15pm 12:30pm",
-    start_timestamp: "1",
-    end_timestamp: "2",
-  },
-];
 
-//TYPES DEFINITIONS
-interface formValues {
-  notes: string;
-  start_timestamp: string;
-  end_timestamp: string;
-}
-type selectedTimeStateType = {
-  start_timestamp: string;
-  end_timestamp: string;
-};
-const selectedTimeStateInitialValues: selectedTimeStateType = {
-  start_timestamp: "",
-  end_timestamp: "",
-};
 //FORMIK
-const appointmentIntialValues: formValues = {
+const appointmentIntialValues: BookFormValuesTypes = {
   notes: "",
   start_timestamp: "",
   end_timestamp: "",
 };
 
 //MAIN COMPONENT
-const BookAppointmentFormCard = () => {
-  const [selectedTimeStamps, setSelectedTimeStamp] = useState<selectedTimeStateType>(selectedTimeStateInitialValues);
-  const [submitButtonTouched, setSubmitButtonTouched] = useState<boolean>(false);
-
+const BookAppointmentFormCard: React.FC<BookAppointmentFormCardProps> = ({
+  selectedTimeStamps,
+  handleSubmitButtonTouched,
+  submitButtonTouched,
+  data,
+  handleSubmitTimeStamps,
+  handleFormSubmit,
+}) => {
   return (
     <Formik
       initialValues={appointmentIntialValues}
-      onSubmit={(values, actions) => {
-        setSubmitButtonTouched(true);
+      onSubmit={(values) => {
+        handleSubmitButtonTouched(true);
         if (!selectedTimeStamps.start_timestamp) return;
         [values.start_timestamp, values.end_timestamp] = [selectedTimeStamps.start_timestamp, selectedTimeStamps.end_timestamp];
-        alert(JSON.stringify(values, null, 2));
+        handleFormSubmit(values);
       }}
     >
-      {({ handleChange, handleBlur, handleSubmit, errors, touched, values }) => (
+      {({ handleChange, handleBlur, handleSubmit, values }) => (
         <View className="items-center">
           <RadioButton
             data={data}
             style={{ flex: 1, flexDirection: "row", flexWrap: "wrap", alignItems: "flex-start", justifyContent: "space-evenly", marginTop: 10 }}
             boxStyle={{ width: "40%", justifyContent: "space-evenly" }}
             textStyle={{ marginLeft: 10, fontSize: 12 }}
-            selectedBtn={(e) => setSelectedTimeStamp({ start_timestamp: e.start_timestamp, end_timestamp: e.end_timestamp })}
+            selectedBtn={(e) => handleSubmitTimeStamps({ start_timestamp: e.start_timestamp, end_timestamp: e.end_timestamp })}
             icon={<Icon name="check-circle" size={25} color={COLORS.blue} />}
           />
           {submitButtonTouched && !selectedTimeStamps.start_timestamp && <Text className="text-red-600 mt-2">Time Should be Specified</Text>}
