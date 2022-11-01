cbb3b54bdef60bf1cf5dadd0420ab4578e0eedb1
mohammadammash <mohammadammash31@gmail.com>
2022-11-01 20:06:53 +0200

    frontend/src/components/Common/ProfileScreen/Buttons.tsx: Extracting types, and states to parent screen

--- frontend/src/components/Common/ProfileScreen/Buttons.tsx
+++ frontend/src/components/Common/ProfileScreen/Buttons.tsx
@@ -1,22 +1,16 @@
 import { View, Text, Pressable } from "react-native";
-import { useNavigation } from "@react-navigation/native";
-import { useContext } from "react";
 //internal imports
 import styles from "../../../../styles";
 import { ROUTES, USERTYPES } from "../../../constants";
-import { UserContext } from "../../../hooks/UserContext";
+import { ButtonsComponentProps } from "../types";
 
-const Buttons = () => {
-  const navigation = useNavigation<any>();
-  const { user, setUser } = useContext(UserContext);
-  const { firstName, lastName, field, start_date, about, speciality, user_type } = user;
-
+const Buttons: React.FC<ButtonsComponentProps> = ({ user_type, navigateToPage }) => {
   return (
     <View className="w-full">
       {/* EXPERT VISITING NOVICE PROFILE*/}
       {user_type === USERTYPES.EXPERT && (
         <View className="flex-row w-full justify-center gap-2 my-1">
-          <Pressable style={styles.blue_button_sm} onPress={() => navigation.navigate(ROUTES.USER_SINGLE_CHAT)}>
+          <Pressable style={styles.blue_button_sm} onPress={() => navigateToPage(ROUTES.USER_SINGLE_CHAT)}>
             <Text className="text-white font-bold text-sm">MESSAGE</Text>
           </Pressable>
           <Pressable style={styles.blue_button_sm} onPress={() => alert("Blocked")}>
@@ -28,10 +22,10 @@ const Buttons = () => {
       {/* ADMIN PROFILE BUTTONS */}
       {user_type === USERTYPES.ADMIN && (
         <View className="flex-row w-full justify-center gap-2 my-1">
-          <Pressable style={styles.blue_button_sm} onPress={() => navigation.navigate(ROUTES.USER_EDIT_PROFILE)}>
+          <Pressable style={styles.blue_button_sm} onPress={() => navigateToPage(ROUTES.USER_EDIT_PROFILE)}>
             <Text className="text-white font-bold text-[10px]">EDIT PROFILE</Text>
           </Pressable>
-          <Pressable style={styles.blue_button_sm} onPress={() => navigation.navigate(ROUTES.ADMIN_VIEW_BANNED_USERS)}>
+          <Pressable style={styles.blue_button_sm} onPress={() => navigateToPage(ROUTES.ADMIN_VIEW_BANNED_USERS)}>
             <Text className="text-white font-bold text-[10px]">BANNED USERS</Text>
           </Pressable>
         </View>
@@ -40,10 +34,10 @@ const Buttons = () => {
       {/* NOVICE VISTING EXPERT PROFILE BUTTONS */}
       {user_type === USERTYPES.NOVICE && (
         <View className="flex-row justify-evenly my-2">
-          <Pressable style={styles.blue_button_sm} onPress={() => navigation.navigate(ROUTES.NOVICE_BOOK_APPOINTMENT)}>
+          <Pressable style={styles.blue_button_sm} onPress={() => navigateToPage(ROUTES.NOVICE_BOOK_APPOINTMENT)}>
             <Text className="text-white font-bold text-[10px]">BOOK</Text>
           </Pressable>
-          <Pressable style={styles.blue_button_sm} onPress={() => navigation.navigate(ROUTES.USER_SINGLE_CHAT)}>
+          <Pressable style={styles.blue_button_sm} onPress={() => navigateToPage(ROUTES.USER_SINGLE_CHAT)}>
             <Text className="text-white font-bold text-[10px]">MESSAGE</Text>
           </Pressable>
           <Pressable style={styles.blue_button_sm} onPress={() => alert("Blocked")}>
@@ -55,7 +49,7 @@ const Buttons = () => {
       {/* NOVICE VISITNG HIS OWN PROFILE */}
       {/* {user_type === USERTYPES.NOVICE && id === shownuser_id && (
         <View className="mt-5" style={styles.blue_button_lg}>
-          <Button color={COLORS.white} title="Edit Profile" onPress={() => navigation.navigate(ROUTES.USER_EDIT_PROFILE)} />
+          <Button color={COLORS.white} title="Edit Profile" onPress={() => navigateToPage(ROUTES.USER_EDIT_PROFILE)} />
         </View>
       )} */}
     </View>
@@ -64,10 +58,10 @@ const Buttons = () => {
 
 export default Buttons;
 
-  /* EDIT PROFILE BUTTON  */
-  /* {user_type === USERTYPES.EXPERT && (
+/* EDIT PROFILE BUTTON  */
+/* {user_type === USERTYPES.EXPERT && (
           <View>
-            <Pressable className="my-2" style={styles.blue_button_md} onPress={() => navigation.navigate(ROUTES.USER_EDIT_PROFILE)}>
+            <Pressable className="my-2" style={styles.blue_button_md} onPress={() => navigateToPage(ROUTES.USER_EDIT_PROFILE)}>
               <Text className="text-white font-bold text-base">EDIT PROFILE</Text>
             </Pressable>
           </View>
