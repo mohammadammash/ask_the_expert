8b114884b2116ebd17a39dc8a93740244941d54a
mohammadammash <mohammadammash31@gmail.com>
2022-11-01 20:10:56 +0200

    frontend/src/components/Expert/SingleReviewStat.tsx:  Extracting types and states to parent screen

--- frontend/src/components/Expert/SingleReviewStat.tsx
+++ frontend/src/components/Expert/SingleReviewStat.tsx
@@ -1,21 +1,23 @@
-import { View, Text } from 'react-native'
+import { View} from 'react-native'
 import React from 'react'
 import { AirbnbRating } from "react-native-ratings";
 import ProgressBar from "react-native-progress/Bar";
 import { COLORS } from '../../constants';
+import { ReviewSingleChatProps } from './types';
 
-interface ReviewSingleChatProps {
-  progress: number,
-  rating: number,
-}
-
-const ReviewSingleStat : React.FC<ReviewSingleChatProps> = ({progress, rating}) => {
+const ReviewSingleStat: React.FC<ReviewSingleChatProps> = ({ progress, rating }) => {
   return (
     <View className="flex-row h-10 items-center justify-center">
-      <AirbnbRating starContainerStyle={{ display: "flex", alignItems: "center", height: 80 }} size={20} defaultRating={rating} isDisabled reviews={[""]} />
+      <AirbnbRating
+        starContainerStyle={{ display: "flex", alignItems: "center", height: 80 }}
+        size={20}
+        defaultRating={rating}
+        isDisabled
+        reviews={[""]}
+      />
       <ProgressBar progress={progress} width={140} color={COLORS.blue} />
     </View>
   );
-}
+};
 
 export default ReviewSingleStat
