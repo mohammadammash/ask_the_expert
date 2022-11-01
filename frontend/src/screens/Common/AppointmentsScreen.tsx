a49b9ffe37781a8e6cb9075c492825f5c00d7162
mohammadammash <mohammadammash31@gmail.com>
2022-11-01 20:16:18 +0200

    frontend/src/screens/Common/AppointmentsScreen.tsx: Adding states and handling components logic

--- frontend/src/screens/Common/AppointmentsScreen.tsx
+++ frontend/src/screens/Common/AppointmentsScreen.tsx
@@ -1,15 +1,26 @@
+import { useNavigation } from "@react-navigation/native";
 import { View } from "react-native";
 //internal imports
 import { ChatAndAppointmentCardComponent } from "../../components";
 import { ScrollView } from "react-native-gesture-handler";
+import { ROUTES } from "../../constants";
 
 const AppointmentsScreen = () => {
+  const navigation = useNavigation<any>();
+  const NavigateToPage = () => {
+    // CHATS PAGE NAVIGATION
+    navigation.navigate(ROUTES.USER_SINGLE_CHAT);
+  };
+
+  // PARAMS
+  const data = {
+    NavigateToPage,
+  };
 
   return (
     <View className="flex-1 items-center bg-white">
       <ScrollView className="w-full  mt-10" contentContainerStyle={{ alignItems: "center" }}>
-
-        <ChatAndAppointmentCardComponent />
+        <ChatAndAppointmentCardComponent {...data} />
       </ScrollView>
     </View>
   );
