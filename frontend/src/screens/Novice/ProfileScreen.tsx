ca2744a5f5eb67e482b0d2da528d96f177b159cd
mohammadammash <mohammadammash31@gmail.com>
2022-11-01 09:53:22 +0200

    frontend/src/screens/Novice/ProfileScreen.tsx: Extracting buttons into button common component

--- frontend/src/screens/Novice/ProfileScreen.tsx
+++ frontend/src/screens/Novice/ProfileScreen.tsx
@@ -1,17 +1,11 @@
 import { useNavigation } from "@react-navigation/native";
-import { View, Text, Button, Image } from "react-native";
+import { View, } from "react-native";
 import { UserContext } from "../../hooks/UserContext";
 import { useContext } from "react";
 //internal imports:
-import { ROUTES, IMAGES, COLORS, USERTYPES } from "../../constants";
-import styles from "../../../styles";
-import { ProfilePersonalInfoComponent, ProfileImageCardComponent, AboutSectionComponent } from "../../components";
+import { ProfilePersonalInfoComponent, ProfileImageCardComponent, AboutSectionComponent, ButtonsComponent } from "../../components";
 
 const ProfileScreen = () => {
-  const navigation = useNavigation<any>();
-  const { user, setUser } = useContext(UserContext);
-  const shownuser_id = 22; //user_id
-  const { id, user_type } = user;
 
   return (
     <View className="flex-1 items-center bg-white">
@@ -21,16 +15,8 @@ const ProfileScreen = () => {
 
       <AboutSectionComponent />
 
-      {/* NOVICE VISITING HIS OWN PROFILE */}
-      {/* {user_type === USERTYPES.NOVICE && id === shownuser_id && (
-        <View className="mt-5" style={styles.blue_button_lg}>
-          <Button color={COLORS.white} title="Edit Profile" onPress={() => navigation.navigate(ROUTES.USER_EDIT_PROFILE)} />
-        </View>
-      )} */}
-
-      <View className="flex-1 items-center justify-center">
-        <Text className="text-slate-800">Novice Profile! 🎉</Text>
-      </View>
+      <ButtonsComponent/>
+        
     </View>
   );
 };
