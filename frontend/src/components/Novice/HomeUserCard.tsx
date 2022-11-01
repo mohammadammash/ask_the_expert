54c361cc44bb539a924f376d298ecdded6cb7786
mohammadammash <mohammadammash31@gmail.com>
2022-11-01 20:12:20 +0200

    frontend/src/components/Novice/HomeUserCard.tsx: Extracting types and states to parent screen

--- frontend/src/components/Novice/HomeUserCard.tsx
+++ frontend/src/components/Novice/HomeUserCard.tsx
@@ -1,15 +1,14 @@
 import { View, Text, TouchableOpacity, Image } from "react-native";
-import { useNavigation } from "@react-navigation/native";
 import { SimpleLineIcons } from "@expo/vector-icons"; 
 //internal imports
 import styles from "../../../styles";
 import { ROUTES, IMAGES, COLORS } from "../../constants";
+import { HomeUserCardProps } from "./types";
 
-const HomeUserCard = () => {
-  const navigation = useNavigation<any>();
 
+const HomeUserCard : React.FC<HomeUserCardProps> = ({navigateToPage}) => {
   return (
-    <TouchableOpacity style={styles.shadow_bg} className="flex-row w-5/6 rounded-xl border-0.5 items-center justify-evenly h-1/5" onPress={() => navigation.navigate(ROUTES.EXPERT_PROFILE)}>
+    <TouchableOpacity style={styles.shadow_bg} className="flex-row w-5/6 rounded-xl border-0.5 items-center justify-evenly h-1/5" onPress={() => navigateToPage(ROUTES.EXPERT_PROFILE)}>
       <View style={{ borderColor: COLORS.blue }} className="avatar aspect-square max-w-1/4 max-h-1/4 h-1/4 w-1/4 rounded-full items-center border-2">
         <Image className="max-w-full max-h-full h-full w-full rounded-full" source={IMAGES.dummyProfile} />
       </View>
