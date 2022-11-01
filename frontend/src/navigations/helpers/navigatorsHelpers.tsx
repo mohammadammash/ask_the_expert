8eca166aa83850d66331fa931db3b97706831174
mohammadammash <mohammadammash31@gmail.com>
2022-11-01 08:18:36 +0200

    frontend/src/navigations/helpers/navigatorsHelpers.tsx: Adding admin helper to same directory helper file

--- frontend/src/navigations/helpers/navigatorsHelpers.tsx
+++ frontend/src/navigations/helpers/navigatorsHelpers.tsx
@@ -1,8 +1,9 @@
-import { MaterialCommunityIcons, MaterialIcons, Entypo } from "@expo/vector-icons";
+import { MaterialCommunityIcons, MaterialIcons, Entypo, FontAwesome5 } from "@expo/vector-icons";
 import { TouchableOpacity } from "react-native";
 //internal imports
 import { COLORS } from "../../constants";
 
+// USED NAVIGATION ICONS
 export const homeIcon = <MaterialCommunityIcons name="home" size={24} color={COLORS.blue} />;
 export const appointmentsIcon = <MaterialCommunityIcons name="calendar-account-outline" size={24} color={COLORS.blue} />;
 export const leaderboardIcon = <MaterialIcons name="leaderboard" size={24} color={COLORS.blue} />;
@@ -11,6 +12,7 @@ export const settingsIcon = <MaterialCommunityIcons name="account-cog" size={24}
 export const menuIcon = (
   <MaterialCommunityIcons style={{ marginLeft: 20, marginBottom: 5 }} name="microsoft-xbox-controller-menu" size={32} color={COLORS.white} />
 );
+export const usersIcon = <FontAwesome5 name="users" size={28} color={COLORS.blue} />;
 
 // DRAWER NAVIGATOR: CUSTOM HEADER STYLE
 export const drawerScreenOptionsStyle = (navigation: any) => ({
@@ -34,4 +36,28 @@ export const stackScreenOptionsStyle = () => ({
   headerTitleStyle: {
     fontWeight: "bold",
   },
+});
+
+//TAB BOTTOM NAVIGATOR: SCREEN OPTIONS
+export const tabBarScreenOptions = (navigation: any) => ({
+  tabBarActiveTintColor: COLORS.blue,
+  tabBarActiveBackgroundColor: COLORS.blue,
+  tabBarStyle: {
+    backgroundColor: COLORS.dark,
+    height: 90,
+  },
+  tabBarItemStyle: {
+    backgroundColor: COLORS.grey,
+    margin: 5,
+    marginBottom: 0,
+    borderRadius: 5,
+  },
+  tabBarInactiveTintColor: COLORS.dark,
+  headerTitle: "",
+  tabBarIconStyle: {
+    backgroundColor: COLORS.blue,
+  },
+  tabBarIcon: () => (
+    <MaterialCommunityIcons style={{ marginLeft: 20, marginBottom: 5 }} name="microsoft-xbox-controller-menu" size={32} color={COLORS.blue} />
+  ),
 });
