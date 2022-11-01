a3f6e3eb40b514d5ddc17948ef449825a016cd25
mohammadammash <mohammadammash31@gmail.com>
2022-11-01 20:18:16 +0200

    frontend/src/screens/Novice/HomeScreen.tsx: Adding states and handling components logic

--- frontend/src/screens/Novice/HomeScreen.tsx
+++ frontend/src/screens/Novice/HomeScreen.tsx
@@ -1,17 +1,21 @@
 import { useNavigation } from "@react-navigation/native";
-import { View, Text, Button, Image, ImageBackground } from "react-native";
-import { ROUTES, IMAGES, COLORS } from "../../constants";
-import styles from "../../../styles";
+import { View ,ImageBackground } from "react-native";
+import {  IMAGES} from "../../constants";
 import { HomeUserCardComponent } from "../../components";
 
 const ProfileScreen = () => {
   const navigation = useNavigation<any>();
+  const navigateToPage = (routeName: string) => navigation.navigate(routeName);
+
+  const data = {
+    navigateToPage,
+  }
 
   return (
     <View className="flex-1 items-center bg-white">
       <ImageBackground className="w-full h-full" source={IMAGES.fakeMapImage}>
         <View className="flex-1 items-center justify-end mb-5">
-          <HomeUserCardComponent/>
+          <HomeUserCardComponent {...data}/>
         </View>
       </ImageBackground>
     </View>
