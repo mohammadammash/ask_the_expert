f9b8e05c29ddb0159b420e05d3c4645b3d95d9e5
mohammadammash <mohammadammash31@gmail.com>
2022-11-01 20:06:02 +0200

    frontend/src/components/Common/ProfileScreen/AboutSection.tsx: Extracting types, and state to parent screen

--- frontend/src/components/Common/ProfileScreen/AboutSection.tsx
+++ frontend/src/components/Common/ProfileScreen/AboutSection.tsx
@@ -1,16 +1,14 @@
 import { View, Text } from "react-native";
-import { UserContext } from "../../../hooks/UserContext";
-import { useContext } from "react";
+//internal imports
 import { USERTYPES, COLORS } from "../../../constants";
+import { AboutSectionProps } from "../types";
 
-const AboutSection = () => {
-  const { user, setUser } = useContext(UserContext);
-  const { firstName, lastName, field, start_date, about, speciality, user_type } = user;
 
+const AboutSection: React.FC<AboutSectionProps> = ({ user_type, about }) => {
   return (
     <View>
       {/* ABOUT SECTION */}
-      <View style={user.user_type === USERTYPES.NOVICE && { backgroundColor: COLORS.white, marginTop: 10 }} className="items-center pt-3 pb-7 px-3">
+      <View style={user_type === USERTYPES.NOVICE && { backgroundColor: COLORS.white, marginTop: 10 }} className="items-center pt-3 pb-7 px-3">
         <Text style={{ color: COLORS.blue }} className="font-bold text-2xl pb-3">
           ABOUT
         </Text>
