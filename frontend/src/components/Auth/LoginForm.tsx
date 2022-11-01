450242fab95df305c123fc46f85e6d3f67979c4f
mohammadammash <mohammadammash31@gmail.com>
2022-11-01 20:03:34 +0200

    frontend/src/components/Auth/LoginForm.tsx: Extracting states to parent screen

--- frontend/src/components/Auth/LoginForm.tsx
+++ frontend/src/components/Auth/LoginForm.tsx
@@ -1,44 +1,17 @@
 import { View, Text, TextInput, Pressable } from "react-native";
-import { useContext, useState } from "react";
-import { UserContext } from "../../hooks/UserContext";
 import styles from "../../../styles";
 import { Formik } from "formik";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth} from "../../../firebaseConfig";
-
+//internal imports
 import { validateLoginFormSchema, loginIntialValues } from "./helpers/loginFormHelper";
+import { LoginFormProps } from "./types";
 
-const LoginForm = () => {
-  const [inValidCredentials, setInValidCredentials] = useState(false);
-
-  const firbaseConfirmLogin = (email: string, password: string) => {
-    signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-        console.log("___SUCESSS________");
-        console.log("___SUCESSS________");
-        console.log(user);
-        console.log("___SUCESSS________");
-        console.log("___SUCESSS________");
-        alert(user);
-        // ...
-      })
-      .catch((error) => {
-        alert(error.code);
-        alert("Invalid Credentials");
-        setInValidCredentials(true);
-      });
-  };
-
+const LoginForm: React.FC<LoginFormProps> = ({ handleInvalidCredentials, inValidCredentials, firbaseConfirmLogin }) => {
   return (
     <Formik
       initialValues={loginIntialValues}
-      onSubmit={(values, actions) => {
-        setInValidCredentials(false);
+      onSubmit={(values) => {
+        handleInvalidCredentials(false);
         firbaseConfirmLogin(values.email, values.password);
-        // console.log({ values, actions });
-        // alert(JSON.stringify(values, null, 2));
       }}
       validationSchema={validateLoginFormSchema}
     >
@@ -46,13 +19,27 @@ const LoginForm = () => {
         <View className="w-4/5 h-64 justify-evenly">
           <View>
             <Text className="font-bold">Email</Text>
-            <TextInput onChangeText={handleChange("email")} onBlur={handleBlur("email")} value={values.email} style={styles.text_input} className="placeholder:pl-3" placeholder="Email" />
+            <TextInput
+              onChangeText={handleChange("email")}
+              onBlur={handleBlur("email")}
+              value={values.email}
+              style={styles.text_input}
+              className="placeholder:pl-3"
+              placeholder="Email"
+            />
             {errors.email && touched.email && <Text className="text-red-600">{errors.email}</Text>}
           </View>
 
           <View className="mt-2">
             <Text className="font-bold">Password</Text>
-            <TextInput onChangeText={handleChange("password")} onBlur={handleBlur("password")} value={values.password} style={styles.text_input} className="placeholder:pl-3" placeholder="Password" />
+            <TextInput
+              onChangeText={handleChange("password")}
+              onBlur={handleBlur("password")}
+              value={values.password}
+              style={styles.text_input}
+              className="placeholder:pl-3"
+              placeholder="Password"
+            />
             {errors.password && touched.password && <Text className="text-red-600  ">{errors.password}</Text>}
           </View>
 
