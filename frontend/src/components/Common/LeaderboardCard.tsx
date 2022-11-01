2397d917447459cfc7f2bd68f961e86546fc87d0
mohammadammash <mohammadammash31@gmail.com>
2022-11-01 08:08:43 +0200

    frontend/src/components/Common/LeaderboardCard.tsx: Adding shadow bg

--- frontend/src/components/Common/LeaderboardCard.tsx
+++ frontend/src/components/Common/LeaderboardCard.tsx
@@ -1,13 +1,14 @@
 import { View, Text, Image } from "react-native";
 import React from "react";
 import { IMAGES } from "../../constants";
+import styles from "../../../styles";
 //internal imports"
 
 const LeaderboardCard = ({ rank = "notTop" }): JSX.Element => {
   //ALL CARDS DESIGN EXCEPT TOP 1
   if (rank === "Top") {
     return (
-      <View className="w-4/6 rounded-lg border-4 relative justify-around items-center h-72 mb-10 border-[#E89923] pt-3">
+      <View style={styles.shadow_bg} className="w-full border-y-2 relative justify-around items-center h-72 my-10 border-[#E89923] pt-3">
         <View className="avatar aspect-square max-w-2/5 max-h-2/5 h-2/5 w-2/5 rounded-full items-center border-4 border-[#E89923]">
           <Image className="max-w-full max-h-full h-full w-full rounded-full" source={IMAGES.dummyProfile} />
         </View>
@@ -32,12 +33,12 @@ const LeaderboardCard = ({ rank = "notTop" }): JSX.Element => {
     //CARD RANK 2,3, and else DESIGN
   } else {
     return (
-      <View className="flex-row w-5/6 rounded-lg border-0.5 items-center justify-around h-24 mb-5">
+      <View  className="flex-row w-full border-0.5 items-center justify-around h-24 mb-2">
         <View className="avatar aspect-square max-w-1/5 max-h-1/5 h-2/5 w-1/5 rounded-full items-center ml-2 border-2">
           <Image className="max-w-full max-h-full h-full w-full rounded-full" source={IMAGES.dummyProfile} />
         </View>
 
-        <View className="h-full items-center justify-between pr-1 w-3/4 flex-row">
+        <View className="h-full items-center justify-between pr-3 w-3/4 flex-row">
           <View className="gap-1">
             <Text className="text-sm font-bold">Mohammad Ammash</Text>
             <Text className="text-xs opacity-50">Senior Web Developer</Text>
