e8b583de71d755026ad1ebc76fb36c83ecae0fc8
mohammadammash <mohammadammash31@gmail.com>
2022-11-02 13:35:59 +0200

    frontend/src/screens/auth/RegisterScreen.tsx: Adding handling states and forms to the screen

--- frontend/src/screens/auth/RegisterScreen.tsx
+++ frontend/src/screens/auth/RegisterScreen.tsx
@@ -1,13 +1,66 @@
-import { useNavigation } from "@react-navigation/native";
-import { View, Image, ScrollView } from "react-native";
+import { View, ScrollView } from "react-native";
+import { useState } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+//internal imports
 import { RegisterFormComponent } from "../../components";
-import { IMAGES } from "../../constants";
+import { auth } from "../../../firebaseConfig";
+import { uploadImageAsync, pickImage } from "../helpers/imageHandlerHelper";
+import { RegisterFormValuesTypes } from "../../components/Auth/types";
 
 const RegisterScreen = () => {
+  //FORM VALUES
+  const [selectedLanguages, setSelectedLanguages] = useState<string[]>([]);
+  const [emailAlreadyUsed, setEmailAlreadyUsed] = useState(false);
+  const [image, setImage] = useState("");
+  const showImage = async () => {
+    const newImage = await pickImage();
+    setImage(newImage);
+  };
+  const handleSelectedLanguages = (languages: string[]) => setSelectedLanguages(languages);
+
+  //REGISTER AUTH
+  const firebaseSubmitSignup = async (email: string, password: string) => {
+    await createUserWithEmailAndPassword(auth, email, password)
+      .then(async (userCredential) => {
+        const user = userCredential.user;
+        if (image) {
+          const profileimage_url = await uploadImageAsync(image, email);
+          setImage(profileimage_url);
+        }
+        console.log(user.uid);
+      })
+
+      .catch((error) => {
+        if (error.code == "auth/email-already-in-use") {
+          alert("Email already in use");
+          setEmailAlreadyUsed(true);
+        }
+      });
+  };
+
+  //SUBMIT FORM
+  const handleFormSubmit = async (values: RegisterFormValuesTypes) => {
+    if (image) values.profile_url = image;
+    await firebaseSubmitSignup(values.email, values.password);
+    //do something with values
+    alert(values);
+  };
+
+  //PARAMS
+  const data = {
+    firebaseSubmitSignup,
+    image,
+    showImage,
+    emailAlreadyUsed,
+    handleSelectedLanguages,
+    selectedLanguages,
+    handleFormSubmit,
+  };
+
   return (
     <ScrollView>
       <View className="items-center mt-3">
-        <RegisterFormComponent />
+        <RegisterFormComponent {...data} />
       </View>
     </ScrollView>
   );
