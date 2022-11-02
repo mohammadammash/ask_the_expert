7451b3c1a0b54aed361217492bc6bd28bb610afe
mohammadammash <mohammadammash31@gmail.com>
2022-11-02 13:35:47 +0200

    frontend/src/screens/auth/LoginScreen.tsx: Adding handling states and forms to the screen

--- frontend/src/screens/auth/LoginScreen.tsx
+++ frontend/src/screens/auth/LoginScreen.tsx
@@ -1,19 +1,49 @@
-import { View, Text, Pressable, TextInput, Image } from "react-native";
+import { View, Text, Pressable, Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+//internal imports
 import { ROUTES, IMAGES } from "../../constants";
 import { LoginFormComponent } from "../../components";
 import styles from "../../../styles";
+import { auth } from "../../../firebaseConfig";
 
 const LoginScreen = () => {
-  const navigation = useNavigation();
+  const navigation = useNavigation<any>();
 
+  //FORM DATA
+  const [inValidCredentials, setInValidCredentials] = useState(false);
+  const handleInvalidCredentials = (value: boolean) => setInValidCredentials(value);
+
+  //FIREBASE AUTH
+  const firbaseConfirmLogin = (email: string, password: string) => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        alert(user);
+      })
+      .catch((error) => {
+        alert(error.code);
+        alert("Invalid Credentials");
+        handleInvalidCredentials(true);
+      });
+  };
+
+  //PARAMS
+  const data = {
+    inValidCredentials,
+    handleInvalidCredentials,
+    firbaseConfirmLogin,
+  };
+
+  //MAIN COMPONENT
   return (
     <View className="flex-1 items-center bg-white">
       <View className="w-full mb-2 h-1/2">
         <Image className="max-w-full max-h-full" source={IMAGES.logo_500px} />
       </View>
 
-      <LoginFormComponent />
+      <LoginFormComponent {...data}/>
 
       <View className="flex-row mt-10">
         <Text className="inline mr-1 font-bold">Don't have an account?</Text>
