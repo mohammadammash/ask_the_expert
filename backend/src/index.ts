d02af05bba6dca75a9bbc8e1b1290503870500ff
mohammadammash <mohammadammash31@gmail.com>
2022-11-02 21:38:05 +0200

    backend/src/index.ts: Adding expert main route and middlewares

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,12 +11,16 @@ app.use(cors());
 //ROUTES
 const authMiddleware = require("./middlewares/auth");
 const checkIfNoviceMiddleware = require("./middlewares/checkIfNovice");
+const checkIfExpertMiddleware = require("./middlewares/checkIfExpert");
 
 const authRoutes = require("./routes/auth");
 app.use("/", authRoutes);
 
 const noviceRoutes = require("./routes/novice");
 app.use("/novice", authMiddleware, checkIfNoviceMiddleware, noviceRoutes);
+
+const expertRoutes = require("./routes/expert");
+app.use("/expert", authMiddleware, checkIfExpertMiddleware, expertRoutes);
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log('Running on ' + process.env.SERVER_PORT);
