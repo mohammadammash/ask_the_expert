dd2238b94301b61b73fec75839a530d93a41eb44
mohammadammash <mohammadammash31@gmail.com>
2022-11-02 20:57:40 +0200

    backend/src/index.ts: Importing noviceRoutes and intialize /novice default to

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 const app: Express = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -9,9 +9,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 //ROUTES
+const authMiddleware = require("./middlewares/auth");
+const checkIfNoviceMiddleware = require("./middlewares/checkIfNovice");
+
 const authRoutes = require("./routes/auth");
 app.use("/", authRoutes);
 
+const noviceRoutes = require("./routes/novice");
+app.use("/novice", authMiddleware, checkIfNoviceMiddleware, noviceRoutes);
 
 app.listen(process.env.SERVER_PORT, () => {
     console.log('Running on ' + process.env.SERVER_PORT);
