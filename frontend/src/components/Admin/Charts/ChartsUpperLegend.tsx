d096af67dd67e5fc475348af40fdfd3f12ccdd47
mohammadammash <mohammadammash31@gmail.com>
2022-11-02 13:31:37 +0200

    frontend/src/components/Admin/Charts: Extracting Charts upper and legend to charts folder

--- frontend/src/components/Admin/Charts/ChartsUpperLegend.tsx
+++ frontend/src/components/Admin/Charts/ChartsUpperLegend.tsx
@@ -1,16 +1,16 @@
 import { View, Text } from "react-native";
 //internal
-import { COLORS } from "../../../../constants";
+import { COLORS } from "../../../constants";
 
 interface ChartsUpperLegendProps {
-  chart_type: string,
-  dot1_title: string,
-  dot2_title: string,
+  chart_type: string;
+  dot1_title: string;
+  dot2_title: string;
 }
 
 const ChartsUpperLegend: React.FC<ChartsUpperLegendProps> = ({ chart_type, dot1_title, dot2_title }): JSX.Element => {
   const firstDotColor = chart_type === "newUsers" ? "#BDB2FA" : COLORS.orange;
-  const chartTitle =  chart_type === "newUsers" ? "New Users" : "New Appointments And Chats";
+  const chartTitle = chart_type === "newUsers" ? "New Users" : "New Appointments And Chats";
 
   return (
     <View className="my-8">
