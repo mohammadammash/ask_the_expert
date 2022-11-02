b16c8a622396376d585e977c6049a9c40af7dafc
mohammadammash <mohammadammash31@gmail.com>
2022-11-02 19:58:33 +0200

    backend/src/database/models/Appointment.ts: Intializing appointment model

---
+++ backend/src/database/models/Appointment.ts
@@ -0,0 +1,34 @@
+import mongoose from "../connection";
+
+const appointmentSchema = new mongoose.Schema({
+    novice_id: {
+        type: String,
+        ref: 'User'
+    },
+    expert_id: {
+        type: String,
+        ref: 'Expert'
+    },
+    isReserved:{
+        type: Boolean,
+        required: 'isReserved is required'
+    },
+    start_timestamp: {
+        type: Date,
+        required: 'Start Timestamp is required', 
+    },
+    end_timestamp: {
+        type: Date,
+        required: 'End Timestamp is required', 
+    },
+    notes: {
+        type: String,
+        required: 'Notes are required',
+    },
+},
+    { timestamps: true } //for admin stats
+);
+
+const AppointmentModel = mongoose.model("Appointment", appointmentSchema);
+
+module.exports = AppointmentModel;
