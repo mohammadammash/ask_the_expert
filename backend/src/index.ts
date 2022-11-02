480bb664ffad19a13c6bc20ef528e19aedc77705
mohammadammash <mohammadammash31@gmail.com>
2022-11-02 17:29:51 +0200

    backend: Installing body parser and cors, and requiring both in 'src/index.ts'

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,18 @@
 import express, {Express, Request, Response} from "express";
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
 const app: Express = express();
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 app.get('/',(req: Request, res: Response)=>{
     res.send('Typescript runs well');
 })
 
-app.listen(4321, ()=>{
-    console.log('Running on 4321');
+app.listen(process.env.SERVER_PORT, ()=>{
+    console.log('Running on 3000');
 });
