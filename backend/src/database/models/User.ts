a8ae0bc8ca90333a8341d33e68cad53b96421a60
mohammadammash <mohammadammash31@gmail.com>
2022-11-02 19:57:50 +0200

    backend/src/database/models/User.ts: Intializing user model

---
+++ backend/src/database/models/User.ts
@@ -0,0 +1,86 @@
+import mongoose from "../connection";
+
+const userSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    required: 'id is required'
+  },
+  firstName: {
+    type: String,
+    required: "firstName is required",
+  },
+  lastName: {
+    type: String,
+    required: "lastName is required",
+  },
+  email: {
+    type: String,
+    required: "email is required",
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: "password is required",
+    select: false,
+  },
+  spoken_languages: {
+    type: String,
+    required: "spoken languages is required",
+  },
+  about: {
+    type: String,
+    required: "About is required",
+  },
+  profile_url: {
+    type: String,
+    required: "profile_url is required",
+  },
+  field: {
+    type: String,
+    required: "Field is required",
+  },
+  speciality: {
+    type: String,
+    required: 'Speciality is required',
+  },
+  app_language: {
+    type: String,
+    required: "Field is required",
+  },
+  theme: {
+    type: String,
+    required: 'Theme is required',
+  },
+  user_type: {
+    type: String,
+    required: 'User Type is required',
+  },
+  isBanned: {
+    type: Boolean,
+    required: 'isBanned is required'
+  },
+  blocked_experts: [
+    {
+      type: String,
+      ref: "Expert",
+    }
+  ],
+  appointments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Appointment'
+    }
+  ],
+  reviews: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Review'
+    }
+  ],
+},
+  { timestamps: true }
+);
+
+const UserModel = mongoose.model("User", userSchema);
+
+module.exports = UserModel;
