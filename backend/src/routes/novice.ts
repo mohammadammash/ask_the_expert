9cfe35e7db484e98977327d1cf8302c4081b2d15
mohammadammash <mohammadammash31@gmail.com>
2022-11-02 20:59:25 +0200

    backend/src/routes/novice.ts: Intializing novice routes with relevant functions needed

---
+++ backend/src/routes/novice.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+const router = Router();
+
+const {
+    getCloseExperts, bookAppointment, addReview, updateReview, deleteReview,
+} = require("../controllers/novice");
+
+router.get("/", getCloseExperts);
+router.post("/book", bookAppointment);
+router.post("/review", addReview); 
+router.put("/review", updateReview); 
+router.delete("/review", deleteReview); 
+
+module.exports = router;
