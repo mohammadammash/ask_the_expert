82adbc15d0a6895b5ef4ed925ec1ca414a1faaba
mohammadammash <mohammadammash31@gmail.com>
2022-11-03 09:46:22 +0200

    backend/src/controllers/auth.ts: Adding register typescript interface and capturing req.body

--- backend/src/controllers/auth.ts
+++ backend/src/controllers/auth.ts
@@ -4,16 +4,32 @@ const bcrypt = require("bcrypt");
 const UserModel = require('../database/models/User');
 const ExpertModel = require('../database/models/Expert');
 
-const registerUserOrExpert = async (req: Request,res: Response)=>{
-    res.send({message: 'hell register'});
+interface RegisterBodyInterface {
+    _id: String,
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: String,
+    spoken_languages: String,
+    about: String,
+    start_date: String,
+    profile_url: String,
+    field: String,
+    speciality: String,
 }
 
-const loginUserOrExpert = async (req: Request, res: Response)=>{
-    res.send({ message: 'hell login' });
+const registerUserOrExpert = async (req: Request<{}, {}, RegisterBodyInterface>, res: Response) => {
+    const { _id, firstName, lastName, email, password, spoken_languages, about, start_date, profile_url, field, speciality } = req.body;
 
+    res.send({ _id, firstName, lastName, email, password, spoken_languages, about, start_date, profile_url, field, speciality });
+}
+
+const loginUserOrExpert = async (req: Request, res: Response) => {
+    res.send({ message: 'hell login' });
 }
 
 module.exports = {
     registerUserOrExpert,
     loginUserOrExpert,
 };
+
