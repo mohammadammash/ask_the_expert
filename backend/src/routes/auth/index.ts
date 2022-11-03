455ba7dd6c10e2773ad7d0cdd01949d092cd756d
mohammadammash <mohammadammash31@gmail.com>
2022-11-03 13:16:31 +0200

    backend/src/routes/auth/validator.ts: Adding login request validation

--- backend/src/routes/auth/index.ts
+++ backend/src/routes/auth/index.ts
@@ -1,12 +1,12 @@
-import { body } from "express-validator";
 import { Router } from "express";
 const router = Router();
 //internal imports:
 const { registerUserOrExpert, loginUserOrExpert } = require('../../controllers/auth');
-const { registerValidationRules, registerValidateMiddleware } = require('./validator');
+const { registerValidationRules, loginValidationRules } = require('./validator');
+const validateRequestMiddleware = require("../../middlewares/validateRequest");
 
 
-router.post("/register", registerValidationRules(), registerValidateMiddleware, registerUserOrExpert);
-router.post("/login", loginUserOrExpert);
+router.post("/register", registerValidationRules(), validateRequestMiddleware, registerUserOrExpert);
+router.post("/login", loginValidationRules(), validateRequestMiddleware, loginUserOrExpert);
 
 module.exports = router;
--- backend/src/routes/auth/validator.ts
+++ backend/src/routes/auth/validator.ts
@@ -1,5 +1,4 @@
-import { Request, Response, NextFunction } from "express"
-const { body, validationResult } = require('express-validator')
+const { body } = require('express-validator')
 
 const registerValidationRules = () => {
     return [
@@ -10,21 +9,18 @@ const registerValidationRules = () => {
         body('password').isStrongPassword({ minLength: 6, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1, returnScore: false, }),
         body('languages').not().isEmpty().withMessage('spoken_languages cannot be empty'),
         body('about').not().isNumeric().withMessage('Cannot be numbers').isLength({ min: 20, max: 250 }).withMessage('20 < < 250'),
-        body('start_date').isLength({min: 8}).withMessage('Date contains at least 8 charcs'),
+        body('start_date').isLength({ min: 8 }).withMessage('Date contains at least 8 charcs'),
         body('field').not().isEmpty().withMessage('cannot be empty'),
         body('speciality').not().isEmpty().withMessage('cannot be empty'),
     ]
 }
 
-const registerValidateMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    const errors = validationResult(req)
-    if (errors.isEmpty()) next()
-    else return res.status(422).send({ errors: errors.array() })
+const loginValidationRules = () => {
+    return [
+        body('_id').not().isEmpty().withMessage('cannot be empty'),
+        body('email').isEmail().withMessage('Invalid email'),
+        body('password').isStrongPassword({ minLength: 6, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1, returnScore: false, }),
+    ]
 }
 
-module.exports = {
-    registerValidationRules,
-    registerValidateMiddleware,
-}
-//                                     field: String,
-//                                         speciality: String,
+module.exports = { registerValidationRules, loginValidationRules }
