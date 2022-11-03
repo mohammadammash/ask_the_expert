d9d7dd0faeaffefbbf5e2d498408e841d32d6ce2
mohammadammash <mohammadammash31@gmail.com>
2022-11-03 18:24:55 +0200

    backend/src/controllers/auth/index.ts: Creating login for prev user

--- backend/src/controllers/auth/index.ts
+++ backend/src/controllers/auth/index.ts
@@ -5,7 +5,6 @@ const UserModel = require('../../database/models/User');
 import { LoginBodyInterface, RegisterBodyInterface, UserInfoObjectType } from "./types";
 import getMonthDifferenceHelper from "./helpers/getMonthDifference";
 
-
 const registerUserOrExpert = async (req: Request<{}, {}, RegisterBodyInterface>, res: Response) => {
     try {
         //to be removed and replaced by 'start_date': get start Year
@@ -13,11 +12,12 @@ const registerUserOrExpert = async (req: Request<{}, {}, RegisterBodyInterface>,
         date.setDate(date.getDate() - 500);
         const yearsDiff = (getMonthDifferenceHelper(date) / 12);
 
-        const userData: UserInfoObjectType = { theme: '', app_language: '', user_type: '', isBanned: false, blocked_users: [], reviews: [] };
+        const password = await bcrypt.hash(req.body.password, 10);
+        const userData: UserInfoObjectType = { theme: '', app_language: '', user_type: '', isBanned: false, blocked_users: [], reviews: [], password };
         if (yearsDiff < 4) [userData.user_type, userData.appointments] = ['novice', []];
         else[userData.user_type, userData.appointments_groups, userData.score, userData.isAvailable, userData.location] = ['expert', [], 0, false, ''];
 
-        const newUser = new UserModel({ ...userData, ...req.body });
+        const newUser = new UserModel({ ...req.body, ...userData });
         await newUser.save();
 
         res.status(200).send(newUser);
@@ -30,8 +30,18 @@ const registerUserOrExpert = async (req: Request<{}, {}, RegisterBodyInterface>,
 }
 
 
-const loginUserOrExpert = async (req: Request, res: Response) => {
-    res.send({ message: 'hell login' });
+const loginUserOrExpert = async (req: Request<{}, {}, LoginBodyInterface>, res: Response) => {
+    const { email, password, _id } = req.body;
+
+    const user = await UserModel.findOne({ _id, email }).select("+password");
+    if (!user) res.status(400).send({ message: "Invalid Credentials" });
+
+    const matchingPassword = await bcrypt.compareSync(password, user.password);
+    if (!matchingPassword) res.status(400).send({ message: "Invalid Credentials" });
+    else {
+        user.password = undefined; //remove pass from result
+        res.status(200).send({ user: user });
+    }
 }
 
 module.exports = {
