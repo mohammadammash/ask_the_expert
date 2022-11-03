2e8ce96ab8af3243f3c9eb2edf7cde6ed34c63f0
mohammadammash <mohammadammash31@gmail.com>
2022-11-03 17:49:44 +0200

    backend/src/controllers/auth/types.ts: Creating types file for auth controllers

---
+++ backend/src/controllers/auth/types.ts
@@ -0,0 +1,29 @@
+export interface LoginBodyInterface {
+    email: String,
+    password: String,
+};
+export interface RegisterBodyInterface extends LoginBodyInterface {
+    _id: String,
+    firstName: String,
+    lastName: String,
+    spoken_languages: String,
+    about: String,
+    start_date: String,
+    profile_url: String,
+    field: String,
+    speciality: String,
+}
+
+export type UserInfoObjectType = {
+    theme: String,
+    app_language: String,
+    user_type: String,
+    isBanned: Boolean,
+    blocked_users: string[],
+    reviews: string[],
+    appointments?: string[],
+    appointments_groups?: string[],
+    score?: Number,
+    isAvailable?: Boolean,
+    location?: String,
+}
