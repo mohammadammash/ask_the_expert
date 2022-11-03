ee90fb5e5b4d28870829c5c63021aaeb8ccfa06e
mohammadammash <mohammadammash31@gmail.com>
2022-11-03 20:34:37 +0200

    backend/src/middlewares/auth.ts: Adding auth middleware jwt authorization check, and add currentuser_id as type in typescript

--- backend/src/middlewares/auth.ts
+++ backend/src/middlewares/auth.ts
@@ -2,8 +2,19 @@ import { NextFunction, Request, Response } from "express";
 const jwt = require("jsonwebtoken");
 
 const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
-    if (false) res.json({message:'authmiddleware'});
-    else next();
+    let token = req.headers.authorization;
+
+    if (!token) return res.status(401).json({ message: "Unauthorized" });
+    token = token.split(" ")[1]; //remove Bearer
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+        req.currentUser_id = decoded._id;
+        next();
+    } catch (err) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
 };
 
 module.exports = authMiddleware;
