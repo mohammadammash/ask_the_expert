5dc29f8e444a2bdf8aea53aaa6f2118e4011ae87
mohammadammash <mohammadammash31@gmail.com>
2022-11-03 13:04:01 +0200

    backend/src/routes/auth/validator.ts: Validating request body data

--- backend/src/routes/auth/validator.ts
+++ backend/src/routes/auth/validator.ts
@@ -3,9 +3,16 @@ const { body, validationResult } = require('express-validator')
 
 const registerValidationRules = () => {
     return [
-        body('email').isEmail(),
-        body('firstName').isLength({ min: 5 }),
-        
+        body('_id').not().isEmpty().withMessage('cannot be empty'),
+        body('firstName').isLength({ min: 2, max: 50 }).withMessage('3 < < 50 charcs'),
+        body('lastName').isLength({ min: 2, max: 50 }).withMessage('3 < < 50 charcs'),
+        body('email').isEmail().withMessage('Invalid email'),
+        body('password').isStrongPassword({ minLength: 6, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1, returnScore: false, }),
+        body('languages').not().isEmpty().withMessage('spoken_languages cannot be empty'),
+        body('about').not().isNumeric().withMessage('Cannot be numbers').isLength({ min: 20, max: 250 }).withMessage('20 < < 250'),
+        body('start_date').isLength({min: 8}).withMessage('Date contains at least 8 charcs'),
+        body('field').not().isEmpty().withMessage('cannot be empty'),
+        body('speciality').not().isEmpty().withMessage('cannot be empty'),
     ]
 }
 
@@ -19,3 +26,5 @@ module.exports = {
     registerValidationRules,
     registerValidateMiddleware,
 }
+//                                     field: String,
+//                                         speciality: String,
