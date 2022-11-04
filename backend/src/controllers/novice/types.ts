7e39d3b777405a0a8f154f4bb3e460be71994a72
mohammadammash <mohammadammash31@gmail.com>
2022-11-04 19:48:09 +0200

    backend/src/controllers/novice/types.ts: Adding types of bookAppointment and getcloseExperts interfaces

---
+++ backend/src/controllers/novice/types.ts
@@ -0,0 +1,25 @@
+import { Number, ObjectId } from "mongoose";
+
+export interface getCloseExpertsBodyInterface {
+    latitude: Number,
+    longitude: Number,
+}
+
+export interface bookAppointmentBodyInterface {
+    expert_id: String,
+    appointment_id: ObjectId,
+};
+
+export interface addReviewBodyInterface {
+    content: String,
+    rating: Number,
+    expert_id: String,
+}
+
+export interface updateReviewBodyInterface{
+    score_to_add: Number
+}
+
+export interface deleteReviewBodyInterface {
+    score_to_add: Number
+}
