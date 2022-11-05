6437bf006c5aa93ac362b7107dee4813e3d5db57
mohammadammash <mohammadammash31@gmail.com>
2022-11-05 20:27:07 +0200

    backend/src/middlewares/checkIfExpert.ts: Adding functionality to expert middleware

--- backend/src/middlewares/checkIfExpert.ts
+++ backend/src/middlewares/checkIfExpert.ts
@@ -1,8 +1,15 @@
 import { NextFunction, Request, Response } from "express";
+//internal imports
+const UserModel = require('../database/models/User');
 
 const checkIfExpert = async (req: Request, res: Response, next: NextFunction) => {
-    if (false) res.json({ message: 'checkifexpert' });
-    else next();
+    const { currentUser_id } = req;
+
+    const currentUser = await UserModel.findOne({ _id: currentUser_id });
+    if (!currentUser) return res.status(401).send({ message: 'Unauthorized' })
+
+    if (currentUser.user_type === 'expert') next();
+    else res.status(401).send({ message: 'Unauthorized' });
 };
 
 module.exports = checkIfExpert;
