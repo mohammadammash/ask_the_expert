377f8187791693ba5d3c8441a96ad99a66a5b0a4
mohammadammash <mohammadammash31@gmail.com>
2022-11-05 19:39:37 +0200

    backend/src/routes/user/index.ts: Removing unused route

--- backend/src/routes/user/index.ts
+++ backend/src/routes/user/index.ts
@@ -1,15 +1,14 @@
 import { Router } from "express";
 const router = Router();
 //internal imports
-const { getRankedExperts, updateProfile, getCurrentUserAppointments, removeAppointment, blockUser, unBlockUser, getUsersData } = require("../controllers/user");
-const { getUsersDataValidationRules, getCurrentUserAppointmentsValidationRules, updateProfileValidationRules, blockOrUnblockUserValidationRules, removeAppoointmentValidationRules } = require('./validator');
+const { getRankedExperts, updateProfile, removeAppointment, blockOrUnblockUser, getUsersData } = require("../../controllers/user");
+const { getUsersDataValidationRules, updateProfileValidationRules, blockOrUnblockUserValidationRules, removeAppointmentValidationRules } = require('./validator');
 const validateRequestMiddleware = require('../../middlewares/validateRequest')
 
 router.get("/leaderboard", getRankedExperts);
 router.get('/users', getUsersDataValidationRules(), validateRequestMiddleware, getUsersData); //for chats firebase table users
-router.get('/appointments', getCurrentUserAppointmentsValidationRules(), validateRequestMiddleware, getCurrentUserAppointments);
 router.put("/", updateProfileValidationRules(), validateRequestMiddleware, updateProfile);
-router.put('/block', blockOrUnblockUserValidationRules(), validateRequestMiddleware, blockUser);
-router.delete('/appointment', removeAppoointmentValidationRules(), validateRequestMiddleware, removeAppointment)
+router.put('/block', blockOrUnblockUserValidationRules(), validateRequestMiddleware, blockOrUnblockUser);
+router.delete('/appointment', removeAppointmentValidationRules(), validateRequestMiddleware, removeAppointment)
 
 module.exports = router;
