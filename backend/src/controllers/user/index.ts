bc66306b8ac9368fad50b30ec28cbf24eef99e5d
mohammadammash <mohammadammash31@gmail.com>
2022-11-05 18:42:17 +0200

    backend/src/controllers/user/index.ts: Adding removeAppointment functinality

--- backend/src/controllers/user/index.ts
+++ backend/src/controllers/user/index.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from "express";
 //internal imports
-import { getCurrentUserAppointmentsBodyInterface, getUsersDataBodyInterface, removeAppointmentBodyInterface, blockOrUnblockUserBodyInterface, updateProfileBodyInterface } from "./types";
+import { getUsersDataBodyInterface, removeAppointmentBodyInterface, blockOrUnblockUserBodyInterface, updateProfileBodyInterface } from "./types";
 const UserModel = require('../../database/models/User');
+const AppointmentModel = require('../../database/models/Appointment');
 
 const getRankedExperts = async (req: Request, res: Response) => {
     //get experts sorted where score more than 0
@@ -14,16 +15,23 @@ const updateProfile = async (req: Request<{}, {}, updateProfileBodyInterface>, r
     const { currentUser_id } = req;
 
     await UserModel.findByIdAndUpdate(currentUser_id, { ...req.body })
-        .then((data: any) => res.status(200).send({message: 'Success'}))
+        .then((data: any) => res.status(200).send({ message: 'Success' }))
         .catch((err: any) => res.status(400).send({ message: err.message }))
 };
 
-const getCurrentUserAppointments = async (req: Request<{}, {}, getCurrentUserAppointmentsBodyInterface>, res: Response) => {
-    res.send({ message: 'getCurrentUSerAppointments' });
-};
-
 const removeAppointment = async (req: Request<{}, {}, removeAppointmentBodyInterface>, res: Response) => {
-    res.send({ message: 'removeAppointment' });
+    const { currentUser_id } = req;
+    const { appointment_id } = req.body;
+
+    const appointmentRemoved = await AppointmentModel.findByIdAndDelete(appointment_id);
+    //if novice removed the appointment notify the expert
+    if(appointmentRemoved.novice_id === currentUser_id){
+        res.status(200).send({message: "lets notify the expert"});
+    }
+    //if expert removed the appointment notify the novice
+    else if(appointmentRemoved.expert_id === currentUser_id){
+        res.status(200).send({ message: "lets notify the novice" });
+    }
 };
 
 const blockOrUnblockUser = async (req: Request<{}, {}, blockOrUnblockUserBodyInterface>, res: Response) => {
@@ -34,4 +42,4 @@ const getUsersData = async (req: Request<{}, {}, getUsersDataBodyInterface>, res
     res.send({ message: 'getUsersDataForChatsFromFirebase' });
 };
 
-module.exports = { getRankedExperts, updateProfile, getCurrentUserAppointments, removeAppointment, blockOrUnblockUser, getUsersData };
+module.exports = { getRankedExperts, updateProfile, removeAppointment, blockOrUnblockUser, getUsersData };
