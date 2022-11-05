be623b949ffba5e35dd20fbf99bef31e46fa18ed
mohammadammash <mohammadammash31@gmail.com>
2022-11-05 20:27:36 +0200

    backend/src/middlewares/checkIfNovice.ts: Adding functionality to novice check middleware

--- backend/src/middlewares/checkIfNovice.ts
+++ backend/src/middlewares/checkIfNovice.ts
@@ -1,8 +1,15 @@
 import { NextFunction, Request, Response } from "express";
+//internal imports
+const UserModel = require('../database/models/User');
 
 const checkIfNovice = async (req: Request, res: Response, next: NextFunction) => {
-    if (false) res.json({ message: 'checkifnovice' });
-    else next();
+    const { currentUser_id } = req;
+
+    const currentUser = await UserModel.findOne({ _id: currentUser_id });
+    if (!currentUser) return res.status(401).send({ message: 'Unauthorized' })
+    
+    if (currentUser.user_type === 'novice') next()
+    else res.status(401).send({ message: 'Unauthorized' });
 };
 
 module.exports = checkIfNovice;
