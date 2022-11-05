2bf1a90bea3781c07156dca1548a8900c7c93563
mohammadammash <mohammadammash31@gmail.com>
2022-11-05 17:09:48 +0200

    backend/src/controllers/user/types.ts: Adding editProfile props types

--- backend/src/controllers/user/types.ts
+++ backend/src/controllers/user/types.ts
@@ -1,8 +1,14 @@
-import { Number, ObjectId } from "mongoose";
+import { Number } from "mongoose";
 
 export interface updateProfileBodyInterface {
-    expert_id: String,
-    appointment_id: ObjectId,
+    firstName: string;
+    lastName: string;
+    about?: string; //optional for admin
+    speciality?: string; //optional for admin
+    profile_url: string,
+    spoken_languages?: string[]; //optional for admin
+    app_language: string,
+    theme: string,
 };
 
 export interface getCurrentUserAppointmentsBodyInterface {
@@ -18,7 +24,6 @@ export interface removeAppointmentBodyInterface {
 export interface blockOrUnblockUserBodyInterface {
     expert_id: String,
 }
-
 
 export interface getUsersDataBodyInterface {
     expert_id: String,
