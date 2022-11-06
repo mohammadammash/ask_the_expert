98244e025d6d26d7f7409f41e3d0b8bfa9364cc4
mohammadammash <mohammadammash31@gmail.com>
2022-11-06 14:21:10 +0200

    backend/src/controllers/expert/index.ts: Adding goOffline to use when expert choose to goOffline before whole meetings time ends

--- backend/src/controllers/expert/index.ts
+++ backend/src/controllers/expert/index.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 const mongoose = require('mongoose');
 //internal imports
-import { GoOnlineBodyInterface, AddScoreBodyInterface } from "./types";
+import { GoOnlineBodyInterface, AddScoreBodyInterface, } from "./types";
 const AppointmentModel = require("../../database/models/Appointment");
 const UserModel = require("../../database/models/User");
 
@@ -35,17 +35,39 @@ const goOnline = async (req: Request<{}, {}, GoOnlineBodyInterface>, res: Respon
     // Add newly created Appointments ids to Appointment group of the expert
     if (appointments_ids) {
         const _id = new mongoose.Types.ObjectId();
-        const appointment_group = { _id:_id, start_timestamp: added_appointments[0].start_timestamp, isActive: true, end_timestamp: added_appointments[added_appointments.length - 1].end_timestamp, appointments: [...appointments_ids] };
+        const appointment_group = { _id: _id, start_timestamp: added_appointments[0].start_timestamp, isActive: true, end_timestamp: added_appointments[added_appointments.length - 1].end_timestamp, appointments: [...appointments_ids] };
 
-        await UserModel.updateOne({ _id: currentUser_id }, {
-            $push: {
-                appointments_groups: appointment_group,
-            }
-        })
+        const update = {
+            $set: { isAvailable: true },
+            $push: { appointments_groups: appointment_group }
+        };
+
+        await UserModel.updateOne({ _id: currentUser_id }, update)
             .then((data: any) => res.status(200).send({ ...appointment_group }))
             .catch((err: any) => res.status(400).send({ message: err.message }));
     }
     else res.status(400).send({ errorMessage: 'Something went wrong' })
+};
+
+//when expert chooses to go Offline before all his meetings timestamp ends
+const goOffline = async (req: Request, res: Response) => {
+    const { currentUser_id } = req;
+
+    const user = await UserModel.findByIdAndUpdate(currentUser_id, { isAvailable: false }).populate('appointments_groups.appointments');
+    const active_appointment = user.appointments_groups.find((app: any) => app.isActive);
+    const now = new Date();
+    for (let app of active_appointment.appointments) {
+        if (app.start_timestamp < now) continue;
+        if (app.isReserved) {
+            //remove from Appointment Model
+            console.log("🚀 ~ file: index.ts ~ line 67 ~ goOffline ~ Appointment Model", 'const app = await AppointmentModel.findByIdAndDelete()')
+            //use app.novice_id to notify novice //maybe i would add each id to array and notify by once
+            console.log("🚀 ~ file: index.ts ~ line 65 ~ goOffline ~ app.novice_id", app.novice_id)
+        }
+    }
+
+    if (!user) res.status(400).send({ message: 'User Not Found' })
+    else res.status(200).send(active_appointment);
 };
 
 const addScore = async (req: Request<{}, {}, AddScoreBodyInterface>, res: Response) => {
@@ -66,5 +88,6 @@ const addScore = async (req: Request<{}, {}, AddScoreBodyInterface>, res: Respon
 
 module.exports = {
     goOnline,
+    goOffline,
     addScore,
 };
--- backend/src/routes/expert/index.ts
+++ backend/src/routes/expert/index.ts
@@ -3,9 +3,10 @@ const router = Router();
 //internal imports
 const validateRequestMiddleware = require("../../middlewares/validateRequest");
 const { goOnlineValidationRules, addScoreValidationRules } = require('./validator');
-const { goOnline, addScore } = require("../../controllers/expert");
+const { goOnline, addScore, goOffline } = require("../../controllers/expert");
 
 router.post("/go_online", goOnlineValidationRules(), validateRequestMiddleware, goOnline);
+router.post("/go_offline", goOffline);
 router.post("/add_score", addScoreValidationRules(), validateRequestMiddleware, addScore);
 
 module.exports = router;
