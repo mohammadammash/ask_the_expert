f496e118a7d9763f84dce02abe2f988bd71eabc7
mohammadammash <mohammadammash31@gmail.com>
2022-11-06 11:59:53 +0200

    backend/src/controllers/novice/index.ts: Adding populates, conditions and achoose data returned

--- backend/src/controllers/novice/index.ts
+++ backend/src/controllers/novice/index.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+const mongoose = require('mongoose');
 //internal imports
 const UserModel = require("../../database/models/User");
 const AppointmentModel = require("../../database/models/Appointment");
@@ -7,30 +8,44 @@ import { getCloseExpertsBodyInterface, bookAppointmentBodyInterface, addReviewBo
 const getCloseExperts = async (req: Request<{}, {}, getCloseExpertsBodyInterface>, res: Response) => {
     const { latitude, longitude } = req.body;
 
-    await UserModel.aggregate([
-        {
+    try {
+        const closeExperts = await UserModel.aggregate([{
             $geoNear: {
                 near: { type: "Point", coordinates: [Number(longitude), Number(latitude)] },
                 distanceField: "distance.calculated",
                 maxDistance: 30000, //30km
-                spherical: true
-            }
+                spherical: true,
+            },
+        }]);
+
+        if (!closeExperts) res.status(200).send([]);
+        else {
+            //remove unactive appointment_groups and populate only active experts with at least one unreserved appointment
+            let experts = closeExperts.filter((expert: any) => {
+                expert.appointments_groups = expert.appointments_groups.find((app:any) => app.isActive)
+                return expert;
+            })
+
+            experts = await UserModel.populate(experts, { path: "appointments_groups.appointments"})
+            res.status(200).send(experts);
         }
-    ]).then((data: any) => res.status(200).send({ users: data }))
-        .catch((err: any) => res.status(400).send({ error: err.message }));
+    }
+    catch (err: any) {
+        res.status(400).send({ message: err.message })
+    }
 };
 
 const bookAppointment = async (req: Request<{}, {}, bookAppointmentBodyInterface>, res: Response) => {
     const { expert_id, appointment_id } = req.body;
     const { currentUser_id } = req;
 
-    const appointment = await AppointmentModel.findOne({ _id: appointment_id });
+    const appointment = await AppointmentModel.findOne({ _id: appointment_id }).lean();
     if (!appointment) res.status(400).send('Appointment Not Found');
 
     //make sure app exists and it is not reserved
     const now = new Date();
     if (appointment.start_timestamp > now && !appointment.isReserved) {
-        await AppointmentModel.findByIdAndUpdate(appointment_id, { $set: { novice_id: currentUser_id, isReserved: true } })
+        await AppointmentModel.findByIdAndUpdate(appointment_id, { $set: { novice_id: currentUser_id, isReserved: true } }, { new: true })
             .then((data: any) => res.status(200).send(data))
             .catch((err: any) => res.status(400).send(err.message))
         //!!!!if appointment is reserved send to expert_id notification!!!!!
@@ -45,9 +60,10 @@ const addReview = async (req: Request<{}, {}, addReviewBodyInterface>, res: Resp
     const exists = await UserModel.findOne({ "reviews.novice_id": currentUser_id });
     if (exists) return res.status(400).send({ message: 'User Cannot have more than one review for one expert' });
 
-    const new_review = { novice_id: currentUser_id, rating: rating, content: content }
+    const _id = new mongoose.Types.ObjectId();
+    const new_review = { _id, novice_id: currentUser_id, rating: rating, content: content }
     await UserModel.updateOne({ _id: expert_id }, { $push: { reviews: new_review } })
-        .then((data: any) => res.status(200).send(data))
+        .then((data: any) => res.status(200).send(new_review))
         .catch((err: any) => res.status(400).send(err.message));
 };
 
@@ -55,9 +71,7 @@ const deleteReview = async (req: Request<{}, {}, deleteReviewBodyInterface>, res
     const { expert_id } = req.body;
     const { currentUser_id } = req;
 
-    await UserModel.updateOne({ _id: expert_id }, {
-        $pull: { reviews: { novice_id: currentUser_id } }
-    })
+    await UserModel.updateOne({ _id: expert_id }, { $pull: { reviews: { novice_id: currentUser_id } } })
         .then((data: any) => res.status(200).send({ message: 'Review Removed' }))
         .catch((err: any) => res.status(400).send(err.message));
 };
