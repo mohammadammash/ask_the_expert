65cc02e98ba4ec7b02c5ec9db95a353d36c5b66e
mohammadammash <mohammadammash31@gmail.com>
2022-11-06 12:00:47 +0200

    backend/src/controllers/auth/helpers/loginUser.ts: Adding needed data populate

--- backend/src/controllers/auth/helpers/loginUser.ts
+++ backend/src/controllers/auth/helpers/loginUser.ts
@@ -3,7 +3,7 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
 const loginUser = async (email: String, password: String, _id: String) => {
-    const user = await UserModel.findOne({ _id, email }).select("+password").lean();
+    const user = await UserModel.findOne({ _id, email }).select("+password").populate('reviews.novice_id appointments appointments_groups.appointments blocked_users').lean();
     if (!user) return false;
 
     const matchingPassword = await bcrypt.compareSync(password, user.password);
