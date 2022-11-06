37781a51e85482e58f4c5584649047745e84d4c1
mohammadammash <mohammadammash31@gmail.com>
2022-11-06 21:40:41 +0200

    backend/src/routes/user/index.ts: Adding getCurrentUser API to update current user on refresh

--- backend/src/routes/user/index.ts
+++ backend/src/routes/user/index.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 const router = Router();
 //internal imports
-const { getRankedExperts, updateProfile, removeAppointment, blockOrUnblockUser, getUsersData } = require("../../controllers/user");
+const { getCurrentUser, getRankedExperts, updateProfile, removeAppointment, blockOrUnblockUser, getUsersData } = require("../../controllers/user");
 const { getUsersDataValidationRules, updateProfileValidationRules, blockOrUnblockUserValidationRules, removeAppointmentValidationRules } = require('./validator');
 const validateRequestMiddleware = require('../../middlewares/validateRequest')
 
+router.get('/', getCurrentUser);
 router.get("/leaderboard", getRankedExperts);
 router.get('/users', getUsersDataValidationRules(), validateRequestMiddleware, getUsersData); //for chats firebase table users
 router.put("/", updateProfileValidationRules(), validateRequestMiddleware, updateProfile);
