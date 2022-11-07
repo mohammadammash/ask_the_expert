27991118cb2dc81d52324e01b7dd70e3baacd83b
mohammadammash <mohammadammash31@gmail.com>
2022-11-07 16:32:06 +0200

    frontend/src/components/Common/helpers/editProfileFormHelper.ts: Adding admin types to use with intial vlaues within editprofileformcomponent

--- frontend/src/components/Admin/types.ts
+++ frontend/src/components/Admin/types.ts
@@ -1,27 +1,3 @@
-export interface EditProfileFormValuesTypes {
-  firstName: string;
-  lastName: string;
-  profile_url: string;
-  language: string;
-  theme: string;
-}
-
-export interface AdminEditProfileFormProps {
-  image: string;
-  showImage: () => void;
-  isAppLanguageFocus: boolean;
-  appLanguage: any;
-  handleAppLanguage: (language: any) => void;
-  focusAppLanguage: (isFocused: any) => void;
-  appTheme: any;
-  isAppThemeFocus: boolean;
-  handleAppTheme: (theme: any) => void;
-  focusAppTheme: (isFocused: any) => void;
-  handleSubmitForm: (values: EditProfileFormValuesTypes) => void;
-}
-
-
-
 export interface FilterPageTitleProps {
   userType: string;
   handleShownUserType: (value: string) => void;
--- frontend/src/components/Common/helpers/editProfileFormHelper.ts
+++ frontend/src/components/Common/helpers/editProfileFormHelper.ts
@@ -1,7 +1,8 @@
 import * as Yup from "yup";
 import { EditProfileFormValuesTypes } from "../types";
 
-export const editProfileInitialValues: EditProfileFormValuesTypes = {
+//EXPERT OR NOVICE
+export const userEditProfileInitialValues: EditProfileFormValuesTypes = {
     firstName: "",
     lastName: "",
     about: "",
@@ -12,9 +13,23 @@ export const editProfileInitialValues: EditProfileFormValuesTypes = {
     theme: '',
   };
 
-export const validateEditProfileFormSchema = Yup.object().shape({
+export const validateUserEditProfileFormSchema = Yup.object().shape({
     firstName: Yup.string().min(2, "Too Short!").max(50, "Too Long!").required("Required"),
     lastName: Yup.string().min(2, "Too Short!").max(50, "Too Long!").required("Required"),
     about: Yup.string().min(20, "Min 20 Characters").max(250, "Too Long").required('Required'),
     languages: Yup.array().min(1, 'At least one language').required('Required')
   });
+
+//ADMIN
+export const adminEditProfileInitialValues: EditProfileFormValuesTypes = {
+  firstName: "",
+  lastName: "",
+  profile_url: "",
+  language: "",
+  theme: "",
+};
+
+export const validateAdminEditProfileFormSchema = Yup.object().shape({
+  firstName: Yup.string().min(2, "Too Short!").max(50, "Too Long!").required("Required"),
+  lastName: Yup.string().min(2, "Too Short!").max(50, "Too Long!").required("Required"),
+});
