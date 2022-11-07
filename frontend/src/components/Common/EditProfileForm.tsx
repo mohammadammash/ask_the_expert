3109e97dafad13175417fdba5d1a1937c2c2fe24
mohammadammash <mohammadammash31@gmail.com>
2022-11-07 16:34:22 +0200

    frontend/src/components/Common/EditProfileForm.tsx: Adding conditional rendering for form initial values and yup validation

--- frontend/src/components/Common/EditProfileForm.tsx
+++ frontend/src/components/Common/EditProfileForm.tsx
@@ -6,15 +6,20 @@ import { Picker } from "@react-native-picker/picker";
 //internal imports:
 import styles from "../../../styles";
 import authStyles from "../Auth/auth.styles";
-import { IMAGES } from "../../constants";
+import { IMAGES, USERTYPES } from "../../constants";
 import { ALLJOBSSPECIALTIES, APP_LANGUAGES_OPTIONS, APP_THEME_OPTIONS } from "../../constants";
-import { validateEditProfileFormSchema, editProfileInitialValues } from "./helpers/editProfileFormHelper";
+import {
+  userEditProfileInitialValues,
+  adminEditProfileInitialValues,
+  validateAdminEditProfileFormSchema,
+  validateUserEditProfileFormSchema,
+} from "./helpers/editProfileFormHelper";
 import { ALLANGUAGES } from "../../constants";
 import commonStyles from "./common.styles";
-import { UserEditProfileFormProps } from "./types";
+import { EditProfileFormProps } from "./types";
 
-// const FilterPageTitle;
-const EditProfileForm: React.FC<UserEditProfileFormProps> = ({
+const EditProfileForm: React.FC<EditProfileFormProps> = ({
+  user_type,
   image,
   showImage,
   isAppLanguageFocus,
@@ -31,11 +36,11 @@ const EditProfileForm: React.FC<UserEditProfileFormProps> = ({
 }) => {
   return (
     <Formik
-      initialValues={editProfileInitialValues}
-      onSubmit={async (values, actions) => {
+      initialValues={user_type === USERTYPES.ADMIN ? adminEditProfileInitialValues : userEditProfileInitialValues}
+      onSubmit={async (values) => {
         handleSubmitForm(values);
       }}
-      validationSchema={validateEditProfileFormSchema}
+      validationSchema={user_type === USERTYPES.ADMIN ? validateAdminEditProfileFormSchema : validateUserEditProfileFormSchema}
     >
       {({ handleChange, handleBlur, handleSubmit, errors, touched, values }) => (
         <View className="w-4/5 justify-center">
@@ -72,60 +77,66 @@ const EditProfileForm: React.FC<UserEditProfileFormProps> = ({
             {errors.lastName && touched.lastName && <Text className="text-red-600">{errors.lastName}</Text>}
           </View>
 
-          <View className="mt-2">
-            <Text className="font-bold">About</Text>
-            <TextInput
-              onChangeText={handleChange("about")}
-              onBlur={handleBlur("about")}
-              value={values.about}
-              className=" placeholder:pl-3 h-20 border-2 rounded-lg"
-              placeholder="About"
-              multiline={true}
-              numberOfLines={10}
-            />
-            {errors.about && touched.about && <Text className="text-red-600">{errors.about}</Text>}
-          </View>
+          {/* FOR EXPERTS AND NOVICES ONLY */}
+          {user_type !== USERTYPES.ADMIN ? (
+            <>
+              <View className="mt-2">
+                <Text className="font-bold">About</Text>
+                <TextInput
+                  onChangeText={handleChange("about")}
+                  onBlur={handleBlur("about")}
+                  value={values.about}
+                  className=" placeholder:pl-3 h-20 border-2 rounded-lg"
+                  placeholder="About"
+                  multiline={true}
+                  numberOfLines={10}
+                />
+                {errors.about && touched.about && <Text className="text-red-600">{errors.about}</Text>}
+              </View>
 
-          <View>
-            <Text className="mt-2 font-bold border-b-2 bold">Speciality</Text>
-            <Picker
-              style={styles.select_input}
-              enabled={true}
-              mode="dropdown"
-              placeholder="Select Field"
-              selectedValue={values.speciality}
-              onValueChange={handleChange("speciality")}
-            >
-              {ALLJOBSSPECIALTIES.map((job, index) => (
-                <Picker.Item label={job} value={job} key={index} />
-              ))}
-            </Picker>
-          </View>
+              <View>
+                <Text className="mt-2 font-bold border-b-2 bold">Speciality</Text>
+                <Picker
+                  style={styles.select_input}
+                  enabled={true}
+                  mode="dropdown"
+                  placeholder="Select Field"
+                  selectedValue={values.speciality}
+                  onValueChange={handleChange("speciality")}
+                >
+                  {ALLJOBSSPECIALTIES.map((job, index) => (
+                    <Picker.Item label={job} value={job} key={index} />
+                  ))}
+                </Picker>
+              </View>
 
-          <Text className="font-bold">Spoken Languages</Text>
-          <View className="border-2 rounded-lg pl-1">
-            <MultiSelect
-              style={authStyles.dropdown}
-              placeholderStyle={authStyles.placeholderStyle}
-              selectedTextStyle={authStyles.selectedTextStyle}
-              inputSearchStyle={authStyles.inputSearchStyle}
-              iconStyle={authStyles.iconStyle}
-              search
-              data={ALLANGUAGES}
-              labelField="label"
-              valueField="value"
-              placeholder="Select Spoken Languages"
-              searchPlaceholder="Search..."
-              value={selectedLanguages}
-              onChange={(item) => {
-                handleSelectedLanguages(item);
-                values.languages = item;
-              }}
-              renderLeftIcon={() => <AntDesign color="black" name="Safety" size={20} />}
-              selectedStyle={authStyles.selectedStyle}
-            />
-            {errors.languages && touched.languages && <Text className="text-red-600  ">{errors.languages}</Text>}
-          </View>
+              <Text className="font-bold">Spoken Languages</Text>
+              <View className="border-2 rounded-lg pl-1">
+                <MultiSelect
+                  style={authStyles.dropdown}
+                  placeholderStyle={authStyles.placeholderStyle}
+                  selectedTextStyle={authStyles.selectedTextStyle}
+                  inputSearchStyle={authStyles.inputSearchStyle}
+                  iconStyle={authStyles.iconStyle}
+                  search
+                  data={ALLANGUAGES}
+                  labelField="label"
+                  valueField="value"
+                  placeholder="Select Spoken Languages"
+                  searchPlaceholder="Search..."
+                  value={selectedLanguages}
+                  onChange={(item) => {
+                    if (handleSelectedLanguages) handleSelectedLanguages(item);
+                    values.languages = item;
+                  }}
+                  renderLeftIcon={() => <AntDesign color="black" name="Safety" size={20} />}
+                  selectedStyle={authStyles.selectedStyle}
+                />
+                {errors.languages && touched.languages && <Text className="text-red-600  ">{errors.languages}</Text>}
+              </View>
+            </>
+          ) : null}
+          {/* END OF NOVICES && EXPERTS ONLY */}
 
           <View className="mt-2">
             <Text className="font-bold">App Language</Text>
@@ -185,7 +196,7 @@ const EditProfileForm: React.FC<UserEditProfileFormProps> = ({
             />
           </View>
 
-          <Pressable className="my-7" style={styles.blue_button_xl} onPress={handleSubmitForm}>
+          <Pressable className="my-7" style={styles.blue_button_xl} onPress={handleSubmit}>
             <Text className="text-xl text-white font-bold">SUBMIT</Text>
           </Pressable>
         </View>
