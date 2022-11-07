50bab98de6b352c42cf08f8f9df648a624914103
mohammadammash <mohammadammash31@gmail.com>
2022-11-07 19:06:44 +0200

    frontend/styles.ts: Adding text colors, backgrounds, and borderColor to use in all files

--- frontend/styles.ts
+++ frontend/styles.ts
@@ -1,26 +1,53 @@
 import { StyleSheet } from "react-native";
-import {COLORS} from "./src/constants/index";
+import { COLORS } from "./src/constants/index";
 
 export default StyleSheet.create({
-  //COLORS
-  orange_text : {
+  //TEXT COLORS
+  orange_text: {
     color: COLORS.orange,
   },
+  blue_text: {
+    color: COLORS.blue
+  },
+  grey_text: {
+    color: COLORS.grey
+  },
+  dark_text: {
+    color: COLORS.dark
+  },
+  white_text: {
+    color: COLORS.white,
+  },
+  bg_blue: {
+    backgroundColor: COLORS.blue
+  },
+  bg_grey: {
+    backgroundColor: COLORS.grey
+  },
+  bg_dark: {
+    backgroundColor: COLORS.dark
+  },
+
+  //BORDER
+  border_blue: {
+    borderColor: COLORS.blue
+  },
+
   //INPUTS
   text_input: {
     borderWidth: 2,
     height: 50,
     borderRadius: 10,
   },
-  search_input:{
-    width:'80%',
+  search_input: {
+    width: '80%',
   },
   select_input: {
     borderWidth: 2,
     borderRadius: 10,
     marginBottom: 15,
   },
-  send_message_input:{
+  send_message_input: {
     backgroundColor: COLORS.white,
     borderWidth: 0,
   },
@@ -29,46 +56,46 @@ export default StyleSheet.create({
   },
 
   //BUTTONS
-    blue_button_xl: {
-      borderWidth: 2,
-      height: 60,
-      borderRadius: 10,
-      alignItems: "center",
-      justifyContent: "center",
-      backgroundColor: COLORS.blue,
-      color: COLORS.white,
-      width: 300,
-    },
-    blue_button_lg: {
-      borderWidth: 2,
-      height: 60,
-      width:200,
-      borderRadius: 10,
-      alignItems: "center",
-      justifyContent: "center",
-      backgroundColor: COLORS.blue,
-    },
-    blue_button_md: {
-      borderWidth: 2,
-      height: 60,
-      width:140,
-      textTransform: "uppercase",
-      borderRadius: 10,
-      alignItems: "center",
-      justifyContent: "center",
-      backgroundColor: COLORS.blue,
-    },
-    blue_button_sm: {
-      borderWidth: 2,
-      height: 40,
-      width:104,
-      textTransform: "uppercase",
-      borderRadius: 10,
-      alignItems: "center",
-      justifyContent: "center",
-      backgroundColor: COLORS.blue,
-    },
-  
+  blue_button_xl: {
+    borderWidth: 2,
+    height: 60,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: COLORS.blue,
+    color: COLORS.white,
+    width: 300,
+  },
+  blue_button_lg: {
+    borderWidth: 2,
+    height: 60,
+    width: 200,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: COLORS.blue,
+  },
+  blue_button_md: {
+    borderWidth: 2,
+    height: 60,
+    width: 140,
+    textTransform: "uppercase",
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: COLORS.blue,
+  },
+  blue_button_sm: {
+    borderWidth: 2,
+    height: 40,
+    width: 104,
+    textTransform: "uppercase",
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: COLORS.blue,
+  },
+
   //CARDS:
   bg_grey_opacity30: {
     backgroundColor: 'rgba(211, 214, 219, 0.20)',
@@ -77,8 +104,13 @@ export default StyleSheet.create({
     backgroundColor: COLORS.white,
     borderWidth: 0,
     shadowColor: '#171717',
-    shadowOffset: {width: -2, height: 4},
+    shadowOffset: { width: -2, height: 4 },
     shadowOpacity: 0.5,
     shadowRadius: 3,
+  },
+
+  //UTILS
+  alignCenter: {
+    alignItems: "center" 
   }
 });
