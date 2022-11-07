6aaf5fd18cb63e49b0e66ee327d5bef3f592d97e
mohammadammash <mohammadammash31@gmail.com>
2022-11-07 14:42:32 +0200

    backend/src/routes/user/index.ts: Altering getCurrent user route and changing getUsers from get to post

--- backend/src/routes/user/index.ts
+++ backend/src/routes/user/index.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
 const router = Router();
 //internal imports
-const { getCurrentUser, getRankedExperts, updateProfile, removeAppointment, blockOrUnblockUser, getUsersData } = require("../../controllers/user");
+const { getSingleUserData, getRankedExperts, updateProfile, removeAppointment, blockOrUnblockUser, getUsersData } = require("../../controllers/user");
 const { getUsersDataValidationRules, updateProfileValidationRules, blockOrUnblockUserValidationRules, removeAppointmentValidationRules } = require('./validator');
 const validateRequestMiddleware = require('../../middlewares/validateRequest')
 
-router.get('/', getCurrentUser);
+router.get('/:user_id?', getSingleUserData);
 router.get("/leaderboard", getRankedExperts);
-router.get('/users', getUsersDataValidationRules(), validateRequestMiddleware, getUsersData); //for chats firebase table users
+router.post('/users', getUsersDataValidationRules(), validateRequestMiddleware, getUsersData); //firebase chats users //used post to send array of ids
 router.put("/", updateProfileValidationRules(), validateRequestMiddleware, updateProfile);
 router.put('/block', blockOrUnblockUserValidationRules(), validateRequestMiddleware, blockOrUnblockUser);
 router.delete('/appointment', removeAppointmentValidationRules(), validateRequestMiddleware, removeAppointment)
