f89eee99c33fefd84534e231e59f0a7ecf0e1464
mohammadammash <mohammadammash31@gmail.com>
2022-11-09 23:17:30 +0200

    frontend/src/screens/auth/LoginScreen.tsx: Getting user from server and updating userContext

--- frontend/src/screens/auth/LoginScreen.tsx
+++ frontend/src/screens/auth/LoginScreen.tsx
@@ -1,15 +1,25 @@
-import { View, Text, Pressable, Image } from "react-native";
+import { View, Text, Pressable, Image, ActivityIndicator } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 //internal imports
-import { ROUTES, IMAGES } from "../../constants";
+import { ROUTES, IMAGES, COLORS } from "../../constants";
 import { LoginFormComponent } from "../../components";
 import styles from "../../../styles";
 import { auth } from "../../../firebaseConfig";
+import { useUserContext } from "../../hooks/UserContext";
+import { useLoginUser } from "../../hooks/useAuth";
 
 const LoginScreen = () => {
   const navigation = useNavigation<any>();
+  const { setUser } = useUserContext();
+
+  //React Query Post:
+  const { mutate: mutateLoginUser, isLoading: mutateLoginUserIsLoading, data: mutateLoginUserData } = useLoginUser();
+  //when user data is found update UserContext
+  useEffect(() => {
+    if (mutateLoginUserData) setUser(mutateLoginUserData.data);
+  }, [mutateLoginUserData]);
 
   //FORM DATA
   const [inValidCredentials, setInValidCredentials] = useState(false);
@@ -18,9 +28,10 @@ const LoginScreen = () => {
   //FIREBASE AUTH
   const firbaseConfirmLogin = (email: string, password: string) => {
     signInWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
+      .then(async (userCredential) => {
         const user = userCredential.user;
-        alert(user);
+        let data = { _id: "dsfs3dfddsdsd1122dsd3322s3", email: "mhmd333132abx@gmail.com", password: "1d@aAA" };
+        mutateLoginUser(data);
       })
       .catch((error) => {
         alert(error.code);
@@ -37,13 +48,22 @@ const LoginScreen = () => {
   };
 
   //MAIN COMPONENT
+  //If Login isLoading //show loading indicator in middle of screen until user is redirected
+  if (mutateLoginUserIsLoading) {
+    return (
+      <View className="flex-1 items-center justify-center">
+        <ActivityIndicator size="large" color={COLORS.dark} />
+      </View>
+    );
+  }
+
   return (
     <View className="flex-1 items-center bg-white">
       <View className="w-full mb-2 h-1/2">
         <Image className="max-w-full max-h-full" source={IMAGES.logo_500px} />
       </View>
 
-      <LoginFormComponent {...data}/>
+      <LoginFormComponent {...data} />
 
       <View className="flex-row mt-10">
         <Text className="inline mr-1 font-bold">Don't have an account?</Text>
