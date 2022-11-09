35ec939945acd1e819bd68ca34adb64037f80da3
mohammadammash <mohammadammash31@gmail.com>
2022-11-09 23:19:00 +0200

    src/hooks/useAuth.ts: Creating react query hooks for login and register

---
+++ frontend/src/hooks/useAuth.ts
@@ -0,0 +1,17 @@
+import { useMutation } from "@tanstack/react-query";
+import { queryClient } from "../../App";
+//internal imports
+import Auth_Apis from "../networks/auth";
+
+export const CURRENT_USER_KEY = ["CURRENT_USER_KEY"];
+
+export const useLoginUser = () => useMutation({
+    mutationFn: (data: any) => Auth_Apis.login_post(data),
+    onSuccess: (data: any) => queryClient.setQueryData(["CURRENT_USER_KEY"], { ...data }),
+});
+
+export const useRegisterUser = () => useMutation({
+    mutationFn: (data: any) => Auth_Apis.register_post(data),
+    onSuccess: (data: any) => queryClient.setQueryData(["CURRENT_USER_KEY"], { ...data.data })
+});
+
