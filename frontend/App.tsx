542a1538e5115cf12f6b711425bea341ec7653e7
mohammadammash <mohammadammash31@gmail.com>
2022-11-10 13:22:55 +0200

    frontend/src/hooks/UserContext.ts: Extracting user initial data from App.tsx to this file

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,38 +1,30 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { AuthStackNavigator, DrawerNavigator, AdminTabNavigator } from "./src/navigations";
 import { useState } from "react";
-import { UserContext } from "./src/hooks/UserContext";
-import { IMAGES, USERTYPES } from "./src/constants";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+//internal imports
+import { userInitialData, UserContext } from "./src/hooks/UserContext";
+import { AuthStackNavigator, DrawerNavigator, AdminTabNavigator } from "./src/navigations";
+import { USERTYPES } from "./src/constants";
+
+export const queryClient = new QueryClient();
 
 export default function App() {
-  const [user, setUser] = useState({
-    id: 22,
-    firstName: "Mohammad",
-    lastName: "Ammash",
-    field: "Technology",
-    spoken_languages: "Arabic, English",
-    speciality: "Software Engineer",
-    email: "mohammad31@gmail.com",
-    user_type: "expert",
-    profile_url: IMAGES.dummyProfile,
-    online: true,
-    start_date: new Date(1990, 7, 17),
-    about:
-      "This is an obvious starting point, but it's a chance to help the interviewer understand more about you and why you love being a web designer. After introducing yourself, talk about your career path, education, previous work experience if you have it, and end with your interest in the job in question.",
-  });
+  const [user, setUser] = useState(userInitialData);
 
   return (
-    <UserContext.Provider value={{ user, setUser }}>
-      <NavigationContainer>
-        {user.user_type === USERTYPES.NOVICE || user.user_type === USERTYPES.EXPERT ? (
-          <DrawerNavigator />
-        ) : user.user_type === USERTYPES.ADMIN ? (
-          <AdminTabNavigator />
-        ) : (
-          <AuthStackNavigator />
-        )}
-      </NavigationContainer>
-    </UserContext.Provider>
+    <QueryClientProvider client={queryClient}>
+      <UserContext.Provider value={{ user, setUser }}>
+        <NavigationContainer>
+          {user.user_type === USERTYPES.NOVICE || user.user_type === USERTYPES.EXPERT ? (
+            <DrawerNavigator />
+          ) : user.user_type === USERTYPES.ADMIN ? (
+            <AdminTabNavigator />
+          ) : (
+            <AuthStackNavigator />
+          )}
+        </NavigationContainer>
+      </UserContext.Provider>
+    </QueryClientProvider>
   );
 }
 {
--- frontend/src/hooks/UserContext.ts
+++ frontend/src/hooks/UserContext.ts
@@ -1,20 +1,41 @@
 import { createContext, useContext } from "react";
+//internal imports:
+import { IMAGES } from "../constants/index";
 
-type userType = {
-id:number, firstName: string, lastName:string, spoken_languages:string, speciality: string, field: string,
-     start_date: Date, user_type: string, profile_url: string, online: boolean, about:string, email:string 
+type reviewsType = {
+  novice_id: string,
+  rating: Number,
+  content: string,
+}
+
+type appointmentsGroupsTypes = {
+  start_timestamp: Date,
+  end_timestamp: Date,
+  isActive: boolean,
+  appointments: string[],
+}
+
+type locationType = {
+  type: string,
+  coordinates: Number[],
+}
+
+export type userType = {
+  _id: string, firstName: string, lastName: string, email: string, spoken_languages: string, start_date: Date, about: string, profile_url: string, field: string, speciality: string, app_language: string, theme: string, user_type: string, isBanned: boolean, blocked_users: string[], device_token: string, reviews?: reviewsType[], appointments?: string[], appointments_groups?: appointmentsGroupsTypes[], score?: Number, isAvailable: boolean, location?: locationType, createdAt?: Date, updatedAt?: Date,
 }
 
 type UserContent = {
   user: userType,
-  setUser:(user: userType) => void
+  setUser: (user: userType) => void
 };
 
-export const UserContext = createContext<UserContent>({
-user: {id:1, firstName: '', lastName:'', spoken_languages:'', speciality: '', field: '', email:'',
- start_date: new Date(), user_type: "", profile_url: "", online:false, about: "" }, // set a default value
+export const userInitialData: userType = {
+  _id: '', firstName: '', lastName: '', email: '', spoken_languages: '', start_date: new Date(), about: '', profile_url: IMAGES.dummyProfile, field: '', speciality: '', app_language: '', theme: '', user_type: '', isBanned: false, blocked_users: [], device_token: '', isAvailable: false,
+}
 
-setUser: () => {},
+export const UserContext = createContext<UserContent>({
+  user: userInitialData, // set a default value
+  setUser: () => { },
 });
 
 export const useUserContext = () => useContext(UserContext);
