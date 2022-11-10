559e139670a4eb5aef5d67fa7392a2ef0ee97e30
mohammadammash <mohammadammash31@gmail.com>
2022-11-10 14:00:01 +0200

    frontend/src/components/Common/ProfileScreen/ProfileImageCard.tsx:: Conditional render image or dummy profile

--- frontend/src/components/Common/ProfileScreen/ProfileImageCard.tsx
+++ frontend/src/components/Common/ProfileScreen/ProfileImageCard.tsx
@@ -1,14 +1,14 @@
 import { View, Image } from "react-native";
 //internal imports
 import styles from "../../../../styles";
+import { IMAGES } from "../../../constants";
 import { ProfileImageCardProps } from "../types";
-
 
 const ProfileImageCard: React.FC<ProfileImageCardProps> = ({ profile_url }) => {
   return (
     <View className="h-52 w-full items-center justify-center">
       <View style={styles.border_blue} className="avatar aspect-square rounded-full items-center w-2/3 my-2 h-3/4 border-8">
-        <Image className="max-w-full max-h-full h-full w-full rounded-full" source={profile_url} />
+        <Image className="max-w-full max-h-full h-full w-full rounded-full" source={profile_url.length >1 ? { uri: profile_url } : IMAGES.dummyProfile} />
       </View>
     </View>
   );
