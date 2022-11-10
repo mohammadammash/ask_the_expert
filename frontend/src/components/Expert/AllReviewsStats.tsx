4683a9a7411092b621fe8d92ce3b99d0c3ea690d
mohammadammash <mohammadammash31@gmail.com>
2022-11-10 17:32:30 +0200

    frontend/src/components/Expert/AllReviewsStats.tsx: Altering conditions and shown data

--- frontend/src/components/Expert/AllReviewsStats.tsx
+++ frontend/src/components/Expert/AllReviewsStats.tsx
@@ -6,28 +6,37 @@ import styles from "../../../styles";
 import { AllReviewsStatsProps } from "./types";
 import expertStyles from "./expert.styles";
 
-const AllReviewsStats: React.FC<AllReviewsStatsProps> = ({ reviews, rating }) => {
+const AllReviewsStats: React.FC<AllReviewsStatsProps> = ({ reviews_length, rating }) => {
+  const five_avg = rating.totalOf5 > 0 ? rating.totalOf5 / reviews_length : 0;
+  const four_avg = rating.totalOf4 > 0 ? rating.totalOf4 / reviews_length : 0;
+  const three_avg = rating.totalOf3 > 0 ? rating.totalOf3 / reviews_length : 0;
+  const two_avg = rating.totalOf2 > 0 ? rating.totalOf2 / reviews_length : 0;
+  const one_avg = rating.totalOf1 > 0 ? rating.totalOf1 / reviews_length : 0;
+  const rating_avg = rating.average ? rating.average : 0;
+
   return (
     // REVIEWS STATS
     <View className="bg-white w-5/6">
       <View className="h-40 justify-start">
         <View>
-          <Text className="text-center text-3xl font-bold">{rating.average}.0</Text>
+          <Text className="text-center text-3xl font-bold">{rating_avg}.0</Text>
         </View>
         <AirbnbRating defaultRating={rating.average} isDisabled reviews={[""]} starContainerStyle={expertStyles.height_10px} />
-        <Text className="text-center mt-6">Based on {reviews.length} Reviews</Text>
+        <Text className="text-center mt-6">Based on {reviews_length} Reviews</Text>
       </View>
 
-      <SingleReviewStat key={1} progress={rating.totalOf5 / reviews.length} rating={5} />
-      <SingleReviewStat key={2} progress={rating.totalOf4 / reviews.length} rating={4} />
-      <SingleReviewStat key={3} progress={rating.totalOf3 / reviews.length} rating={3} />
-      <SingleReviewStat key={4} progress={rating.totalOf2 / reviews.length} rating={2} />
-      <SingleReviewStat key={5} progress={rating.totalOf1 / reviews.length} rating={1} />
+      <SingleReviewStat key={1} progress={five_avg} rating={5} />
+      <SingleReviewStat key={2} progress={four_avg} rating={4} />
+      <SingleReviewStat key={3} progress={three_avg} rating={3} />
+      <SingleReviewStat key={4} progress={two_avg} rating={2} />
+      <SingleReviewStat key={5} progress={one_avg} rating={1} />
 
       {/* SEARCH BAR */}
-      <View className="items-center mt-10">
-        <TextInput style={[styles.text_input, styles.search_input]} className="placeholder:pl-3" placeholder="Search" />
-      </View>
+      {reviews_length ? (
+        <View className="items-center mt-10 mb-5">
+          <TextInput style={[styles.text_input, styles.search_input]} className="placeholder:pl-3" placeholder="Search" />
+        </View>
+      ) : null}
     </View>
   );
 };
