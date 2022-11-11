d665d21ee47c1a9cd1ec2ab747ea57680317ab5b
mohammadammash <mohammadammash31@gmail.com>
2022-11-11 14:49:42 +0200

    frontend/src/networks/expert.ts: Adding goOffline api axios post request

--- frontend/src/networks/expert.ts
+++ frontend/src/networks/expert.ts
@@ -2,6 +2,8 @@ import axios from './index';
 
 class Expert_Apis {
     goOnline_post(data: any) { return axios.post('/expert/go_online', data).then(data => data).catch(err => err) }
+
+    goOffline_post() { return axios.post('/expert/go_offline').then(data => data).catch(err => err) }
 }
 
 export default new Expert_Apis();
