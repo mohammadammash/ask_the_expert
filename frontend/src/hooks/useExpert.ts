ed6cf88017487510299d91aeced23b457524d632
mohammadammash <mohammadammash31@gmail.com>
2022-11-11 14:50:37 +0200

    frontend/src/hooks/useExpert.ts: Adding goOffline handler react query

--- frontend/src/hooks/useExpert.ts
+++ frontend/src/hooks/useExpert.ts
@@ -5,10 +5,18 @@ import Expert_Apis from "../networks/expert";
 
 export const CURRENT_USER_APPOINTMENT_GROUP_KEY = ["CURRENT_USER_APPOINTMENT_GROUP_KEY"];
 
+export const NOVICES_DEVICES_TOKENS_KEYS = ['NOVICES_DEVICES_TOKENS_KEYS'];
+
 export const useGoOnlineExpert = () => useMutation({
     mutationFn: (data: any) => Expert_Apis.goOnline_post(data),
     onSuccess: (data: any) => {
-        queryClient.setQueryData(CURRENT_USER_APPOINTMENT_GROUP_KEY, { ...data })
-        alert(JSON.stringify(data, null, 2));
+        queryClient.setQueryData(CURRENT_USER_APPOINTMENT_GROUP_KEY, { ...data.data })
+    }
+});
+
+export const useGoOfflineExpert = () => useMutation({
+    mutationFn: () => Expert_Apis.goOffline_post(),
+    onSuccess: (data: any) => {
+        queryClient.setQueryData(NOVICES_DEVICES_TOKENS_KEYS, { ...data.data })
     }
 });
