9b2f875aa1ed499560f53e5e26ad647bb237ee4e
mohammadammash <mohammadammash31@gmail.com>
2022-11-12 13:11:21 +0200

    frontend/src/networks/user.ts: Adding axios getRankedExperts api

--- frontend/src/networks/user.ts
+++ frontend/src/networks/user.ts
@@ -2,6 +2,8 @@ import axios from './index';
 
 class User_Apis {
     user_get() { return axios.get('/user').then(data => data).catch(err => err) }
+
+    leaderboard_get() { return axios.get('/user/leaderboard').then(data => data).catch(err => err) }
 }
 
 export default new User_Apis();
