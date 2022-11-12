f8e7981c7124470e90a0b84beee9bec546acd05a
mohammadammash <mohammadammash31@gmail.com>
2022-11-12 13:11:03 +0200

    frontend/src/hooks/useUser.ts: Adding useUser leaderboard query get

--- frontend/src/hooks/useUser.ts
+++ frontend/src/hooks/useUser.ts
@@ -4,6 +4,7 @@ import { queryClient } from "../../App";
 import User_Apis from "../networks/user";
 
 export const CURRENT_USER_KEY = ["CURRENT_USER_KEY"];
+export const LEADERBOARD_EXPERTS_KEY = ["RANKED_EXPERTS_KEY"];
 
 export const useCurrentUser = ({ enabled }) =>
     useQuery({
@@ -13,6 +14,17 @@ export const useCurrentUser = ({ enabled }) =>
         queryFn: () => User_Apis.user_get(),
         onSuccess: (data) => {
             queryClient.setQueryData(CURRENT_USER_KEY, { ...data.data })
-            alert(JSON.stringify(data));
+        }
+    });
+
+//leaderboard ranked experts
+export const useLeaderboardExperts = () =>
+    useQuery({
+        refetchOnWindowFocus: false,
+        queryKey: LEADERBOARD_EXPERTS_KEY,
+        queryFn: () => User_Apis.leaderboard_get(),
+        placeholderData: [],
+        onSuccess: (data) => {
+            queryClient.setQueryData(LEADERBOARD_EXPERTS_KEY, { ...data.data })
         }
     });
