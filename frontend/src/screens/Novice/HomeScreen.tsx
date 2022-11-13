01ef250c306ab87a1b78d7a7cda973c8f57e9125
mohammadammash <mohammadammash31@gmail.com>
2022-11-13 09:46:49 +0200

    frontend/src/screens/Novice/HomeScreen.tsx: Adding google map and place view card with intial region

--- frontend/src/screens/Novice/HomeScreen.tsx
+++ frontend/src/screens/Novice/HomeScreen.tsx
@@ -1,19 +1,22 @@
 import { useNavigation } from "@react-navigation/native";
-import { View, ImageBackground, Text, ActivityIndicator } from "react-native";
+import { View, ImageBackground, Text, ActivityIndicator, StyleSheet, Dimensions, TouchableOpacity, Image } from "react-native";
 import { useEffect, useState } from "react";
 import * as Location from "expo-location";
-import { useUserContext } from "../../hooks/UserContext";
+import MapView, { Marker } from "react-native-maps";
 //internal imports
+import { userType, useUserContext } from "../../hooks/UserContext";
 import { IMAGES, COLORS, ROUTES } from "../../constants";
-import { HomeUserCardComponent } from "../../components";
+import styles from "../../../styles";
 import { getAuthToken } from "../../networks";
 import { useCloseExperts } from "../../hooks/useNovice";
+import { SimpleLineIcons } from "@expo/vector-icons";
+//internal imports
 
 const ProfileScreen = () => {
   const navigation = useNavigation<any>();
   const navigateToPage = (routeName: string) => navigation.navigate(routeName);
   const { user } = useUserContext();
-  const { field } = user;
+  const { field, profile_url } = user;
 
   //-------------------------------------------------------------
   //START OF GET CURRENT NOVICE LOCATION (longitude and latitude)
@@ -22,13 +25,13 @@ const ProfileScreen = () => {
   useEffect(() => {
     console.log(getAuthToken);
     (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
+      const { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== "granted") {
         return;
       }
 
       setIsLoadingLocation(true);
-      let location = await Location.getCurrentPositionAsync({});
+      const location = await Location.getCurrentPositionAsync({});
       setIsLoadingLocation(false);
       setLocation({ longitude: location.coords.longitude, latitude: location.coords.latitude });
     })();
@@ -50,7 +53,7 @@ const ProfileScreen = () => {
   }, [location]);
 
   useEffect(() => {
-    if (closeExpertData) alert(JSON.stringify(closeExpertData, null, 2));
+    // if (closeExpertData) alert(JSON.stringify(closeExpertData, null, 2));
   }, [closeExpertData]);
   //END OF GETTING CLOSE EXPERTS
   //-------------------------------
@@ -69,26 +72,80 @@ const ProfileScreen = () => {
     );
   }
 
-  //Params
-  const data = {
-    shown_user: user,
-    handleShowExpert: () => {
-      navigateToPage(ROUTES.EXPERT_PROFILE);
-    },
-  };
-
-  return (
-    <View className="flex-1 bg-white">
+  //EMPTY STATE
+  if (!closeExpertData.data) {
+    return (
       <ImageBackground className="w-full h-full" source={IMAGES.fakeMapImage}>
         <View className="w-full items-center justify-center h-1/5">
-          <Text className="font-bold">{closeExpertData.data[0].firstName}</Text>
-        </View>
-        <View className="flex-1 items-center justify-end mb-7">
-          <HomeUserCardComponent {...data} />
+          <Text className="font-bold">
+            No Experts found right now! Refresh the page after few minutes, your answer maybe one minute away, Stay Patient!
+          </Text>
         </View>
       </ImageBackground>
+    );
+  }
+
+  //IF AT LEAST ONE EXPERT FOUND
+  // {
+  // closeExpertData.data?.map((expert: userType) => {
+  return (
+    <View style={mapStyles.container}>
+      <MapView
+        style={mapStyles.map}
+        initialRegion={{
+          latitude: location.latitude,
+          longitude: location.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        <Marker coordinate={location} title="My Place" description="Current location" image={{ uri: profile_url }}>
+          <Image source={profile_url.length > 1 ? { uri: profile_url } : IMAGES.dummyProfile} />
+        </Marker>
+      </MapView>
+      <View className="flex-1 w-4/5 items-center justify-end absolute bottom-5 h-32">
+        <TouchableOpacity
+          style={styles.shadow_bg}
+          className="flex-row w-full rounded-xl border-0.5 items-center justify-evenly h-full"
+          onPress={navigateToPage}
+        >
+          <View style={styles.border_blue} className="avatar aspect-square max-w-1/4 max-h-1/4 h-1/4 w-1/4 rounded-full items-center border-2">
+            <Image className="max-w-full max-h-full h-full w-full rounded-full" source={IMAGES.dummyProfile} />
+          </View>
+
+          <View className="h-full justify-around">
+            <View className="h-1/2 gap-y-1">
+              <Text className="text-base font-bold">Mohammad Ammash</Text>
+              <Text className="text-sm opacity-80">Technology</Text>
+              <Text className="text-xs opacity-50">Senior Web Developer</Text>
+            </View>
+            <View className="flex-row items-center justify-between w-52">
+              <View className="flex-row items-center gap-x-1">
+                <SimpleLineIcons name="location-pin" size={20} color={COLORS.blue} />
+                <Text className="text-[8px] opacity-80">5 km away</Text>
+              </View>
+              <Text className="text-[8px] opacity-50">3 years Experience</Text>
+            </View>
+          </View>
+        </TouchableOpacity>
+      </View>
     </View>
   );
+  // });
+  // }
 };
+
+const mapStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+});
 
 export default ProfileScreen;
