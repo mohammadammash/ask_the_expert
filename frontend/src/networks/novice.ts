e072e0ca83f02f1d9e2e29437096a42ac2274a71
mohammadammash <mohammadammash31@gmail.com>
2022-11-14 11:54:51 +0200

    frontend/src/networks/novice.ts: Adding book appointment post api

--- frontend/src/networks/novice.ts
+++ frontend/src/networks/novice.ts
@@ -1,8 +1,9 @@
 import axios from './index';
 
 class Novice_Apis {
-    closeExperts_get(params: any) { 
-        return axios.get(`/novice/${params.longitude}/${params.latitude}/${params.field}`).then(data => data).catch(err => err) }
+    closeExperts_get(params: any) { return axios.get(`/novice/${params.longitude}/${params.latitude}/${params.field}`).then(data => data).catch(err => err) }
+
+    bookAppointment_post(data: any) { return axios.post('/novice/book', data).then(data => data).catch(err => err) }
 }
 
 export default new Novice_Apis();
