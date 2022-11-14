e05d1c143cf0174052e57b5dc553ccda3c5b1f48
mohammadammash <mohammadammash31@gmail.com>
2022-11-14 23:03:34 +0200

    frontend/src/screens/Common/AppointmentsScreen.tsx: Updating user appointment instantly

--- frontend/src/screens/Common/AppointmentsScreen.tsx
+++ frontend/src/screens/Common/AppointmentsScreen.tsx
@@ -1,30 +1,87 @@
 import { useNavigation } from "@react-navigation/native";
-import { View, ScrollView, Text, Image } from "react-native";
+import { View, ScrollView, Text, Image, ActivityIndicator, Alert } from "react-native";
 import { useState, useEffect } from "react";
 //internal imports
 import { ChatAndAppointmentCardComponent } from "../../components";
 import { ROUTES, USERTYPES, IMAGES } from "../../constants";
 import styles from "../../../styles";
 import { useUserContext } from "../../hooks/UserContext";
+import { useDeleteAppointment } from "../../hooks/useUser";
 
 const AppointmentsScreen = () => {
   const navigation = useNavigation<any>();
+  const [removedAppointmentId, setRemovedAppointmentId] = useState("");
+  const { user, setUser } = useUserContext();
+  const { user_type } = user;
+  const [myAppointments, setMyAppointments] = useState<any>([]);
+
+  //----------------------------------
+  //START OF HANDLE REMOVE APPOINTMENT
+  const {
+    mutate: mutateRemoveAppointment,
+    data: mutateRemoveAppointmentData,
+    isLoading: mutateRemoveAppointmentDataIsLoading,
+    isSuccess: mutateRemoveAppointmentDataIsSuccess,
+  } = useDeleteAppointment();
+
+  const handleRemoveAppointment = (appointment_id: string) => {
+    Alert.alert("Remove Appointment", "\nAre you sure you want to Remove this appointment?", [
+      {
+        text: "Cancel",
+        style: "destructive",
+      },
+      {
+        text: "Submit",
+        onPress: () => {
+          setRemovedAppointmentId(appointment_id);
+          mutateRemoveAppointment(appointment_id);
+        },
+        style: "default",
+      },
+    ]);
+  };
+
+  useEffect(() => {
+    if (mutateRemoveAppointmentData && mutateRemoveAppointmentDataIsSuccess) {
+      const { user_device_token } = mutateRemoveAppointmentData.data;
+      //!!!!!!!!!SEND NOTIFICATION TO USERDEVICETOKEN!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+      const new_appointments = myAppointments.filter((app: any) => app._id !== removedAppointmentId); //update shown appointments
+      setMyAppointments([...new_appointments]);
+      setRemovedAppointmentId("");
+      if (user_type === USERTYPES.NOVICE) {
+        setUser({ ...user, appointments: new_appointments }); //update only user and appointments will be updated auto by bottom useEffect
+      } else {
+        //if expert
+        const { appointments_groups } = user; //then update current expert appointment group
+        for (let grp of appointments_groups) {
+          const end_timestamp = new Date(grp.end_timestamp);
+          const now = new Date();
+          if (!grp.isActive || end_timestamp < now) continue; //user chosen to go unactive before it finished => so I ignore end_timestamp
+          for (let app of grp.appointments) {
+            if (app._id === removedAppointmentId) {
+              app.isReserved = false;
+              break;
+            }
+          }
+        }
+        setUser({ ...user, appointments_groups });
+      }
+    }
+  }, [mutateRemoveAppointmentData]);
+  //END OF HANDLE REMOVE APPOINTMENT
+  //----------------------------------
 
   //Handle appointment card buttons interaction
   //if type === 'chat' send the user in data,if remove => send appointment_id in data
   const handleAppointmentClick = (type: string, data: any) => {
     if (type === "chat") navigation.navigate(ROUTES.USER_SINGLE_CHAT, { data });
-    else if (type === "remove") alert(JSON.stringify({ remove: "removeieto", appointment_id: data }, null, 2));
+    else if (type === "remove") handleRemoveAppointment(data);
   };
 
   //---------------------------------------
   //START OF SHOW CURRENT USER APPOINTMENTS
-  const { user, setUser } = useUserContext();
-  const { user_type } = user;
-  const [myAppointments, setMyAppointments] = useState<any>([]);
   useEffect(() => {
     const now = new Date();
-    setMyAppointments([]);
     const appointments = [];
     //Get novice appointments
     if (user.user_type === USERTYPES.NOVICE && user.appointments) {
@@ -54,6 +111,15 @@ const AppointmentsScreen = () => {
 
   //------------------
   //MAIN COMPONENT
+  if (mutateRemoveAppointmentDataIsLoading && !mutateRemoveAppointmentDataIsSuccess) {
+    return (
+      <View className="flex-1 justify-center items-center">
+        <ActivityIndicator size="large" />
+        <Text className="text-center w-3/4 text-xs mt-5 bold">Appointment is getting Removed &#10006</Text>
+      </View>
+    );
+  }
+
   if (myAppointments.length === 0) {
     return (
       <View className="flex-1 justify-center items-center">
