9c89b5d94d72f126db27b3febec6546f5ffad7b1
mohammadammash <mohammadammash31@gmail.com>
2022-11-14 19:55:41 +0200

    backend/src/controllers/expert/types.ts: Altering received props

--- backend/src/controllers/expert/types.ts
+++ backend/src/controllers/expert/types.ts
@@ -3,6 +3,8 @@ import { Number } from "mongoose";
 export interface GoOnlineBodyInterface {
     meetings_time: Number,
     single_session_time: Number,
+    longitude: Number,
+    latitude: Number,
 }
 
 export interface AddScoreBodyInterface {
