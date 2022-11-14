2abd90f9499b48488af67c7293b743cced8d9947
mohammadammash <mohammadammash31@gmail.com>
2022-11-14 21:34:44 +0200

    frontend/src/networks/user.ts: Adding axios delete api

--- frontend/src/networks/user.ts
+++ frontend/src/networks/user.ts
@@ -4,6 +4,8 @@ class User_Apis {
     user_get() { return axios.get('/user').then(data => data).catch(err => err) }
 
     leaderboard_get() { return axios.get('/user/leaderboard').then(data => data).catch(err => err) }
+
+    appointment_delete(data: any) { return axios.delete('/user/appointment', { data: { appointment_id: data } }).then(data => data).catch(err => err) }
 }
 
 export default new User_Apis();
