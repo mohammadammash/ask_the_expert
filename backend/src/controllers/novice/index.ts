4d54fd7d73b60f5e4b0c6c56bfc4706da7ca584b
mohammadammash <mohammadammash31@gmail.com>
2022-11-14 19:56:19 +0200

    backend/src/controllers/novice/index.ts: Altering getCloseExperts populated data and conditions

--- backend/src/controllers/novice/index.ts
+++ backend/src/controllers/novice/index.ts
@@ -3,11 +3,11 @@ const mongoose = require('mongoose');
 //internal imports
 const UserModel = require("../../database/models/User");
 const AppointmentModel = require("../../database/models/Appointment");
-import { getCloseExpertsBodyInterface, bookAppointmentBodyInterface, addReviewBodyInterface, deleteReviewBodyInterface } from "./types";
+import { getCloseExpertsParamsInterface, bookAppointmentBodyInterface, addReviewBodyInterface, deleteReviewBodyInterface } from "./types";
 
 //Get Close Experts within range to show on map in landing page
-const getCloseExperts = async (req: Request<{}, {}, getCloseExpertsBodyInterface>, res: Response) => {
-    const { latitude, longitude, field } = req.body;
+const getCloseExperts = async (req: Request<getCloseExpertsParamsInterface>, res: Response) => {
+    const { latitude, longitude, field } = req.params;
 
     try {
         const closeExperts = await UserModel.aggregate([{
@@ -23,9 +23,12 @@ const getCloseExperts = async (req: Request<{}, {}, getCloseExpertsBodyInterface
         if (!closeExperts) res.status(200).send([]);
         else {
             //remove unactive appointment_groups and populate only active experts with at least one unreserved appointment within same field
+            const now = new Date();
             let experts = closeExperts.filter((expert: any) => {
-                expert.appointments_groups = expert.appointments_groups.find((app: any) => app.isActive)
-                return expert;
+                if (expert.isAvailable) {
+                    expert.appointments_groups = expert.appointments_groups.filter((app: any) => app.isActive && app.end_timestamp > now);
+                    return expert;
+                }
             })
 
             const populateQuery = [{ path: 'appointments_groups.appointments' }, { path: 'reviews.novice_id' }];
@@ -40,7 +43,7 @@ const getCloseExperts = async (req: Request<{}, {}, getCloseExpertsBodyInterface
 
 // Novice book single appointment with expert
 const bookAppointment = async (req: Request<{}, {}, bookAppointmentBodyInterface>, res: Response) => {
-    const { appointment_id } = req.body;
+    const { appointment_id, notes } = req.body;
     const { currentUser_id } = req;
 
     try {
@@ -54,8 +57,9 @@ const bookAppointment = async (req: Request<{}, {}, bookAppointmentBodyInterface
         // make sure app exists and it is not reserved
         const now = new Date();
         if (appointment.start_timestamp > now && !appointment.isReserved) {
-            const data = await AppointmentModel.findByIdAndUpdate(appointment_id, { $set: { novice_id: currentUser_id, isReserved: true } }, { new: true }).lean();
-            res.status(200).send({ ...data, expert_device_token })
+            const data = await AppointmentModel.findByIdAndUpdate(appointment_id, { $set: { novice_id: currentUser_id, isReserved: true, notes: notes } }, { new: true }).lean();
+            await UserModel.updateOne({ _id: currentUser_id }, { $push: { appointments: appointment_id } })
+            res.status(200).send({ ...data, expert, expert_device_token })
         }
         else res.status(400).send('Cannot be Reserved');
     }
@@ -73,7 +77,8 @@ const addReview = async (req: Request<{}, {}, addReviewBodyInterface>, res: Resp
     if (exists) return res.status(400).send({ message: 'User Cannot have more than one review for one expert' });
 
     const _id = new mongoose.Types.ObjectId();
-    const new_review = { _id, novice_id: currentUser_id, rating: rating, content: content }
+    const now = new Date();
+    const new_review = { _id, novice_id: currentUser_id, rating: rating, content: content, createdAt: now };
     UserModel.updateOne({ _id: expert_id }, { $push: { reviews: new_review } })
         .then((data: any) => res.status(200).send(new_review))
         .catch((err: any) => res.status(400).send(err.message));
