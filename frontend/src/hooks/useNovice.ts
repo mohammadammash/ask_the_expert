630dbfd9366559a6a2eca36ef1187f05226d9052
mohammadammash <mohammadammash31@gmail.com>
2022-11-15 09:24:59 +0200

    frontend/src/hooks/useNovice.ts: Adding react query hooks to handle addReview and deleteReview APIs

--- frontend/src/hooks/useNovice.ts
+++ frontend/src/hooks/useNovice.ts
@@ -5,6 +5,7 @@ import Novice_Apis from "../networks/novice";
 
 export const CLOSE_EXPERTS_KEYS = ["CLOSE_EXPERTS_KEYS"];
 export const BOOKED_APPOINTMENT_KEY = (id: string) => [`BOOKED_APPOINTMENT_KEY_${id}`];
+export const CURRENT_USER_REVIEW_KEY = (expert_id: string) => [`CURRENT_USER_REVIEW_KEY_${expert_id}`]
 
 export const useCloseExperts = ({ enabled, params }: { enabled: boolean, params: any }) => {
     return useQuery({
@@ -24,3 +25,14 @@ export const useBookAppointment = () => useMutation({
         queryClient.setQueryData(BOOKED_APPOINTMENT_KEY(data.data._id), { ...data.data })
     }
 });
+
+export const useAddReview = () => useMutation({
+    mutationFn: (data: any) => Novice_Apis.addReview_post(data),
+    onSuccess: (data: any) => {
+        queryClient.setQueryData(BOOKED_APPOINTMENT_KEY(data.data._id), { ...data.data })
+    }
+});
+
+export const useDeleteReview = () => useMutation({
+    mutationFn: (data: any) => Novice_Apis.removeReview_delete(data),
+});;
