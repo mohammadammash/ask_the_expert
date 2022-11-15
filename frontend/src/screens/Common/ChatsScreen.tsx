ef295aeb01205cb43376b36add74fec621b39404
mohammadammash <mohammadammash31@gmail.com>
2022-11-15 21:18:22 +0200

    frontend/src/screens/Common/ChatsScreen.tsx: Displaying user chats with realtime update with firestore

--- frontend/src/screens/Common/ChatsScreen.tsx
+++ frontend/src/screens/Common/ChatsScreen.tsx
@@ -1,18 +1,40 @@
 import { useNavigation } from "@react-navigation/native";
-import { View, Text, Image } from "react-native";
-import { useState } from "react";
+import { View, Text, Image, ScrollView } from "react-native";
+import { useState, useEffect } from "react";
+import { doc, onSnapshot } from "firebase/firestore";
 //internal imports
 import { ChatAndAppointmentCardComponent } from "../../components";
 import { ROUTES, IMAGES } from "../../constants";
+import { useUserContext } from "../../hooks/UserContext";
+import { firebase_db } from "../../../firebaseConfig";
+import styles from "../../../styles";
+import { deepCopy } from "@firebase/util";
 
 const ChatsScreen = () => {
   const navigation = useNavigation<any>();
-  const [myChats, setMyChats] = useState([]);
-  const handleChatClick = () => {
-    // CHATS PAGE NAVIGATION
-    navigation.navigate(ROUTES.USER_SINGLE_CHAT);
-  };
+  const [myChats, setMyChats] = useState<any>([]);
+  const { user } = useUserContext();
 
+  //-----------------------------------------------
+  //START OF HANDLING GETTING ALL CHATS IMMEDIATELY
+  useEffect(() => {
+    const userChatsRef = doc(firebase_db, "userChats", user._id);
+    const unsub = onSnapshot(userChatsRef, (doc: any) => {
+      if (doc.data()) setMyChats(Object.entries(doc.data()));
+    });
+
+    return () => {
+      unsub();
+    };
+  }, []);
+  console.log(myChats);
+  //END OF HANDLING GETTING ALL CHATS IMMEDIATELY
+  //-----------------------------------------------
+
+  const handleChatClick = (type: string, data: any) => {
+    // CHATS PAGE NAVIGATION
+    if (type === "chat") navigation.navigate(ROUTES.USER_SINGLE_CHAT, { data });
+  };
 
   if (myChats.length === 0) {
     return (
@@ -23,15 +45,15 @@ const ChatsScreen = () => {
     );
   }
 
-  // return (
-  //   <View className="flex-1 items-center bg-white">
-  //     <ScrollView className="w-full" contentContainerStyle={styles.alignCenter}>
-  //       {myChats?.map((chat: any, index: number) => {
-  //         return <ChatAndAppointmentCardComponent key={index} shown_user={shown_user} data={chat} handleAppointmentClick={handleChatClick} />;
-  //       })}
-  //     </ScrollView>
-  //   </View>
-  // );
+  return (
+    <View className="flex-1 items-center bg-white">
+      <ScrollView className="w-full" contentContainerStyle={styles.alignCenter}>
+        {myChats?.map((chat: any, index: number) => {
+          return <ChatAndAppointmentCardComponent key={index} shown_user={chat[1]} data={""} handleCardClick={handleChatClick} />;
+        })}
+      </ScrollView>
+    </View>
+  );
 };
 
 export default ChatsScreen;
