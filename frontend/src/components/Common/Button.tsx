67453cbf56a96a401c54713ccbf5150c35660fd1
mohammadammash <mohammadammash31@gmail.com>
2022-11-15 09:41:55 +0200

    frontend/src/components/Common/Button.ts: Extracting button from profileScreen to common and add condition for lg button

--- frontend/src/components/Common/Button.tsx
+++ frontend/src/components/Common/Button.tsx
@@ -1,16 +1,17 @@
 import { View, Text, Pressable } from "react-native";
 //internal imports
-import styles from "../../../../styles";
-import { ButtonComponentProps } from "../types";
+import styles from "../../../styles";
+import { ButtonComponentProps } from "./types";
 
 const Button: React.FC<ButtonComponentProps> = ({ button_style, title, handlePress, route_name, disabled }) => {
   let current_style;
   if (button_style === "md") current_style = styles.blue_button_md;
-  else if (button_style === "sm") current_style = [styles.blue_button_xs];
+  else if (button_style === "sm") current_style = styles.blue_button_xs;
+  else if (button_style === "lg") current_style = styles.blue_button_lg; 
 
   return (
     <Pressable disabled={disabled} style={current_style} className={disabled ? "opacity-30" : ""} onPress={() => handlePress(route_name)}>
-      <Text className={`text-white font-bold ${button_style === "sm" ? "text-xs" : "text-sm"}`}>{title}</Text>
+      <Text className={`text-white font-bold text-${button_style}`}>{title}</Text>
     </Pressable>
   );
 };
