418e76c24a088e5a35c5bc2bc429cb105d2f4574
mohammadammash <mohammadammash31@gmail.com>
2022-11-15 09:23:57 +0200

    frontend/src/networks/novice.ts: Adding addReview, and deleteReview axios fetch APIs

--- frontend/src/networks/novice.ts
+++ frontend/src/networks/novice.ts
@@ -4,6 +4,10 @@ class Novice_Apis {
     closeExperts_get(params: any) { return axios.get(`/novice/${params.longitude}/${params.latitude}/${params.field}`).then(data => data).catch(err => err) }
 
     bookAppointment_post(data: any) { return axios.post('/novice/book', data).then(data => data).catch(err => err) }
+
+    addReview_post(data: any) { return axios.post('/novice/review', data).then(data => data).catch(err => err) }
+
+    removeReview_delete(data: any) { return axios.delete('/user/appointment', { data: { expert_id: data } }).then(data => data).catch(err => err) }
 }
 
 export default new Novice_Apis();
