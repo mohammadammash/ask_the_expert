37a7b1d26259da100c8a851bccd275a1e6b3669e
mohammadammash <mohammadammash31@gmail.com>
2022-11-17 00:16:42 +0200

    frontend/src/assets/languages/index.ts: Adding main config for languages change

---
+++ frontend/src/assets/languages/index.ts
@@ -0,0 +1,18 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import english from "./english.json";
+import french from "./french.json";
+
+i18n.use(initReactI18next).init({
+    lng: "en",
+    fallbackLng: "en",
+    resources: {
+        english: english,
+        french: french,
+    },
+    interpolation: {
+        escapeValue: false, // react already safes from xss
+    },
+});
+
+export default i18n;
