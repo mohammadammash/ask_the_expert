ea7cd992173aceed02b95608f4c27ca062a41750
mohammadammash <mohammadammash31@gmail.com>
2022-11-17 19:06:54 +0200

    frontend/App.tsx: Intializing notification to run and be set on main app file

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,6 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { useEffect, useState } from "react";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import * as Notifications from "expo-notifications";
 //internal imports
 import { userInitialData, UserContext } from "./src/hooks/UserContext";
 import { AuthStackNavigator, DrawerNavigator, AdminTabNavigator } from "./src/navigations";
@@ -8,13 +9,14 @@ import { USERTYPES } from "./src/constants";
 import "./src/assets/languages";
 import { useTranslation } from "react-i18next";
 import { useColorScheme } from "nativewind";
+import { useNotifications } from "./src/hooks/useNotifications";
 
 export const queryClient = new QueryClient();
 
 export default function App() {
   const [user, setUser] = useState(userInitialData);
 
-  //Translation && Theme
+  //START OF TRANSLATION AND THEME
   const { t, i18n } = useTranslation();
   const { colorScheme, toggleColorScheme } = useColorScheme();
   useEffect(() => {
@@ -27,7 +29,27 @@ export default function App() {
     if (user.theme === "dark" && colorScheme === "light") toggleColorScheme();
     else if ((!user.theme || user.theme === "light") && colorScheme === "dark") toggleColorScheme();
   }, [user.app_language, user.theme]);
+  //END OF TRANSLATION AND THEME
 
+  //START OF HANDLE NOTIFICATIONS
+  const { handleNotificationResponse } = useNotifications();
+  useEffect(() => {
+    Notifications.setNotificationHandler({
+      handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: true,
+        shouldSetBadge: true,
+      }),
+    });
+    const responseListener = Notifications.addNotificationResponseReceivedListener(handleNotificationResponse);
+    return () => {
+      if (responseListener) Notifications.removeNotificationSubscription(responseListener);
+    };
+  }, []);
+  //END OF HANDLE NOTIFICATIONS
+
+  //---------------------------
+  //MAIN RENDER
   return (
     <QueryClientProvider client={queryClient}>
       <UserContext.Provider value={{ user, setUser }}>
