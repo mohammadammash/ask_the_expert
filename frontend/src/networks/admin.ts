1b30b11575abbbec7b1e7780586afdb76ad87763
mohammadammash <mohammadammash31@gmail.com>
2022-11-17 22:28:11 +0200

    frontend/src/networks/admin.ts: Intializing admin networks/api with getAllUsersWithAppointmentStats

---
+++ frontend/src/networks/admin.ts
@@ -0,0 +1,7 @@
+import axios from './index';
+
+class Admin_Apis {
+    all_users_with_statistics_get() { return axios.get('/admin').then(data => data).catch(err => err) }
+}
+
+export default new Admin_Apis();
