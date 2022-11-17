32db032df722f1b2df56ee3af25ee4cfcd31af46
mohammadammash <mohammadammash31@gmail.com>
2022-11-17 22:31:58 +0200

    frontend/src/hooks/useAdmin.ts: Intializing admin getAllUsersWithAppointmentsCount Statistics react query hook

---
+++ frontend/src/hooks/useAdmin.ts
@@ -0,0 +1,15 @@
+import { useQuery } from "@tanstack/react-query";
+import { queryClient } from "../../App";
+//internal imports
+import Admin_Apis from "../networks/admin";
+
+export const ALL_USERS_KEY = ["ALL_USERS_KEY"];
+
+export const useGetAllUsersWithStatistics = () => useQuery({
+    queryKey: ALL_USERS_KEY,
+    refetchOnWindowFocus: true,
+    queryFn: () => Admin_Apis.all_users_with_statistics_get(),
+    onSuccess: (data) => {
+        queryClient.setQueryData(ALL_USERS_KEY, { ...data.data })
+    }
+})
