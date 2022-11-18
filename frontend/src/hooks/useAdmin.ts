59bf47b349c6ed8072beb38a255fac160fe9c106
mohammadammash <mohammadammash31@gmail.com>
2022-11-18 17:38:48 +0200

    frontend/src/hooks/useAdmin.ts: Adding auto reFetch every 30min to getAllUsers

--- frontend/src/hooks/useAdmin.ts
+++ frontend/src/hooks/useAdmin.ts
@@ -7,6 +7,7 @@ export const ALL_USERS_KEY = ["ALL_USERS_KEY"];
 
 export const useGetAllUsersWithStatistics = () => useQuery({
     queryKey: ALL_USERS_KEY,
+    refetchInterval: 1800000, //auto refetch every 30min
     refetchOnWindowFocus: true,
     queryFn: () => Admin_Apis.all_users_with_statistics_get(),
     onSuccess: (data) => {
