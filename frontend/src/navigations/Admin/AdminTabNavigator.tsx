19017caffb3c87ca16f388202262ba48b2c36c3c
mohammadammash <mohammadammash31@gmail.com>
2022-11-19 20:56:45 +0200

    frontend/src/navigations/Admin/AdminTabNavigator.tsx: Adding admin logout functionality

--- frontend/src/navigations/Admin/AdminTabNavigator.tsx
+++ frontend/src/navigations/Admin/AdminTabNavigator.tsx
@@ -1,45 +1,31 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Alert } from "react-native";
 //internal imports
-import { userInitialData, useUserContext } from "../../hooks/UserContext";
 import { ROUTES } from "../../constants";
 import { homeIcon, leaderboardIcon, tabBarScreenOptions, settingsIcon, usersIcon } from "../Helpers/NavigatorsHelpers";
 import { LeaderboardScreen, AdminHomeScreen, AdminViewUsersScreen } from "../../screens";
 import ProfileStackNavigator from "../Common/ProfileStackNavigator";
-import { t, use } from "i18next";
-import { removeAuthToken } from "../../networks";
+import { t } from "i18next";
+import { logoutUser } from "../../utils/authentication";
+import { userInitialData, useUserContext } from "../../hooks/UserContext";
 
 const Tab = createBottomTabNavigator();
 
 const AdminTabNavigator = () => {
-  const { setUser } = useUserContext();
-
   //translation
   const leaderboard_title = t("Leaderboard");
   const profile_title = t("Profile");
   const allusers_title = t("All Users");
   const home_title = t("Home");
-  const cancel_string = t("Cancel");
-  const submit_string = t("Submit");
-  const logout_string = t("Logout");
 
-  const handleLogout = () => {
-    const logout = async () => {
-      await removeAuthToken();
-      setUser({ ...userInitialData });
-    };
-
-    Alert.alert(logout_string, "\nAre you sure you want to Logout?", [
-      {
-        text: cancel_string,
-        style: "destructive",
-      },
-      { text: submit_string, onPress: () => logout(), style: "default" },
-    ]);
+  //Handle logout
+  const { setUser } = useUserContext();
+  const handleLogout = async () => {
+    const result = await logoutUser();
+    if (result) setUser({ ...userInitialData });
   };
 
   return (
-    <Tab.Navigator screenOptions={({ navigation }) => tabBarScreenOptions(navigation)}>
+    <Tab.Navigator screenOptions={() => tabBarScreenOptions(handleLogout)}>
       <Tab.Screen
         name={ROUTES.ADMIN_HOME}
         component={AdminHomeScreen}
