184ac25a8437464fc6712848b82369dfc9c3d5dc
mohammadammash <mohammadammash31@gmail.com>
2022-11-20 23:25:00 +0200

    frontend/src/networks/admin.ts: Adding axios fetch API to ban or unban user

--- frontend/src/networks/admin.ts
+++ frontend/src/networks/admin.ts
@@ -2,6 +2,10 @@ import axios from './base';
 
 class Admin_Apis {
     all_users_with_statistics_get() { return axios.get('/admin').then(data => data).catch(err => err) }
+
+    ban_or_unban_user_post(data: any) {
+        return axios.post('/admin/ban', data).then(data => data).catch(err => err)
+    }
 }
 
 export default new Admin_Apis();
