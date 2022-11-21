454e4fcabe5ba80176011a76ac569214b3a9a949
mohammadammash <mohammadammash31@gmail.com>
2022-11-21 16:21:06 +0200

    frontend/src/networks/auth.ts: Replacing then/catch Promise by async await to handle Error by react query

--- frontend/src/networks/auth.ts
+++ frontend/src/networks/auth.ts
@@ -1,11 +1,9 @@
 import axios from './base';
 
 class Auth_Apis {
-    login_post(data: any) { return axios.post('/login', data).then(data => data).catch(err => err) }
+    async login_post(data: any) { return await axios.post('/login', data) }
 
-    register_post(data: any) {
-        return axios.post('/register', data).then(data => data).catch(error => error);
-    }
+    async register_post(data: any) { return await axios.post('/register', data)}
 }
 
 export default new Auth_Apis();
