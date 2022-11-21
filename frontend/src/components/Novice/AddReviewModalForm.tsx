d1ecf5bde006f84a003a9b9c3fb8175ad7e8ae6f
mohammadammash <mohammadammash31@gmail.com>
2022-11-21 17:05:25 +0200

    frontend/src/components/Novice/AddReviewModalForm.tsx: Installing keyobardAwareScrolView library to show reiew input properly when keyboard pops up

--- frontend/src/components/Novice/AddReviewModalForm.tsx
+++ frontend/src/components/Novice/AddReviewModalForm.tsx
@@ -2,6 +2,7 @@ import { View, Text, TextInput, Pressable } from "react-native";
 import { Formik } from "formik";
 import * as Yup from "yup";
 import { AirbnbRating } from "react-native-ratings";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 //internal imports
 import styles from "../../../styles";
 import { RatingFormValuesTypes, AddReviewModalFormProps } from "./types";
@@ -29,30 +30,32 @@ const AddReviewModalForm: React.FC<AddReviewModalFormProps> = ({ modalRef, handl
       validationSchema={validateReviewIntialValues}
     >
       {({ handleChange, handleBlur, handleSubmit, errors, touched, values }) => (
-        <View className="w-full items-center">
-          <AirbnbRating
-            defaultRating={values.rating}
-            onFinishRating={(v) => (values.rating = v)}
-            starContainerStyle={noviceStyles.starRowRating}
-            size={20}
-            reviews={["Terrible", "Bad", "Okay", "Good", "Great"]}
-          />
+        <KeyboardAwareScrollView className='w-full'>
+          <View className="w-full items-center">
+            <AirbnbRating
+              defaultRating={values.rating}
+              onFinishRating={(v) => (values.rating = v)}
+              starContainerStyle={noviceStyles.starRowRating}
+              size={20}
+              reviews={["Terrible", "Bad", "Okay", "Good", "Great"]}
+            />
 
-          <TextInput
-            onChangeText={handleChange("content")}
-            onBlur={handleBlur("content")}
-            value={values.content}
-            className=" placeholder:pl-3 h-20 my-5 border-2 w-5/6 rounded-lg"
-            placeholder="Clearify what "
-            multiline={true}
-            numberOfLines={10}
-          />
-          {errors.content && touched.content && <Text className="text-red-600">{errors.content}</Text>}
+            <TextInput
+              onChangeText={handleChange("content")}
+              onBlur={handleBlur("content")}
+              value={values.content}
+              className=" placeholder:pl-3 h-20 my-5 border-2 w-5/6 rounded-lg"
+              placeholder="Clearify what "
+              multiline={true}
+              numberOfLines={10}
+            />
+            {errors.content && touched.content && <Text className="text-red-600">{errors.content}</Text>}
 
-          <Pressable className="mt-2" style={styles.blue_button_sm} onPress={handleSubmit}>
-            <Text className="text-sm text-white font-bold">SUBMIT</Text>
-          </Pressable>
-        </View>
+            <Pressable className="mt-2" style={styles.blue_button_sm} onPress={handleSubmit}>
+              <Text className="text-sm text-white font-bold">SUBMIT</Text>
+            </Pressable>
+          </View>
+        </KeyboardAwareScrollView>
       )}
     </Formik>
   );
