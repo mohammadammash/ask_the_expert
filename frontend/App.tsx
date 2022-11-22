fed590a29a89436abfd827b8622f80824f81e5d6
mohammadammash <mohammadammash31@gmail.com>
2022-11-22 12:50:31 +0200

    frontend/App.tsx: Handling loading google fonts

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,11 +1,15 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import * as Notifications from "expo-notifications";
+import * as SplashScreen from "expo-splash-screen";
+import { Roboto_400Regular, Roboto_400Regular_Italic, Roboto_700Bold, useFonts } from "@expo-google-fonts/roboto";
+import { setCustomText } from "react-native-global-props";
+import * as Font from "expo-font";
 //internal imports
 import { userInitialData, UserContext } from "./src/hooks/UserContext";
 import { AuthStackNavigator, DrawerNavigator, AdminTabNavigator } from "./src/navigations";
-import { USERTYPES } from "./src/constants";
+import { FONTS, USERTYPES } from "./src/constants";
 import "./src/assets/languages/i18n";
 import { useTranslation } from "react-i18next";
 import { useColorScheme } from "nativewind";
@@ -15,31 +19,24 @@ export const queryClient = new QueryClient();
 
 export default function App() {
   const [user, setUser] = useState(userInitialData);
+  const [appIsReady, setAppIsReady] = useState(false); //After loading fonts
 
   //START OF TRANSLATION AND THEME
   const { t, i18n } = useTranslation();
   const { colorScheme, toggleColorScheme } = useColorScheme();
   useEffect(() => {
-    if (user.app_language) {
-      i18n
-        .changeLanguage(user.app_language)
-        .then(() => console.log("🚀 ~ file: App.tsx ~ line 22 ~ useEffect ~ changed", user.app_language))
-        .catch((err) => console.log("🚀 ~ file: App.tsx ~ line 23 ~ useEffect ~ changed", err.message));
-    }
+    if (user.app_language) i18n.changeLanguage(user.app_language);
     if (user.theme === "dark" && colorScheme === "light") toggleColorScheme();
     else if ((!user.theme || user.theme === "light") && colorScheme === "dark") toggleColorScheme();
   }, [user.app_language, user.theme]);
   //END OF TRANSLATION AND THEME
 
+  //-----------------------------
   //START OF HANDLE NOTIFICATIONS
   const { handleNotificationResponse } = useNotifications();
   useEffect(() => {
     Notifications.setNotificationHandler({
-      handleNotification: async () => ({
-        shouldShowAlert: true,
-        shouldPlaySound: true,
-        shouldSetBadge: true,
-      }),
+      handleNotification: async () => ({ shouldShowAlert: true, shouldPlaySound: true, shouldSetBadge: true }),
     });
     const responseListener = Notifications.addNotificationResponseReceivedListener(handleNotificationResponse);
     return () => {
@@ -47,22 +44,51 @@ export default function App() {
     };
   }, []);
   //END OF HANDLE NOTIFICATIONS
+  //---------------------------
 
   //---------------------------
+  //START OF HANDLE FONT
+  useEffect(() => {
+    (async () => {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        await Font.loadAsync({ Roboto_400Regular, Roboto_400Regular_Italic, Roboto_700Bold });
+      } catch (err) {
+        console.log("🚀 ~ file: App.tsx ~ line 78 ~ { ~ err", err);
+      } finally {
+
+        setAppIsReady(true);
+      }
+    })();
+  }, []);
+
+  const onLayout = useCallback(() => {
+    (async () => {
+      if (appIsReady) {
+        await SplashScreen.hideAsync();
+      }
+    })();
+  }, [appIsReady]);
+  if (!appIsReady) return null;
+  //END OF HANDLE FONT
+  //---------------------------
+  //---------------------------
   //MAIN RENDER
-  return (
-    <QueryClientProvider client={queryClient}>
-      <UserContext.Provider value={{ user, setUser }}>
-        <NavigationContainer>
-          {user.user_type === USERTYPES.NOVICE || user.user_type === USERTYPES.EXPERT ? (
-            <DrawerNavigator />
-          ) : user.user_type === USERTYPES.ADMIN ? (
-            <AdminTabNavigator />
-          ) : (
-            <AuthStackNavigator />
-          )}
-        </NavigationContainer>
-      </UserContext.Provider>
-    </QueryClientProvider>
-  );
+  else {
+    return (
+      <QueryClientProvider client={queryClient}>
+        <UserContext.Provider value={{ user, setUser }}>
+          <NavigationContainer onReady={onLayout}>
+            {user.user_type === USERTYPES.NOVICE || user.user_type === USERTYPES.EXPERT ? (
+              <DrawerNavigator />
+            ) : user.user_type === USERTYPES.ADMIN ? (
+              <AdminTabNavigator />
+            ) : (
+              <AuthStackNavigator />
+            )}
+          </NavigationContainer>
+        </UserContext.Provider>
+      </QueryClientProvider>
+    );
+  }
 }
