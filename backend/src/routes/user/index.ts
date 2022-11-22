41fb3b4dd97dab965b413d23e7ed62a5ae37296a
mohammadammash <mohammadammash31@gmail.com>
2022-11-22 23:10:45 +0200

    frontend/src/routes/user: Removing unused routes

--- backend/src/routes/user/index.ts
+++ backend/src/routes/user/index.ts
@@ -1,15 +1,13 @@
 import { Router } from "express";
 const router = Router();
 //internal imports
-const { getSingleUserData, getRankedExperts, updateProfile, removeAppointment, blockOrUnblockUser, getUsersData } = require("../../controllers/user");
-const { getUsersDataValidationRules, updateProfileValidationRules, blockOrUnblockUserValidationRules, removeAppointmentValidationRules } = require('./validator');
+const { getSingleUserData, getRankedExperts, updateProfile, removeAppointment } = require("../../controllers/user");
+const { updateProfileValidationRules, removeAppointmentValidationRules } = require('./validator');
 const validateRequestMiddleware = require('../../middlewares/validateRequest')
 
 router.get("/leaderboard", getRankedExperts);
 router.get('/:user_id?', getSingleUserData);
-router.post('/users', getUsersDataValidationRules(), validateRequestMiddleware, getUsersData); //firebase chats users //used post to send array of ids
 router.put("/", updateProfileValidationRules(), validateRequestMiddleware, updateProfile);
-router.put('/block', blockOrUnblockUserValidationRules(), validateRequestMiddleware, blockOrUnblockUser);
 router.delete('/appointment', removeAppointmentValidationRules(), validateRequestMiddleware, removeAppointment)
 
 module.exports = router;
