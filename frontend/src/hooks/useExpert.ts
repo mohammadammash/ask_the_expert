85a8f4ea95a6e1b1e875466b4fb01a454895c107
mohammadammash <mohammadammash31@gmail.com>
2022-11-22 17:55:58 +0200

    frontend/src/hooks/useExpert.ts: Logging user out if response status 403(banned) or 401(unauthorized)

--- frontend/src/hooks/useExpert.ts
+++ frontend/src/hooks/useExpert.ts
@@ -1,7 +1,10 @@
 import { useMutation } from "@tanstack/react-query";
+import { AxiosError } from "axios";
 //internal imports 
 import { queryClient } from "../../App";
 import Expert_Apis from "../networks/expert";
+import { logoutUser } from "../utils/authentication";
+import { userInitialData, useUserContext } from "./UserContext";
 
 export const CURRENT_USER_APPOINTMENT_GROUP_KEY = ["CURRENT_USER_APPOINTMENT_GROUP_KEY"];
 
@@ -9,6 +12,13 @@ export const NOVICES_DEVICES_TOKENS_KEYS = ['NOVICES_DEVICES_TOKENS_KEYS'];
 
 export const useGoOnlineExpert = () => useMutation({
     mutationFn: (data: any) => Expert_Apis.goOnline_post(data),
+    onError: async (error: AxiosError) => {
+        const { setUser } = useUserContext();
+        if (error.request.status === 403 || error.request.status === 401) {
+            await logoutUser(true);
+            setUser({ ...userInitialData });
+        }
+    },
     onSuccess: (data: any) => {
         queryClient.setQueryData(CURRENT_USER_APPOINTMENT_GROUP_KEY, { ...data.data })
     }
@@ -16,6 +26,13 @@ export const useGoOnlineExpert = () => useMutation({
 
 export const useGoOfflineExpert = () => useMutation({
     mutationFn: () => Expert_Apis.goOffline_post(),
+    onError: async (error: AxiosError) => {
+        const { setUser } = useUserContext();
+        if (error.request.status === 403 || error.request.status === 401) {
+            await logoutUser(true);
+            setUser({ ...userInitialData });
+        }
+    },
     onSuccess: (data: any) => {
         queryClient.setQueryData(NOVICES_DEVICES_TOKENS_KEYS, { ...data.data })
     }
