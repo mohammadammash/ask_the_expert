ca193637c8b3ee067fec0cb01a70fb35ba455602
mohammadammash <mohammadammash31@gmail.com>
2022-11-23 11:21:26 +0200

    frontend/src/utils/authentication.ts: Removing alert

--- frontend/src/utils/authentication.ts
+++ frontend/src/utils/authentication.ts
@@ -20,21 +20,6 @@ export const setAuthToken = async (token: string) => {
 
 //User Logout by removing Authtoken and returning Boolean Success
 export const logoutUser = async () => {
-    const cancel_string = t("Cancel");
-    const submit_string = t("Submit");
-    const logout_string = t("Logout");
-
-    const AsyncAlert = async () => {
-        const choice = await AlertAsync(
-            logout_string,
-            "\nAre you sure you want to Logout?",
-            [{ text: cancel_string, style: "destructive", onPress: () => false },
-            { text: submit_string, style: 'default', onPress: () => true }],
-            { cancelable: true },
-        );
-        if (choice) await SecureStore.deleteItemAsync('token');
-        return choice;
-    }
-
-    return AsyncAlert();
+    await SecureStore.deleteItemAsync('token');
+    return true;
 }
